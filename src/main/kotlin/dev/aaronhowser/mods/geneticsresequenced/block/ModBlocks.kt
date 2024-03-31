fbf21e9079fd3388b1753d0c27980ef69b49a1f1
Aaron Howser <aaronhowser1@gmail.com>
2024-03-31 12:40:12 -0500

    Refactor to real name

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/block/ModBlocks.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/block/ModBlocks.kt
@@ -1,6 +1,6 @@
 package dev.aaronhowser.mods.geneticsresequenced.block
 
-import dev.aaronhowser.mods.geneticsresequenced.ExampleMod
+import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.state.BlockBehaviour
 import net.minecraft.world.level.material.Material
@@ -9,7 +9,7 @@ import net.minecraftforge.registries.ForgeRegistries
 import thedarkcolour.kotlinforforge.forge.registerObject
 
 object ModBlocks {
-    val REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, ExampleMod.ID)
+    val REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, GeneticsResequenced.ID)
 
     // the returned ObjectHolderDelegate can be used as a property delegate
     // this is automatically registered by the deferred registry at the correct times
