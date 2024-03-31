2a33ca32d82646d44f072e3166d0429a7fa1a01a
Aaron Howser <aaronhowser1@gmail.com>
2024-03-31 17:10:12 -0500

    Commands started

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/commands/ModCommands.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/commands/ModCommands.kt
@@ -1,32 +1,37 @@
 package dev.aaronhowser.mods.geneticsresequenced.commands
 
 import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.builder.ArgumentBuilder
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
+import dev.aaronhowser.mods.geneticsresequenced.api.capability.genes.EnumGenes
 import net.minecraft.commands.CommandSourceStack
 import net.minecraft.commands.Commands
-import net.minecraft.world.entity.player.Player
+import net.minecraft.commands.SharedSuggestionProvider
 
 object ModCommands {
 
     fun register(dispatcher: CommandDispatcher<CommandSourceStack>) {
         val modCommands = dispatcher.register(
             Commands.literal(GeneticsResequenced.ID)
-                .then(addGeneArgument(dispatcher))
+                .then(addGeneArgument())
         )
+
+        dispatcher.register(Commands.literal("genes").redirect(modCommands))
     }
 
-    fun addGeneArgument(dispatcher: CommandDispatcher<CommandSourceStack>): ArgumentBuilder<CommandSourceStack, *> {
+    fun addGeneArgument(): ArgumentBuilder<CommandSourceStack, *> {
         return Commands
             .literal("addGene")
-            .executes { context ->
-                val player = context.source.player
-
-                if (player != null) {
-                    println("Player is not null")
-                }
-
-                return@executes 1
-            }
+            .requires { it.hasPermission(2) }
+            .then(
+                Commands.argument("gene", StringArgumentType.string())
+                    .suggests { ctx, builder ->
+                        SharedSuggestionProvider.suggest(
+                            EnumGenes.values().map { it.name },
+                            builder
+                        )
+                    }
+            )
     }
 }
