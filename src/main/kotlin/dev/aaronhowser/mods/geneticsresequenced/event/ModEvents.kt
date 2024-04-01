81cd03525ee0f7e30700b0cd785d3e511f602507
Aaron Howser <aaronhowser1@gmail.com>
2024-03-31 19:40:14 -0500

    Wooly works (but isn't checked

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/event/ModEvents.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/event/ModEvents.kt
@@ -4,20 +4,21 @@ import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
 import dev.aaronhowser.mods.geneticsresequenced.ModTags
 import dev.aaronhowser.mods.geneticsresequenced.api.capability.CapabilityHandler
 import dev.aaronhowser.mods.geneticsresequenced.api.capability.genes.EnumGenes
-import dev.aaronhowser.mods.geneticsresequenced.api.capability.genes.GeneCapabilityProvider
 import dev.aaronhowser.mods.geneticsresequenced.api.capability.genes.Genes.getGenes
+import dev.aaronhowser.mods.geneticsresequenced.api.capability.genes.GenesCapabilityProvider
 import dev.aaronhowser.mods.geneticsresequenced.commands.ModCommands
-import net.minecraft.data.tags.ItemTagsProvider
-import net.minecraft.tags.ItemTags
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.LivingEntity
-import net.minecraft.world.entity.player.Player
+import net.minecraft.world.entity.item.ItemEntity
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.block.Blocks
 import net.minecraftforge.event.AttachCapabilitiesEvent
 import net.minecraftforge.event.RegisterCommandsEvent
 import net.minecraftforge.event.entity.player.PlayerEvent
 import net.minecraftforge.event.entity.player.PlayerInteractEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber
+import kotlin.random.Random
 
 @EventBusSubscriber(
     modid = GeneticsResequenced.ID
@@ -26,21 +27,20 @@ object ModEvents {
 
     @SubscribeEvent
     fun onAttachCapability(event: AttachCapabilitiesEvent<Entity>) {
-        val player = event.`object`
-        if (player !is Player) return
+        val entity = event.`object`
 
-        val playerHasCapability = player.getCapability(GeneCapabilityProvider.GENE_CAPABILITY).isPresent
-        if (playerHasCapability) return
+        val entityHasCapability = entity.getCapability(GenesCapabilityProvider.GENE_CAPABILITY).isPresent
+        if (entityHasCapability) return
 
-        event.addCapability(CapabilityHandler.GENE_CAPABILITY_RL, GeneCapabilityProvider)
+        event.addCapability(CapabilityHandler.GENE_CAPABILITY_RL, GenesCapabilityProvider)
     }
 
     @SubscribeEvent
     fun onPlayerCloned(event: PlayerEvent.Clone) {
         if (!event.isWasDeath) return
 
-        event.original.getCapability(GeneCapabilityProvider.GENE_CAPABILITY).ifPresent { oldGenes ->
-            event.original.getCapability(GeneCapabilityProvider.GENE_CAPABILITY).ifPresent { newGenes ->
+        event.original.getCapability(GenesCapabilityProvider.GENE_CAPABILITY).ifPresent { oldGenes ->
+            event.original.getCapability(GenesCapabilityProvider.GENE_CAPABILITY).ifPresent { newGenes ->
                 newGenes.setGeneList(oldGenes.getGeneList())
             }
         }
@@ -53,17 +53,40 @@ object ModEvents {
 
     @SubscribeEvent
     fun onInteractEntity(event: PlayerInteractEvent.EntityInteract) {
+
+        if (event.side.isClient) return
+
         val target = event.target
         if (target !is LivingEntity) return
 
         val genes = target.getGenes()
 
-        if (genes.hasGene(EnumGenes.WOOLY)) wooly(event)
+//        if (genes.hasGene(EnumGenes.WOOLY))
+        wooly(event)
         if (genes.hasGene(EnumGenes.MILKY)) milky(event)
     }
 
     private fun wooly(event: PlayerInteractEvent.EntityInteract) {
-        println(event.itemStack.`is`(ModTags.SHEARS_TAG))
+        val clickedWithShears = event.itemStack.`is`(ModTags.SHEARS_TAG)
+        if (!clickedWithShears) return
+
+        val woolItemStack = ItemStack(Blocks.WHITE_WOOL)
+
+        val woolEntity = ItemEntity(
+            event.level,
+            event.target.eyePosition.x,
+            event.target.eyePosition.y,
+            event.target.eyePosition.z,
+            woolItemStack
+        )
+        event.level.addFreshEntity(woolEntity)
+        woolEntity.setDeltaMovement(
+            Random.nextDouble(-0.05, 0.05),
+            Random.nextDouble(0.05, 0.1),
+            Random.nextDouble(-0.05, 0.05)
+        )
+
+        event.itemStack.hurtAndBreak(1, event.entity) { }
     }
 
     private fun milky(event: PlayerInteractEvent.EntityInteract) {
