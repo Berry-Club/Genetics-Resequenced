ca062cd6048bf70729de58bb07d56443fb59f662
Aaron Howser <aaronhowser1@gmail.com>
2024-04-02 04:06:35 -0500

    Implemented No Fall Damage

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/event/entity/OtherEntityEvents.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/event/entity/OtherEntityEvents.kt
@@ -3,9 +3,11 @@ package dev.aaronhowser.mods.geneticsresequenced.event.entity
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
 import dev.aaronhowser.mods.geneticsresequenced.api.capability.CapabilityHandler
 import dev.aaronhowser.mods.geneticsresequenced.api.capability.genes.GenesCapabilityProvider
+import dev.aaronhowser.mods.geneticsresequenced.genebehavior.OtherGenes
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.LivingEntity
 import net.minecraftforge.event.AttachCapabilitiesEvent
+import net.minecraftforge.event.entity.living.LivingDamageEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 
@@ -21,6 +23,13 @@ object OtherEntityEvents {
         if (entity !is LivingEntity) return
 
         event.addCapability(CapabilityHandler.GENE_CAPABILITY_RL, GenesCapabilityProvider())
+    }
+
+    @SubscribeEvent
+    fun onLivingDamage(event: LivingDamageEvent) {
+        if (event.entity.level.isClientSide) return
+
+        OtherGenes.handleNoFallDamage(event)
     }
 
 }
