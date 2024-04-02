1623f2ffb2cb50198b01d885a75820c0407db457
Aaron Howser <aaronhowser1@gmail.com>
2024-04-02 04:22:31 -0500

    Implemented WitherProof and WitherHit

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/event/entity/OtherEntityEvents.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/event/entity/OtherEntityEvents.kt
@@ -3,10 +3,11 @@ package dev.aaronhowser.mods.geneticsresequenced.event.entity
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
 import dev.aaronhowser.mods.geneticsresequenced.api.capability.CapabilityHandler
 import dev.aaronhowser.mods.geneticsresequenced.api.capability.genes.GenesCapabilityProvider
-import dev.aaronhowser.mods.geneticsresequenced.genebehavior.OtherGenes
+import dev.aaronhowser.mods.geneticsresequenced.genebehavior.DamageGenes
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.LivingEntity
 import net.minecraftforge.event.AttachCapabilitiesEvent
+import net.minecraftforge.event.entity.living.LivingAttackEvent
 import net.minecraftforge.event.entity.living.LivingDamageEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
@@ -29,7 +30,15 @@ object OtherEntityEvents {
     fun onLivingDamage(event: LivingDamageEvent) {
         if (event.entity.level.isClientSide) return
 
-        OtherGenes.handleNoFallDamage(event)
+        DamageGenes.handleNoFallDamage(event)
+        DamageGenes.handleWitherProof(event)
+    }
+
+    @SubscribeEvent
+    fun onAttack(event: LivingAttackEvent) {
+        if (event.entity.level.isClientSide) return
+
+        DamageGenes.handleWitherHit(event)
     }
 
 }
---
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/genebehavior/DamageGenes.kt
@@ -0,0 +1,52 @@
+package dev.aaronhowser.mods.geneticsresequenced.genebehavior
+
+import dev.aaronhowser.mods.geneticsresequenced.api.capability.genes.EnumGenes
+import dev.aaronhowser.mods.geneticsresequenced.api.capability.genes.Genes.Companion.getGenes
+import net.minecraft.world.damagesource.DamageSource
+import net.minecraft.world.damagesource.IndirectEntityDamageSource
+import net.minecraft.world.effect.MobEffectInstance
+import net.minecraft.world.effect.MobEffects
+import net.minecraft.world.entity.LivingEntity
+import net.minecraftforge.event.entity.living.LivingAttackEvent
+import net.minecraftforge.event.entity.living.LivingDamageEvent
+
+object DamageGenes {
+
+    fun handleNoFallDamage(event: LivingDamageEvent) {
+        if (event.source != DamageSource.FALL) return
+
+        val genes = event.entity.getGenes()
+        if (genes?.hasGene(EnumGenes.NO_FALL_DAMAGE) != true) return
+
+        event.isCanceled = true
+    }
+
+    fun handleWitherProof(event: LivingDamageEvent) {
+        if (event.source != DamageSource.WITHER) return
+
+        val genes = event.entity.getGenes()
+        if (genes?.hasGene(EnumGenes.WITHER_PROOF) != true) return
+
+        event.isCanceled = true
+    }
+
+    fun handleWitherHit(event: LivingAttackEvent) {
+
+        // Make it not proc if it's an arrow or whatever
+        if (event.source is IndirectEntityDamageSource) return
+
+        val witherEffect = MobEffectInstance(
+            MobEffects.WITHER,
+            100
+        )
+
+        if (!event.entity.canBeAffected(witherEffect)) return
+
+        val attacker = event.source.entity as? LivingEntity ?: return
+        val genes = attacker.getGenes()
+        if (genes?.hasGene(EnumGenes.WITHER_HIT) != true) return
+
+        event.entity.addEffect(witherEffect)
+    }
+
+}
