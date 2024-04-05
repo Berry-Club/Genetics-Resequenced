e9717bb2854f62b1c3afbec442ecf19e41f1634d
Aaron Howser <aaronhowser1@gmail.com>
2024-04-05 17:54:13 -0500

    BioluminescenceBlock now extends LightBlock instead of AirBlock

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/block/BioluminescenceBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/block/BioluminescenceBlock.kt
@@ -1,10 +1,19 @@
 package dev.aaronhowser.mods.geneticsresequenced.block
 
-import net.minecraft.world.level.block.AirBlock
+import dev.aaronhowser.mods.geneticsresequenced.blockentity.BioluminescenceBlockEntity
+import dev.aaronhowser.mods.geneticsresequenced.blockentity.ModBlockEntities
+import net.minecraft.core.BlockPos
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.EntityBlock
+import net.minecraft.world.level.block.LightBlock
+import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityTicker
+import net.minecraft.world.level.block.entity.BlockEntityType
+import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.material.Material
 
 object BioluminescenceBlock :
-    AirBlock(
+    LightBlock(
         Properties
             .of(Material.AIR)
             .air()
