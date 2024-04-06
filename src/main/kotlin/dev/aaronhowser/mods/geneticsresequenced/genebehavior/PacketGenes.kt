a7c9830918612d5ab170be6161da1e7488702973
Aaron Howser <aaronhowser1@gmail.com>
2024-04-06 03:56:07 -0500

    Teleporting works more now?

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/genebehavior/PacketGenes.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/genebehavior/PacketGenes.kt
@@ -1,45 +1,50 @@
 package dev.aaronhowser.mods.geneticsresequenced.genebehavior
 
+import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
 import net.minecraft.server.level.ServerPlayer
-import net.minecraft.world.level.ClipBlockStateContext
+import net.minecraft.world.level.ClipContext
 import net.minecraft.world.phys.BlockHitResult
 import net.minecraft.world.phys.HitResult
 import net.minecraft.world.phys.Vec3
+import thedarkcolour.kotlinforforge.forge.vectorutil.toVec3
 
 object PacketGenes {
 
+    @Suppress("MoveVariableDeclarationIntoWhen")
     fun teleport(player: ServerPlayer) {
         val lookDirection = player.lookAngle.normalize().scale(10.0)
 
-        val lookingAtBlock: BlockHitResult = player.level.isBlockInLine(ClipBlockStateContext(
-            player.eyePosition,
-            player.eyePosition.add(lookDirection)
-        ) { state -> !state.isAir })
+        val lookingAtBlock: BlockHitResult = player.level.clip(
+            ClipContext(
+                player.eyePosition,
+                player.eyePosition.add(lookDirection),
+                ClipContext.Block.OUTLINE,
+                ClipContext.Fluid.NONE,
+                player
+            )
+        )
 
-        val destination = if (lookingAtBlock.type == HitResult.Type.MISS) {
+        var destination = if (lookingAtBlock.type == HitResult.Type.MISS) {
             player.eyePosition.add(lookDirection)
         } else {
 
-            val blockLocation = lookingAtBlock.location.add(0.5, 1.1, 0.5)
+            val blockLocation = lookingAtBlock.location
             val sideHit = lookingAtBlock.direction
 
-            println("Block hit: $blockLocation")
-            println("Side hit: $sideHit")
-
             val offset = when (sideHit) {
-                Direction.UP -> Vec3(0.0, 1.0, 0.0)
-                Direction.DOWN -> Vec3(0.0, -1.0, 0.0)
-                Direction.NORTH -> Vec3(0.0, 0.0, -1.0)
-                Direction.SOUTH -> Vec3(0.0, 0.0, 1.0)
-                Direction.WEST -> Vec3(-1.0, 0.0, 0.0)
-                Direction.EAST -> Vec3(1.0, 0.0, 0.0)
-                else -> Vec3.ZERO
+                Direction.DOWN -> Vec3(0.0, -2.0, 0.0)
+                Direction.UP -> Vec3(0.0, 0.5, 0.0)
+                else -> sideHit.normal.toVec3()
             }
 
             blockLocation.add(offset)
         }
-        println("Destination: $destination")
+
+        val blockAtNewFootLocation = player.level.getBlockState(BlockPos(destination.add(0.0, -1.0, 0.0)))
+        val footBlockIsSolid = blockAtNewFootLocation.entityCanStandOn(player.level, BlockPos(destination), player)
+
+        if (footBlockIsSolid) destination = destination.add(0.0, 1.0, 0.0)
 
         player.teleportTo(destination.x, destination.y, destination.z)
     }
