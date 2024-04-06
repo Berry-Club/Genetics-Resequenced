20ecdb9d5527485234ed59c464ce3029e8c9ef49
Aaron Howser <aaronhowser1@gmail.com>
2024-04-05 19:09:27 -0500

    Only place biolumen if it's dark

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/genebehavior/TickGenes.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/genebehavior/TickGenes.kt
@@ -5,12 +5,15 @@ import dev.aaronhowser.mods.geneticsresequenced.api.capability.genes.Genes.Compa
 import dev.aaronhowser.mods.geneticsresequenced.block.ModBlocks
 import dev.aaronhowser.mods.geneticsresequenced.config.ServerConfig
 import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.level.LightLayer
 
 object TickGenes {
 
     fun handleBioluminescence(entity: LivingEntity) {
 
         if (entity.tickCount % ServerConfig.bioluminescenceCooldown.get() != 0) return
+
+        if (entity.level.getBrightness(LightLayer.BLOCK, entity.blockPosition()) > 8) return
 
         val genes = entity.getGenes() ?: return
         if (!genes.hasGene(EnumGenes.BIOLUMINESCENCE)) return
