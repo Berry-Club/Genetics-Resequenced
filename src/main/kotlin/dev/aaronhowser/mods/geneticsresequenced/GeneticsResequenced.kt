8d333536ba3236385485677684c2fe308f59652e
Aaron Howser <aaronhowser1@gmail.com>
2024-04-06 22:30:25 -0500

    Attributes are now registerred

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/GeneticsResequenced.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/GeneticsResequenced.kt
@@ -1,24 +1,20 @@
 package dev.aaronhowser.mods.geneticsresequenced
 
+import dev.aaronhowser.mods.geneticsresequenced.attribute.ModAttributes
 import dev.aaronhowser.mods.geneticsresequenced.block.ModBlocks
 import dev.aaronhowser.mods.geneticsresequenced.blockentity.ModBlockEntities
 import dev.aaronhowser.mods.geneticsresequenced.config.ServerConfig
 import dev.aaronhowser.mods.geneticsresequenced.item.ModItems
 import dev.aaronhowser.mods.geneticsresequenced.packet.ModPacketHandler
 import dev.aaronhowser.mods.geneticsresequenced.potion.ModEffects
-import net.minecraft.client.Minecraft
 import net.minecraft.world.item.CreativeModeTab
 import net.minecraft.world.item.ItemStack
 import net.minecraftforge.fml.ModLoadingContext
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.config.ModConfig
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
-import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent
-import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import thedarkcolour.kotlinforforge.forge.MOD_BUS
-import thedarkcolour.kotlinforforge.forge.runForDist
 
 @Mod(GeneticsResequenced.ID)
 object GeneticsResequenced {
@@ -37,6 +33,7 @@ object GeneticsResequenced {
             registerConfig(ModConfig.Type.SERVER, ServerConfig.SPEC, "geneticsresequenced-server.toml")
         }
 
+        ModAttributes.REGISTRY.register(MOD_BUS)
         ModEffects.REGISTRY.register(MOD_BUS)
         ModBlocks.REGISTRY.register(MOD_BUS)
         ModBlockEntities.REGISTRY.register(MOD_BUS)
--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/attribute/ModAttributes.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/attribute/ModAttributes.kt
@@ -1,13 +1,25 @@
 package dev.aaronhowser.mods.geneticsresequenced.attribute
 
+import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
+import net.minecraft.world.entity.ai.attributes.Attribute
 import net.minecraft.world.entity.ai.attributes.RangedAttribute
+import net.minecraftforge.registries.DeferredRegister
+import net.minecraftforge.registries.ForgeRegistries
+import thedarkcolour.kotlinforforge.forge.registerObject
 
 object ModAttributes {
 
+    val REGISTRY: DeferredRegister<Attribute> =
+        DeferredRegister.create(ForgeRegistries.ATTRIBUTES, GeneticsResequenced.ID)
+
     private const val EFFICIENCY_ATTRIBUTE_NAME = "geneticsresequenced.efficiency"
-    val EFFICIENCY = RangedAttribute(EFFICIENCY_ATTRIBUTE_NAME, 0.0, 0.0, 100000.0)
+    val EFFICIENCY by REGISTRY.registerObject("efficiency") {
+        RangedAttribute(EFFICIENCY_ATTRIBUTE_NAME, 0.0, 0.0, 1.0)
+    }
 
     private const val CLIMBING_ATTRIBUTE_NAME = "geneticsresequenced.climbing"
-    val CLIMBING = RangedAttribute(CLIMBING_ATTRIBUTE_NAME, 0.0, 0.0, 1.0)
+    val CLIMBING by REGISTRY.registerObject("climbing") {
+        RangedAttribute(CLIMBING_ATTRIBUTE_NAME, 0.0, 0.0, 1.0)
+    }
 
 }
