c5f3e82947c14bc2ea7f14821454f69a916930b4
Aaron Howser <aaronhowser1@gmail.com>
2024-04-14 18:28:00 -0500

    refactor

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/blockentities/ModBlockEntities.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/blockentities/ModBlockEntities.kt
@@ -10,11 +10,11 @@ import net.minecraftforge.registries.RegistryObject
 
 object ModBlockEntities {
 
-    val Block_ENTITY_REGISTRY: DeferredRegister<BlockEntityType<*>> =
+    val BLOCK_ENTITY_REGISTRY: DeferredRegister<BlockEntityType<*>> =
         DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, GeneticsResequenced.ID)
 
     val CELL_ANALYZER: RegistryObject<BlockEntityType<CellAnalyzerBlockEntity>> =
-        Block_ENTITY_REGISTRY.register("cell_analyzer") {
+        BLOCK_ENTITY_REGISTRY.register("cell_analyzer") {
             BlockEntityType.Builder.of(
                 { pos, state -> CellAnalyzerBlockEntity(pos, state) },
                 ModBlocks.CELL_ANALYZER
@@ -22,7 +22,7 @@ object ModBlockEntities {
         }
 
     val COAL_GENERATOR: RegistryObject<BlockEntityType<CoalGeneratorBlockEntity>> =
-        Block_ENTITY_REGISTRY.register("coal_generator") {
+        BLOCK_ENTITY_REGISTRY.register("coal_generator") {
             BlockEntityType.Builder.of(
                 { pos, state -> CoalGeneratorBlockEntity(pos, state) },
                 ModBlocks.COAL_GENERATOR
@@ -30,7 +30,7 @@ object ModBlockEntities {
         }
 
     val DNA_DECRYPTOR: RegistryObject<BlockEntityType<DnaDecryptorBlockEntity>> =
-        Block_ENTITY_REGISTRY.register("dna_decryptor") {
+        BLOCK_ENTITY_REGISTRY.register("dna_decryptor") {
             BlockEntityType.Builder.of(
                 { pos, state -> DnaDecryptorBlockEntity(pos, state) },
                 ModBlocks.DNA_DECRYPTOR
