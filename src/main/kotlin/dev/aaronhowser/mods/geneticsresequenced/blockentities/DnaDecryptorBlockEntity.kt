155faee02588846fb53b05899ec44242aa2a6c1c
Aaron Howser <aaronhowser1@gmail.com>
2024-04-14 22:27:03 -0500

    Trying out better inheritence

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/blockentities/DnaDecryptorBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/blockentities/DnaDecryptorBlockEntity.kt
@@ -2,159 +2,55 @@ package dev.aaronhowser.mods.geneticsresequenced.blockentities
 
 import dev.aaronhowser.mods.geneticsresequenced.api.capability.genes.Gene
 import dev.aaronhowser.mods.geneticsresequenced.api.capability.genes.MobGenesRegistry
+import dev.aaronhowser.mods.geneticsresequenced.blockentities.base.CraftingMachine
 import dev.aaronhowser.mods.geneticsresequenced.items.DnaHelixItem
 import dev.aaronhowser.mods.geneticsresequenced.items.DnaHelixItem.setGene
 import dev.aaronhowser.mods.geneticsresequenced.items.EntityDnaItem
 import dev.aaronhowser.mods.geneticsresequenced.items.ModItems
-import dev.aaronhowser.mods.geneticsresequenced.packets.ModPacketHandler
-import dev.aaronhowser.mods.geneticsresequenced.packets.server_to_client.EnergySyncPacket
 import dev.aaronhowser.mods.geneticsresequenced.screens.DnaDecryptorMenu
-import dev.aaronhowser.mods.geneticsresequenced.util.ModEnergyStorage
 import net.minecraft.core.BlockPos
-import net.minecraft.core.Direction
-import net.minecraft.nbt.CompoundTag
 import net.minecraft.network.chat.Component
-import net.minecraft.world.Containers
 import net.minecraft.world.MenuProvider
 import net.minecraft.world.SimpleContainer
 import net.minecraft.world.entity.player.Inventory
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.inventory.AbstractContainerMenu
-import net.minecraft.world.inventory.ContainerData
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
-import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
-import net.minecraftforge.common.capabilities.Capability
-import net.minecraftforge.common.capabilities.ForgeCapabilities
-import net.minecraftforge.common.util.LazyOptional
-import net.minecraftforge.energy.IEnergyStorage
-import net.minecraftforge.items.IItemHandler
 import net.minecraftforge.items.ItemStackHandler
 
 class DnaDecryptorBlockEntity(
     pPos: BlockPos,
     pBlockState: BlockState
-) : BlockEntity(
+) : CraftingMachine(
     ModBlockEntities.DNA_DECRYPTOR.get(),
     pPos,
     pBlockState
 ), MenuProvider {
+    override val inventoryNbtKey: String = "dna_decryptor.inventory"
+    override val energyNbtKey: String = "dna_decryptor.energy"
+    override val energyMaximum: Int = 60_000
+    override val energyTransferMaximum: Int = 256
+    override val energyPerTick: Int = 32
 
-    private val itemHandler: ItemStackHandler = object : ItemStackHandler(ITEMSTACK_HANDLER_SIZE) {
+    override val itemHandler: ItemStackHandler = object : ItemStackHandler(itemstackHandlerSize) {
         override fun onContentsChanged(slot: Int) {
             setChanged()
         }
 
         override fun isItemValid(slot: Int, stack: ItemStack): Boolean {
             return when (slot) {
-                INPUT_SLOT -> stack.`is`(ModItems.DNA_HELIX)
-                OUTPUT_SLOT -> false
+                inputSlot -> stack.`is`(ModItems.DNA_HELIX)
+                outputSlot -> false
                 OVERCLOCK_SLOT -> false
                 else -> false
             }
         }
     }
 
-    private var lazyItemHandler = LazyOptional.empty<IItemHandler>()
-
-    private val energyStorage: ModEnergyStorage = object : ModEnergyStorage(CAPACITY, MAX_TRANSFER) {
-        override fun onEnergyChanged() {
-            setChanged()
-
-            ModPacketHandler.messageAllPlayers(EnergySyncPacket(energy, blockPos))
-        }
-    }
-
-    private var lazyEnergyStorage = LazyOptional.empty<IEnergyStorage>()
-
-    override fun <T : Any?> getCapability(cap: Capability<T>, side: Direction?): LazyOptional<T> {
-        return when (cap) {
-            ForgeCapabilities.ITEM_HANDLER -> getItemCapability(side).cast()
-
-            ForgeCapabilities.ENERGY -> getEnergyCapability(side).cast()
-
-            else -> super.getCapability(cap, side)
-        }
-    }
-
-    private fun getItemCapability(side: Direction?): LazyOptional<IItemHandler> {
-        return lazyItemHandler.cast()
-    }
-
-    private fun getEnergyCapability(side: Direction?): LazyOptional<IEnergyStorage> {
-        return lazyEnergyStorage.cast()
-    }
-
-    fun getEnergyStorage(): IEnergyStorage = energyStorage
-
-    override fun onLoad() {
-        super.onLoad()
-        lazyItemHandler = LazyOptional.of { itemHandler }
-        lazyEnergyStorage = LazyOptional.of { energyStorage }
-    }
-
-    override fun invalidateCaps() {
-        super.invalidateCaps()
-        lazyItemHandler.invalidate()
-        lazyEnergyStorage.invalidate()
-    }
-
-    override fun saveAdditional(pTag: CompoundTag) {
-        pTag.put(INVENTORY_NBT_KEY, itemHandler.serializeNBT())
-        pTag.putInt(ENERGY_NBT_KEY, energyStorage.energyStored)
-        super.saveAdditional(pTag)
-    }
-
-    override fun load(pTag: CompoundTag) {
-        super.load(pTag)
-        itemHandler.deserializeNBT(pTag.getCompound(INVENTORY_NBT_KEY))
-        energyStorage.setEnergy(pTag.getInt(ENERGY_NBT_KEY))
-    }
-
-    fun drops() {
-        val inventory = SimpleContainer(itemHandler.slots)
-        for (i in 0 until itemHandler.slots) {
-            inventory.setItem(i, itemHandler.getStackInSlot(i))
-        }
-
-        Containers.dropContents(this.level!!, this.blockPos, inventory)
-    }
-
-    private var progress = 0
-    private var maxProgress = 20 * 4
-    private fun resetProgress() {
-        progress = 0
-    }
-
-    // Client only
-    fun setEnergy(energy: Int) {
-        this.energyStorage.setEnergy(energy)
-    }
-
-    private val data = object : ContainerData {
-        override fun get(pIndex: Int): Int {
-            return when (pIndex) {
-                PROGRESS_INDEX -> progress
-                MAX_PROGRESS_INDEX -> maxProgress
-                else -> 0
-            }
-        }
-
-        override fun set(pIndex: Int, pValue: Int) {
-            when (pIndex) {
-                PROGRESS_INDEX -> progress = pValue
-                MAX_PROGRESS_INDEX -> maxProgress = pValue
-            }
-        }
-
-        override fun getCount(): Int {
-            return SIMPLE_CONTAINER_SIZE
-        }
-    }
-
     override fun createMenu(pContainerId: Int, pPlayerInventory: Inventory, pPlayer: Player): AbstractContainerMenu {
-        return DnaDecryptorMenu(pContainerId, pPlayerInventory, this, this.data)
+        return DnaDecryptorMenu(pContainerId, pPlayerInventory, this, this.containerData)
     }
 
     override fun getDisplayName(): Component {
@@ -260,10 +156,6 @@ class DnaDecryptorBlockEntity(
             return DnaHelixItem.getGene(outputSlot) == DnaHelixItem.getGene(potentialOutput)
         }
 
-        private const val INVENTORY_NBT_KEY = "dna_decryptor.inventory"
-        private const val PROGRESS_INDEX = 0
-        private const val MAX_PROGRESS_INDEX = 1
-
         const val SIMPLE_CONTAINER_SIZE = 2
         const val ITEMSTACK_HANDLER_SIZE = 3
 
@@ -271,9 +163,6 @@ class DnaDecryptorBlockEntity(
         const val OUTPUT_SLOT = 1
         const val OVERCLOCK_SLOT = 2
 
-        private const val ENERGY_NBT_KEY = "dna_decryptor.energy"
-        private const val CAPACITY = 60_000
-        private const val MAX_TRANSFER = 256
         private const val ENERGY_PER_TICK = 32
 
     }
---
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/blockentities/base/CraftingMachine.kt
@@ -0,0 +1,154 @@
+package dev.aaronhowser.mods.geneticsresequenced.blockentities.base
+
+import dev.aaronhowser.mods.geneticsresequenced.packets.ModPacketHandler
+import dev.aaronhowser.mods.geneticsresequenced.packets.server_to_client.EnergySyncPacket
+import dev.aaronhowser.mods.geneticsresequenced.util.ModEnergyStorage
+import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.world.Containers
+import net.minecraft.world.SimpleContainer
+import net.minecraft.world.inventory.ContainerData
+import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityType
+import net.minecraft.world.level.block.state.BlockState
+import net.minecraftforge.common.capabilities.Capability
+import net.minecraftforge.common.capabilities.ForgeCapabilities
+import net.minecraftforge.common.util.LazyOptional
+import net.minecraftforge.energy.IEnergyStorage
+import net.minecraftforge.items.IItemHandler
+import net.minecraftforge.items.ItemStackHandler
+
+/**
+ * Base class for all crafting machines.
+ * Note that this is only for basic crafting machines, ie, those that turn one thing into one other thing.
+ */
+@Suppress("MemberVisibilityCanBePrivate")
+abstract class CraftingMachine(
+    pType: BlockEntityType<*>,
+    pPos: BlockPos,
+    pBlockState: BlockState
+) : BlockEntity(
+    pType,
+    pPos,
+    pBlockState
+) {
+
+    abstract val inventoryNbtKey: String
+
+    val simpleContainerSize = 2
+    val itemstackHandlerSize = 3
+
+    val inputSlot = 0
+    val outputSlot = 1
+    val overclockSlot = 2
+
+    abstract val energyNbtKey: String
+    abstract val energyMaximum: Int
+    abstract val energyTransferMaximum: Int
+    abstract val energyPerTick: Int
+
+    abstract val itemHandler: ItemStackHandler
+
+    protected var lazyItemHandler: LazyOptional<IItemHandler> = LazyOptional.empty()
+
+    protected val energyStorage: ModEnergyStorage = object : ModEnergyStorage(energyMaximum, energyTransferMaximum) {
+        override fun onEnergyChanged() {
+            setChanged()
+
+            ModPacketHandler.messageAllPlayers(EnergySyncPacket(energy, blockPos))
+        }
+    }
+
+    protected var lazyEnergyStorage: LazyOptional<IEnergyStorage> = LazyOptional.empty()
+
+    override fun <T : Any?> getCapability(cap: Capability<T>, side: Direction?): LazyOptional<T> {
+        return when (cap) {
+            ForgeCapabilities.ITEM_HANDLER -> getItemCapability(side).cast()
+
+            ForgeCapabilities.ENERGY -> getEnergyCapability(side).cast()
+
+            else -> super.getCapability(cap, side)
+        }
+    }
+
+    protected fun getItemCapability(side: Direction?): LazyOptional<IItemHandler> {
+        return lazyItemHandler.cast()
+    }
+
+    protected fun getEnergyCapability(side: Direction?): LazyOptional<IEnergyStorage> {
+        return lazyEnergyStorage.cast()
+    }
+
+    fun getEnergyStorage(): IEnergyStorage = energyStorage
+
+    override fun onLoad() {
+        super.onLoad()
+        lazyItemHandler = LazyOptional.of { itemHandler }
+        lazyEnergyStorage = LazyOptional.of { energyStorage }
+    }
+
+    override fun invalidateCaps() {
+        super.invalidateCaps()
+        lazyItemHandler.invalidate()
+        lazyEnergyStorage.invalidate()
+    }
+
+    override fun saveAdditional(pTag: CompoundTag) {
+        pTag.put(inventoryNbtKey, itemHandler.serializeNBT())
+        pTag.putInt(energyNbtKey, energyStorage.energyStored)
+        super.saveAdditional(pTag)
+    }
+
+    override fun load(pTag: CompoundTag) {
+        super.load(pTag)
+        itemHandler.deserializeNBT(pTag.getCompound(inventoryNbtKey))
+        energyStorage.setEnergy(pTag.getInt(energyNbtKey))
+    }
+
+    fun drops() {
+        val inventory = SimpleContainer(itemHandler.slots)
+        for (i in 0 until itemHandler.slots) {
+            inventory.setItem(i, itemHandler.getStackInSlot(i))
+        }
+
+        Containers.dropContents(this.level!!, this.blockPos, inventory)
+    }
+
+    protected var progress = 0
+    protected var maxProgress = 20 * 4
+    protected fun resetProgress() {
+        progress = 0
+    }
+
+    // Client only
+    fun setEnergy(energy: Int) {
+        this.energyStorage.setEnergy(energy)
+    }
+
+    protected val containerData = object : ContainerData {
+
+        private val PROGRESS_INDEX = 0
+        private val MAX_PROGRESS_INDEX = 1
+
+        override fun get(pIndex: Int): Int {
+            return when (pIndex) {
+                PROGRESS_INDEX -> progress
+                MAX_PROGRESS_INDEX -> maxProgress
+                else -> 0
+            }
+        }
+
+        override fun set(pIndex: Int, pValue: Int) {
+            when (pIndex) {
+                PROGRESS_INDEX -> progress = pValue
+                MAX_PROGRESS_INDEX -> maxProgress = pValue
+            }
+        }
+
+        override fun getCount(): Int {
+            return simpleContainerSize
+        }
+    }
+
+}
