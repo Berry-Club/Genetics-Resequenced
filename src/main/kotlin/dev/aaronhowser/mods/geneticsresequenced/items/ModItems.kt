01595ce1ee31d9859e7d8124dbaf486eac0dc670
Aaron Howser <aaronhowser1@gmail.com>
2024-04-15 18:45:13 -0500

    rearranged item registry

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/items/ModItems.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/items/ModItems.kt
@@ -19,13 +19,14 @@ object ModItems {
     val ITEM_REGISTRY: DeferredRegister<Item> =
         DeferredRegister.create(ForgeRegistries.ITEMS, GeneticsResequenced.ID)
 
-    val DRAGON_HEALTH_CRYSTAL by ITEM_REGISTRY.registerObject("dragon_health_crystal") { DragonHealthCrystal }
-    val ANTI_FIELD_ORB by ITEM_REGISTRY.registerObject("anti_field_orb") { AntiFieldOrbItem }
     val SCRAPER by ITEM_REGISTRY.registerObject("scraper") { ScraperItem }
     val SYRINGE by ITEM_REGISTRY.registerObject("syringe") { SyringeItem }
     val ORGANIC_MATTER by ITEM_REGISTRY.registerObject("organic_matter") { EntityDnaItem() }
     val CELL by ITEM_REGISTRY.registerObject("cell") { EntityDnaItem() }
     val DNA_HELIX by ITEM_REGISTRY.registerObject("dna_helix") { DnaHelixItem }
+    val PLASMID by ITEM_REGISTRY.registerObject("plasmid") { PlasmidItem }
     val OVERCLOCKER by ITEM_REGISTRY.registerObject("overclocker") { Item(Item.Properties().tab(MOD_TAB).stacksTo(8)) }
+    val ANTI_FIELD_ORB by ITEM_REGISTRY.registerObject("anti_field_orb") { AntiFieldOrbItem }
+    val DRAGON_HEALTH_CRYSTAL by ITEM_REGISTRY.registerObject("dragon_health_crystal") { DragonHealthCrystal }
 
 }
