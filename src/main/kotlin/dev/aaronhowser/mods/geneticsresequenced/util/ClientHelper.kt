580ea70b09ee06936aea253d22f71aa257ea17ee
Aaron Howser <aaronhowser1@gmail.com>
2024-04-30 01:42:28 -0500

    Readded reflection because I'm feeling particularly sexy today

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/util/ClientHelper.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/util/ClientHelper.kt
@@ -3,36 +3,54 @@ package dev.aaronhowser.mods.geneticsresequenced.util
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
 import dev.aaronhowser.mods.geneticsresequenced.default_genes.behavior.ClickGenes.recentlySheered
 import net.minecraft.client.Minecraft
+import net.minecraft.client.player.LocalPlayer
 import net.minecraft.world.entity.player.PlayerModelPart
+import net.minecraftforge.fml.util.ObfuscationReflectionHelper
 
 object ClientHelper {
 
-    fun playerIsCreative(): Boolean = Minecraft.getInstance().player?.isCreative == true
-    fun playerIsSpectator(): Boolean = Minecraft.getInstance().player?.isSpectator == true
+    private val localPlayer: LocalPlayer? = Minecraft.getInstance().player
+
+    fun playerIsCreative(): Boolean = localPlayer?.isCreative == true
+    fun playerIsSpectator(): Boolean = localPlayer?.isSpectator == true
+
+    private val options = Minecraft.getInstance().options
+
+    private val modelPartsField = ObfuscationReflectionHelper.findField(
+        options::class.java,
+        "f_92108_"  // "modelParts"
+    )
+
+    private var removedSkinLayers: Set<PlayerModelPart> = emptySet()
 
     fun shearPlayerSkin() {
-        val options = Minecraft.getInstance().options
-        val modelParts: Set<PlayerModelPart> = PlayerModelPart.values().toSet()
+        try {
+            @Suppress("UNCHECKED_CAST")
+            val enabledModelParts = (modelPartsField.get(options) as Set<PlayerModelPart>).toSet()
+            for (part in enabledModelParts) {
+                options.toggleModelPart(part, false)
+            }
 
-        for (part in modelParts) {
-            options.toggleModelPart(part, false)
+            removedSkinLayers = enabledModelParts
+
+            GeneticsResequenced.LOGGER.info("Sheared layers off player skin: ${removedSkinLayers.joinToString(", ")}")
+
+            val addLayersBackTask = { addSkinLayersBack() }
+
+            recentlySheered.cooldownEndedTasks.add(addLayersBackTask)
+
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-
-        GeneticsResequenced.LOGGER.info("Sheared player skin")
-
-        val addLayersBackTask = { addSkinLayersBack() }
-        recentlySheered.cooldownEndedTasks.add(addLayersBackTask)
     }
 
     fun addSkinLayersBack() {
-        val options = Minecraft.getInstance().options
-        val modelParts: Set<PlayerModelPart> = PlayerModelPart.values().toSet()
-
-        for (part in modelParts) {
+        for (part in removedSkinLayers) {
             options.toggleModelPart(part, true)
         }
 
-        GeneticsResequenced.LOGGER.info("Added layers back to player skin")
+        GeneticsResequenced.LOGGER.info("Added layers back to player skin: ${removedSkinLayers.joinToString(", ")}")
+        removedSkinLayers = emptySet()
     }
 
 }
