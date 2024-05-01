c8d5a00b1f492e782a04b353506b4d68a5f44759
Aaron Howser <aaronhowser1@gmail.com>
2024-05-01 02:42:06 -0500

    Trying to use access transformers

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/util/ClientHelper.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/util/ClientHelper.kt
@@ -3,6 +3,7 @@ package dev.aaronhowser.mods.geneticsresequenced.util
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
 import dev.aaronhowser.mods.geneticsresequenced.default_genes.behavior.ClickGenes.recentlySheered
 import net.minecraft.client.Minecraft
+import net.minecraft.client.Options
 import net.minecraft.client.player.LocalPlayer
 import net.minecraft.world.entity.player.PlayerModelPart
 import net.minecraftforge.fml.util.ObfuscationReflectionHelper
@@ -14,19 +15,13 @@ object ClientHelper {
     fun playerIsCreative(): Boolean = localPlayer?.isCreative == true
     fun playerIsSpectator(): Boolean = localPlayer?.isSpectator == true
 
-    private val options = Minecraft.getInstance().options
-
-    private val modelPartsField = ObfuscationReflectionHelper.findField(
-        options::class.java,
-        "f_92108_"  // "modelParts"
-    )
+    private val options: Options = Minecraft.getInstance().options
 
     private var removedSkinLayers: Set<PlayerModelPart> = emptySet()
 
     fun shearPlayerSkin() {
         try {
-            @Suppress("UNCHECKED_CAST")
-            val enabledModelParts = (modelPartsField.get(options) as Set<PlayerModelPart>).toSet()
+            val enabledModelParts = options.modelParts
             for (part in enabledModelParts) {
                 options.toggleModelPart(part, false)
             }
