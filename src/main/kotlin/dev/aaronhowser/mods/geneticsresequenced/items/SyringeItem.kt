ce2930eec0958e2670a38bdb6cc4e521a112f7ed
Aaron Howser <aaronhowser1@gmail.com>
2024-05-02 20:23:42 -0500

    More progress with SyringeItem

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/items/SyringeItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/items/SyringeItem.kt
@@ -1,12 +1,19 @@
 package dev.aaronhowser.mods.geneticsresequenced.items
 
 import dev.aaronhowser.mods.geneticsresequenced.util.OtherUtil.getUuidOrNull
+import dev.aaronhowser.mods.geneticsresequenced.util.OtherUtil.withColor
+import net.minecraft.ChatFormatting
+import net.minecraft.network.chat.Component
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResultHolder
+import net.minecraft.world.damagesource.DamageSource
+import net.minecraft.world.effect.MobEffectInstance
+import net.minecraft.world.effect.MobEffects
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.TooltipFlag
 import net.minecraft.world.item.UseAnim
 import net.minecraft.world.level.Level
 import net.minecraftforge.common.util.FakePlayer
@@ -31,12 +38,21 @@ object SyringeItem : Item(
         val tag = syringeStack.getOrCreateTag()
 
         tag.putUUID(ENTITY_UUID_NBT_KEY, entity.uuid)
+        setContaminated(syringeStack, true)
     }
 
     private fun injectEntity(syringeStack: ItemStack, entity: LivingEntity) {
         val entityDna = getEntity(syringeStack) ?: return
         if (entity.uuid != entityDna) return
+
+        if (isContaminated(syringeStack)) {
+            entity.addEffect(MobEffectInstance(MobEffects.CONFUSION, 200))
+            entity.addEffect(MobEffectInstance(MobEffects.WITHER, 200))
+            setContaminated(syringeStack, false)
+        }
+
         syringeStack.getOrCreateTag().remove(ENTITY_UUID_NBT_KEY)
+
     }
 
     private fun getEntity(syringeStack: ItemStack): UUID? {
@@ -47,6 +63,15 @@ object SyringeItem : Item(
 
     fun hasBlood(syringeStack: ItemStack): Boolean {
         return getEntity(syringeStack) != null
+    }
+
+    private const val CONTAMINATED_NBT_KEY = "contaminated"
+    fun isContaminated(syringeStack: ItemStack): Boolean {
+        return syringeStack.getOrCreateTag().getBoolean(CONTAMINATED_NBT_KEY)
+    }
+
+    fun setContaminated(syringeStack: ItemStack, contaminated: Boolean) {
+        syringeStack.getOrCreateTag().putBoolean(CONTAMINATED_NBT_KEY, contaminated)
     }
 
     override fun getUseDuration(pStack: ItemStack): Int = 40
@@ -79,9 +104,35 @@ object SyringeItem : Item(
             injectEntity(pStack, pLivingEntity)
         } else {
             setEntity(pStack, pLivingEntity)
+            pLivingEntity.hurt(DamageSource.WITHER, 1f)
         }
 
         pLivingEntity.cooldowns.addCooldown(this, 10)
+    }
+
+    override fun getName(pStack: ItemStack): Component {
+        return if (hasBlood(pStack)) {
+            Component.translatable("item.geneticsresequenced.syringe.full")
+        } else {
+            super.getName(pStack)
+        }
+    }
+
+    override fun appendHoverText(
+        pStack: ItemStack,
+        pLevel: Level?,
+        pTooltipComponents: MutableList<Component>,
+        pIsAdvanced: TooltipFlag
+    ) {
+
+        if (isContaminated(pStack)) {
+            pTooltipComponents.add(
+                Component.translatable("tooltip.geneticsresequenced.syringe.contaminated")
+                    .withColor(ChatFormatting.DARK_GREEN)
+            )
+        }
+
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced)
     }
 
 }
