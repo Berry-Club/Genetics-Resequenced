d5924a951df4fa31e920710a5a783d52be3e6894
Aaron Howser <aaronhowser1@gmail.com>
2024-05-19 08:07:07 -0500

    Revert "Trying to improve the thing but filed"
    This reverts commit 7fd5c72f96d9b428e48903e898c84fd6e1333311.

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/potions/ModPotions.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/potions/ModPotions.kt
@@ -2,7 +2,6 @@ package dev.aaronhowser.mods.geneticsresequenced.potions
 
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
 import dev.aaronhowser.mods.geneticsresequenced.potions.mob_effects.ModEffects
-import net.minecraft.world.effect.MobEffect
 import net.minecraft.world.effect.MobEffectInstance
 import net.minecraft.world.item.alchemy.Potion
 import net.minecraftforge.registries.DeferredRegister
@@ -15,20 +14,19 @@ object ModPotions {
     val POTION_REGISTRY: DeferredRegister<Potion> =
         DeferredRegister.create(ForgeRegistries.POTIONS, GeneticsResequenced.ID)
 
-    //TODO: Remove splash, lingering, and arrow potions (apparently needs mixins >:( )
+    //TODO: Remove splash, lingering, and arrow potions
     //TODO: Recipes
-    val SUBSTRATE by register("substrate", ModEffects.SUBSTRATE)
-    val CELL_GROWTH by register("cell_growth", ModEffects.CELL_GROWTH)
-    val MUTATION by register("mutation", ModEffects.MUTATION)
-    val VIRAL_AGENTS by register("viral_agents", ModEffects.VIRAL_AGENTS)
-    val THE_CURE by register("the_cure", ModEffects.THE_CURE)
+    val SUBSTRATE by register("substrate") { Potion("substrate", MobEffectInstance(ModEffects.SUBSTRATE)) }
+    val CELL_GROWTH by register("cell_growth") { Potion("cell_growth", MobEffectInstance(ModEffects.CELL_GROWTH)) }
+    val MUTATION by register("mutation") { Potion("mutation", MobEffectInstance(ModEffects.MUTATION)) }
+    val VIRAL_AGENTS by register("viral_agents") { Potion("viral_agents", MobEffectInstance(ModEffects.VIRAL_AGENTS)) }
+    val THE_CURE by register("the_cure") { Potion("the_cure", MobEffectInstance(ModEffects.THE_CURE)) }
 
     private fun register(
         name: String,
-        effect: MobEffect
+        supplier: () -> Potion
     ): ObjectHolderDelegate<Potion> {
-        val potion = POTION_REGISTRY
-            .registerObject(name) { Potion(GeneticsResequenced.ID + "." + name, MobEffectInstance(effect)) }
+        val potion = POTION_REGISTRY.registerObject(name, supplier)
         return potion
     }
 
