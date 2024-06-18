696661d32dec49fd66611c3a5168ec2d39b1f48a
Aaron Howser <aaronhowser1@gmail.com>
2024-06-18 15:07:24 -0500

    Revert "Trying to use skull renderer"
    This reverts commit dbda270e1d8ea45980ec96337e5ec7ca2cb776ea.

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/entities/client/SupportSlimeRenderer.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/entities/client/SupportSlimeRenderer.kt
@@ -1,26 +1,19 @@
 package dev.aaronhowser.mods.geneticsresequenced.entities.client
 
-import com.mojang.authlib.GameProfile
-import com.mojang.authlib.minecraft.MinecraftProfileTexture
 import com.mojang.blaze3d.vertex.PoseStack
 import dev.aaronhowser.mods.geneticsresequenced.entities.SupportSlime
-import net.minecraft.client.Minecraft
 import net.minecraft.client.model.SlimeModel
 import net.minecraft.client.model.geom.ModelLayers
 import net.minecraft.client.player.AbstractClientPlayer
 import net.minecraft.client.renderer.MultiBufferSource
-import net.minecraft.client.renderer.RenderType
 import net.minecraft.client.renderer.entity.EntityRendererProvider
 import net.minecraft.client.renderer.entity.MobRenderer
 import net.minecraft.client.renderer.entity.SlimeRenderer
 import net.minecraft.client.renderer.entity.layers.SlimeOuterLayer
-import net.minecraft.client.resources.DefaultPlayerSkin
-import net.minecraft.core.UUIDUtil
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.util.Mth
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.api.distmarker.OnlyIn
-import java.util.*
 
 //TODO: What if it looks like your skin's head???
 @OnlyIn(Dist.CLIENT)
@@ -38,13 +31,11 @@ class SupportSlimeRenderer(
         this.addLayer(SlimeOuterLayer(this, context.modelSet))
     }
 
-    private var ownerUuid: UUID? = null
     private var skinRl: ResourceLocation? = null
     private fun setSkinRl(pEntity: SupportSlime) {
         if (skinRl != null) return
 
         val getOwnerUuid = pEntity.getOwnerUuid() ?: return
-        ownerUuid = getOwnerUuid
         val player = pEntity.level.getPlayerByUUID(getOwnerUuid) ?: return
 
         if (player is AbstractClientPlayer) {
@@ -76,52 +67,19 @@ class SupportSlimeRenderer(
         pMatrixStack: PoseStack,
         pPartialTickTime: Float
     ) {
+        val f = 0.999f
+        pMatrixStack.scale(0.999f, 0.999f, 0.999f)
+        pMatrixStack.translate(0.0, 0.001, 0.0)
         val f1 = pLivingEntity.size.toFloat()
         val f2 = Mth.lerp(pPartialTickTime, pLivingEntity.oSquish, pLivingEntity.squish) / (f1 * 0.5f + 1.0f)
         val f3 = 1.0f / (f2 + 1.0f)
         pMatrixStack.scale(f3 * f1, 1.0f / f3 * f1, f3 * f1)
     }
 
-    override fun getRenderType(
-        pLivingEntity: SupportSlime,
-        pBodyVisible: Boolean,
-        pTranslucent: Boolean,
-        pGlowing: Boolean
-    ): RenderType? {
-
-        val ownerPlayer = ownerUuid?.let { pLivingEntity.level.getPlayerByUUID(it) } ?: return super.getRenderType(
-            pLivingEntity,
-            pBodyVisible,
-            pTranslucent,
-            pGlowing
-        )
-
-        val skinManager = Minecraft.getInstance().skinManager
-
-        val ownerProfile: GameProfile = ownerPlayer.gameProfile
-        val map = skinManager.getInsecureSkinInformation(ownerProfile)
-
-        return if (map.containsKey(MinecraftProfileTexture.Type.SKIN)) {
-            RenderType.entityTranslucent(
-                skinManager.registerTexture(
-                    map[MinecraftProfileTexture.Type.SKIN],
-                    MinecraftProfileTexture.Type.SKIN
-                )
-            )
-        } else {
-            RenderType.entityCutoutNoCull(
-                DefaultPlayerSkin.getDefaultSkin(
-                    UUIDUtil.getOrCreatePlayerUUID(ownerProfile)
-                )
-            )
-        }
-
-    }
-
     /**
      * Returns the location of an entity's texture.
      */
     override fun getTextureLocation(pEntity: SupportSlime): ResourceLocation {
-        return skinRl ?: SlimeRenderer.SLIME_LOCATION
+        return SlimeRenderer.SLIME_LOCATION
     }
 }
