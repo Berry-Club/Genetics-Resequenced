c37777f16e59b00d80ff3bf8c61b3a55cda32c9b
Aaron Howser <aaronhowser1@gmail.com>
2024-06-20 20:27:50 -0500

    util

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/util/OtherUtil.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/util/OtherUtil.kt
@@ -1,32 +1,45 @@
 package dev.aaronhowser.mods.geneticsresequenced.util
 
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
-import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.ChatFormatting
+import net.minecraft.network.chat.MutableComponent
 import net.minecraft.resources.ResourceLocation
-import net.minecraft.world.entity.EntityType
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.ai.targeting.TargetingConditions
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
+import java.util.*
 
 object OtherUtil {
 
     fun modResource(path: String): ResourceLocation =
         ResourceLocation.fromNamespaceAndPath(GeneticsResequenced.ID, path)
 
-    fun getEntityType(resourceLocation: ResourceLocation): EntityType<*> {
-
-        val entityType = BuiltInRegistries.ENTITY_TYPE.get(resourceLocation)
-
-        if (entityType == EntityType.PIG && resourceLocation != getEntityResourceLocation(EntityType.PIG)) {
-            throw IllegalArgumentException("Unknown entity type: $resourceLocation")
-        }
-
-        return entityType
-    }
-
-    fun getEntityResourceLocation(entityType: EntityType<*>): ResourceLocation {
-        return BuiltInRegistries.ENTITY_TYPE.getKey(entityType)
-    }
-
     val Item.itemStack: ItemStack
         get() = this.defaultInstance
+
+    fun MutableComponent.withColor(color: ChatFormatting): MutableComponent = this.withStyle { it.withColor(color) }
+
+    private val entityUuidMap: MutableMap<UUID, LivingEntity> = mutableMapOf()
+    fun getNearbyEntityFromUuid(uuid: UUID, searchAroundEntity: LivingEntity): LivingEntity? {
+        val mappedValue = entityUuidMap[uuid]
+        if (mappedValue != null) return mappedValue
+
+        val nearbyEntities = searchAroundEntity.level().getNearbyEntities(
+            LivingEntity::class.java,
+            TargetingConditions.DEFAULT,
+            searchAroundEntity,
+            searchAroundEntity.boundingBox.inflate(50.0)
+        )
+
+        for (entity in nearbyEntities) {
+            if (entity.uuid == uuid) {
+                entityUuidMap[uuid] = entity
+                return entity
+            }
+        }
+
+        return null
+    }
+
 }
