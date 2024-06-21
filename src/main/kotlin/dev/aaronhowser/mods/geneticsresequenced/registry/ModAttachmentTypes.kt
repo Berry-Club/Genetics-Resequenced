3fda04f2bc03ecf21844aff86fd14ec5a12f6bd1
Aaron Howser <aaronhowser1@gmail.com>
2024-06-21 12:45:43 -0500

    undo the get

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/ModAttachmentTypes.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/ModAttachmentTypes.kt
@@ -3,6 +3,7 @@ package dev.aaronhowser.mods.geneticsresequenced.registry
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
 import dev.aaronhowser.mods.geneticsresequenced.api.capability.genes.GeneContainer
 import net.neoforged.neoforge.attachment.AttachmentType
+import net.neoforged.neoforge.registries.DeferredHolder
 import net.neoforged.neoforge.registries.DeferredRegister
 import net.neoforged.neoforge.registries.NeoForgeRegistries
 import java.util.function.Supplier
@@ -12,12 +13,12 @@ object ModAttachmentTypes {
     val ATTACHMENT_TYPES: DeferredRegister<AttachmentType<*>> =
         DeferredRegister.create(NeoForgeRegistries.ATTACHMENT_TYPES, GeneticsResequenced.ID)
 
-    val GENE_CONTAINER: AttachmentType<GeneContainer> =
+    val GENE_CONTAINER: DeferredHolder<AttachmentType<*>, AttachmentType<GeneContainer>> =
         ATTACHMENT_TYPES.register("genes", Supplier {
             AttachmentType
                 .builder(Supplier { GeneContainer() })
                 .serialize(GeneContainer.CODEC)
                 .build()
-        }).get()
+        })
 
 }
