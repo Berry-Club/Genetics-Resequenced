363e1c754376ee87435cd560657bd58a2fc732c6
Aaron Howser <aaronhowser1@gmail.com>
2024-06-21 13:01:23 -0500

    Works?

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/GeneRegistry.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/GeneRegistry.kt
@@ -1,22 +1,20 @@
 package dev.aaronhowser.mods.geneticsresequenced.registry
 
-import dev.aaronhowser.mods.geneticsresequenced.api.capability.genes.Gene
 import dev.aaronhowser.mods.geneticsresequenced.api.capability.genes.GeneBuilder
 import dev.aaronhowser.mods.geneticsresequenced.util.OtherUtil
-import net.minecraft.core.Registry
-import net.minecraft.resources.ResourceKey
-import net.neoforged.neoforge.registries.RegistryBuilder
 
 //TODO: Do this eventually
 object GeneRegistry {
 
-    val GENE_REGISTRY_KEY: ResourceKey<Registry<Gene>> =
-        ResourceKey.createRegistryKey(OtherUtil.modResource("genes"))
+//    val GENE_REGISTRY_KEY: ResourceKey<Registry<Gene>> =
+//        ResourceKey.createRegistryKey(OtherUtil.modResource("genes"))
+//
+//    val GENE_REGISTRY: Registry<Gene> = RegistryBuilder(GENE_REGISTRY_KEY)
+//        .sync(true)
+//        .create()
 
-    val GENE_REGISTRY: Registry<Gene> = RegistryBuilder(GENE_REGISTRY_KEY)
-        .sync(true)
-        .create()
 
+    fun registerGenes() {}
 
     val testGene = GeneBuilder(OtherUtil.modResource("test_gene"))
         .build()
