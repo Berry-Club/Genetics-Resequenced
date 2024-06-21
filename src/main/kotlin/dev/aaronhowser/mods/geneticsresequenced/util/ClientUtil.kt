463af9105960c541aa557fdaf2cb0051c1dc1c09
Aaron Howser <aaronhowser1@gmail.com>
2024-06-20 20:29:02 -0500

    ClientUtil

---
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/util/ClientUtil.kt
@@ -0,0 +1,17 @@
+package dev.aaronhowser.mods.geneticsresequenced.util
+
+import net.minecraft.client.Minecraft
+import net.minecraft.client.Options
+import net.minecraft.client.player.LocalPlayer
+import net.minecraft.world.entity.player.PlayerModelPart
+
+object ClientUtil {
+
+    val localPlayer: LocalPlayer?
+        get() = Minecraft.getInstance().player
+
+    private val options: Options = Minecraft.getInstance().options
+
+    private var removedSkinLayers: Set<PlayerModelPart> = emptySet()
+
+}
