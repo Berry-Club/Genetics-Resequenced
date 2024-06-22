b80e8ec6266c0e3c97c05c4ef5be94c8e829f0d2
Aaron Howser <aaronhowser1@gmail.com>
2024-06-21 19:17:34 -0500

    Client Config

---
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/config/ClientConfig.kt
@@ -0,0 +1,53 @@
+package dev.aaronhowser.mods.geneticsresequenced.config
+
+import net.neoforged.neoforge.common.ModConfigSpec
+import net.neoforged.neoforge.common.ModConfigSpec.BooleanValue
+import org.apache.commons.lang3.tuple.Pair
+
+class ClientConfig(
+    private val builder: ModConfigSpec.Builder
+) {
+
+    companion object {
+
+        val configPair: Pair<ClientConfig, ModConfigSpec> = ModConfigSpec.Builder().configure(::ClientConfig)
+
+        val CONFIG: ClientConfig = configPair.left
+        val CONFIG_SPEC: ModConfigSpec = configPair.right
+
+    }
+
+    lateinit var woolyRemovesCape: BooleanValue
+    lateinit var disableParrotNarrator: BooleanValue
+    lateinit var disableCringeLangChange: BooleanValue
+    lateinit var supportSlimeRenderDebug: BooleanValue
+
+    init {
+        generalConfigs()
+
+        builder.build()
+    }
+
+    private fun generalConfigs() {
+        builder.push("general")
+
+        woolyRemovesCape = builder
+            .comment("When a player with the Wooly gene is sheared, their outer skin layers are removed. Enable this to also remove the cape.")
+            .define("woolyRemovesCape", true)
+
+        disableParrotNarrator = builder
+            .comment("Disables the narrator when a player with the Parrot gene speaks.")
+            .define("disableParrotNarrator", false)
+
+        disableCringeLangChange = builder
+            .comment("Disables the change to LOLCAT language when you get the Cringe gene. This comes with a resource-reload, which may cause lag.")
+            .define("disableCringeLangChange", false)
+
+        supportSlimeRenderDebug = builder
+            .comment("Enable to render the base Slime model for Support Slime entities.")
+            .define("supportSlimeRenderDebug", false)
+
+        builder.pop()
+    }
+
+}
