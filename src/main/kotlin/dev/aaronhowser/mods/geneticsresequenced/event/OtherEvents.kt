ee603587421611f7964890532d0d92b05e7505b9
Aaron Howser <aaronhowser1@gmail.com>
2024-06-21 19:53:44 -0500

    ModScheduler

---
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/event/OtherEvents.kt
@@ -0,0 +1,19 @@
+package dev.aaronhowser.mods.geneticsresequenced.event
+
+import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
+import dev.aaronhowser.mods.geneticsresequenced.util.ModScheduler
+import net.neoforged.bus.api.SubscribeEvent
+import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.neoforge.event.tick.ServerTickEvent
+
+@EventBusSubscriber(
+    modid = GeneticsResequenced.ID
+)
+object OtherEvents {
+
+    @SubscribeEvent
+    fun onServerTick(event: ServerTickEvent.Post) {
+        ModScheduler.currentTick++
+    }
+
+}
