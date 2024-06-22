fbc30b9dd6f4794b2c0efb16708fc593b7c47814
Aaron Howser <aaronhowser1@gmail.com>
2024-06-21 23:49:56 -0500

    ClientModBusEvents

---
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/event/ClientModBusEvents.kt
@@ -0,0 +1,71 @@
+package dev.aaronhowser.mods.geneticsresequenced.event
+
+import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
+import dev.aaronhowser.mods.geneticsresequenced.control.ModKeyMappings
+import dev.aaronhowser.mods.geneticsresequenced.registry.ModItems
+import dev.aaronhowser.mods.geneticsresequenced.util.OtherUtil
+import net.minecraft.client.renderer.entity.EntityRenderers
+import net.minecraft.client.renderer.item.ItemProperties
+import net.minecraft.client.renderer.item.ItemPropertyFunction
+import net.neoforged.api.distmarker.Dist
+import net.neoforged.bus.api.SubscribeEvent
+import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
+import net.neoforged.neoforge.client.event.ModelEvent
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent
+
+@EventBusSubscriber(
+    modid = GeneticsResequenced.ID,
+    bus = EventBusSubscriber.Bus.MOD,
+    value = [Dist.CLIENT]
+)
+object ClientModBusEvents {
+
+    @SubscribeEvent
+    fun onKeyRegister(event: RegisterKeyMappingsEvent) {
+        event.register(ModKeyMappings.DRAGONS_BREATH)
+        event.register(ModKeyMappings.TELEPORT)
+    }
+
+    //TODO
+    @SubscribeEvent
+    fun onClientSetup(event: FMLClientSetupEvent) {
+//        ModMenuTypes.registerScreens()
+        registerEntityRenderers()
+    }
+
+
+    private fun registerEntityRenderers() {
+//        EntityRenderers.register(ModEntityTypes.SUPPORT_SLIME.get(), ::SupportSlimeRenderer)
+    }
+
+    @SubscribeEvent
+    fun onModelRegistry(event: ModelEvent.RegisterAdditional) {
+
+        ItemProperties.register(
+            ModItems.SYRINGE.get(),
+            OtherUtil.modResource("full"),
+            ItemPropertyFunction { stack, _, _, _ ->
+                if (SyringeItem.hasBlood(stack)) 1f else 0f
+            }
+        )
+
+        ItemProperties.register(
+            ModItems.SYRINGE.get(),
+            OtherUtil.modResource("injecting"),
+            ItemPropertyFunction { stack, _, entity, _ ->
+                if (SyringeItem.isBeingUsed(stack, entity)) 1f else 0f
+            }
+        )
+
+        ItemProperties.register(
+            ModItems.METAL_SYRINGE.get(),
+            OtherUtil.modResource("full"),
+            ItemPropertyFunction { stack, _, _, _ ->
+                if (SyringeItem.hasBlood(stack)) 1f else 0f
+            }
+        )
+
+    }
+
+}
