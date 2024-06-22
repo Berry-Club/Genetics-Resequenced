1860b6053a8c4d4930ef6c85bbf31a41f7989a5d
Aaron Howser <aaronhowser1@gmail.com>
2024-06-21 23:25:21 -0500

    Added InventoryListener

---
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/event/CustomEvents.kt
@@ -0,0 +1,23 @@
+package dev.aaronhowser.mods.geneticsresequenced.event
+
+import dev.aaronhowser.mods.geneticsresequenced.api.capability.genes.Gene
+import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.item.ItemStack
+import net.neoforged.bus.api.Event
+
+object CustomEvents {
+
+    data class PlayerInventoryChangeEvent(
+        val player: ServerPlayer,
+        val slot: Int,
+        val stack: ItemStack
+    ) : Event()
+
+    data class GeneChangeEvent(
+        val entity: LivingEntity,
+        val gene: Gene,
+        val wasAdded: Boolean
+    ) : Event()
+
+}
