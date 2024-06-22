e586e6676d73e30d08bd52929c7efc1e032cd4bb
Aaron Howser <aaronhowser1@gmail.com>
2024-06-21 21:06:12 -0500

    more events

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/event/entity/OtherEntityEvents.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/event/entity/OtherEntityEvents.kt
@@ -1,9 +1,11 @@
 package dev.aaronhowser.mods.geneticsresequenced.event.entity
 
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
+import dev.aaronhowser.mods.geneticsresequenced.gene.behavior.DeathGenes
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
-import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent
+import net.neoforged.neoforge.event.level.ExplosionEvent
 
 @EventBusSubscriber(
     modid = GeneticsResequenced.ID
@@ -11,7 +13,15 @@ import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent
 object OtherEntityEvents {
 
     @SubscribeEvent
-    fun onSpawn(event: EntityJoinLevelEvent) {
+    fun onLivingDeath(event: LivingDeathEvent) {
+        DeathGenes.handleEmeraldHeart(event)
+        DeathGenes.handleExplosiveExit(event)
+//        DeathGenes.handleSlimyDeath(event)
+    }
+
+    @SubscribeEvent
+    fun onDetonate(event: ExplosionEvent.Detonate) {
+        DeathGenes.explosiveExitDetonation(event)
     }
 
 }
