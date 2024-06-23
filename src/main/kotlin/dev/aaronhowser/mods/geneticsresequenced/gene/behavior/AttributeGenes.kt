b5194c542ae14bae65392d932c033f2880555e9d
Aaron Howser <aaronhowser1@gmail.com>
2024-06-23 00:23:32 -0500

    more hearts

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/gene/behavior/AttributeGenes.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/gene/behavior/AttributeGenes.kt
@@ -3,6 +3,7 @@ package dev.aaronhowser.mods.geneticsresequenced.gene.behavior
 import dev.aaronhowser.mods.geneticsresequenced.gene.ModGenes
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModAttributes
 import dev.aaronhowser.mods.geneticsresequenced.util.OtherUtil
+import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.ai.attributes.AttributeModifier
 import net.minecraft.world.entity.ai.attributes.Attributes
 import net.minecraft.world.entity.player.Player
@@ -10,16 +11,16 @@ import net.neoforged.neoforge.event.entity.player.PlayerEvent
 
 object AttributeGenes {
 
-    private val efficiencyModifierRL = OtherUtil.modResource("efficiency")
+    private val efficiencyRL = OtherUtil.modResource("efficiency")
     private val efficiencyAttributeModifier = AttributeModifier(
-        efficiencyModifierRL,
+        efficiencyRL,
         1.0,
         AttributeModifier.Operation.ADD_VALUE
     )
 
-    private val efficiencyFourModifierRL = OtherUtil.modResource("efficiency_four")
+    private val efficiencyFourRl = OtherUtil.modResource("efficiency_four")
     private val efficiencyFourAttributeModifier = AttributeModifier(
-        efficiencyFourModifierRL,
+        efficiencyFourRl,
         3.0, // Because you can't have this without the first level
         AttributeModifier.Operation.ADD_VALUE
     )
@@ -70,5 +71,52 @@ object AttributeGenes {
         }
     }
 
+    private val moreHealthOneRl = OtherUtil.modResource("more_health_one")
+    private val moreHealthOneModifier = AttributeModifier(
+        moreHealthOneRl,
+        20.0,
+        AttributeModifier.Operation.ADD_VALUE
+    )
+
+    private val moreHealthTwoRl = OtherUtil.modResource("more_health_two")
+    private val moreHealthTwoModifier = AttributeModifier(
+        moreHealthTwoRl,
+        20.0,
+        AttributeModifier.Operation.ADD_VALUE
+    )
+
+    fun setMoreHearts(entity: LivingEntity, level: Int, adding: Boolean) {
+        val maxHealthAttribute = entity.getAttribute(Attributes.MAX_HEALTH) ?: return
+
+        when (level) {
+            1 -> if (!ModGenes.moreHearts.isActive) return
+            2 -> if (!ModGenes.moreHeartsTwo.isActive) return
+            else -> throw IllegalArgumentException("Invalid more hearts level: $level")
+        }
+
+        val hasLevelOne = maxHealthAttribute.hasModifier(moreHealthOneRl)
+        val hasLevelTwo = maxHealthAttribute.hasModifier(moreHealthTwoRl)
+
+        when (level) {
+
+            1 -> if (adding) {
+                if (hasLevelOne) return
+                maxHealthAttribute.addPermanentModifier(moreHealthOneModifier)
+            } else {
+                if (!hasLevelOne) return
+                maxHealthAttribute.removeModifier(moreHealthOneModifier)
+            }
+
+            2 -> if (adding) {
+                if (hasLevelTwo) return
+                maxHealthAttribute.addPermanentModifier(moreHealthTwoModifier)
+            } else {
+                if (!hasLevelTwo) return
+                maxHealthAttribute.removeModifier(moreHealthTwoModifier)
+            }
+
+        }
+
+    }
 
 }
