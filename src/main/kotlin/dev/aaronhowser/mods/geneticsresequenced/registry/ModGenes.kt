700f60dfb651b1465810418f52199c94926ddaf6
Aaron Howser <aaronhowser1@gmail.com>
2024-06-22 23:45:18 -0500

    changes to ModGenes

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/ModGenes.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/ModGenes.kt
@@ -4,7 +4,7 @@ import dev.aaronhowser.mods.geneticsresequenced.api.genes.GeneBuilder
 import dev.aaronhowser.mods.geneticsresequenced.util.OtherUtil
 
 //TODO: Do this eventually
-object GeneRegistry {
+object ModGenes {
 
 //    val GENE_REGISTRY_KEY: ResourceKey<Registry<Gene>> =
 //        ResourceKey.createRegistryKey(OtherUtil.modResource("genes"))
@@ -12,12 +12,5 @@ object GeneRegistry {
 //    val GENE_REGISTRY: Registry<Gene> = RegistryBuilder(GENE_REGISTRY_KEY)
 //        .sync(true)
 //        .create()
-
-    fun register() {}
-
-    val testGene = GeneBuilder(OtherUtil.modResource("test_gene"))
-        .build()
-    val another = GeneBuilder(OtherUtil.modResource("another"))
-        .build()
 
 }
