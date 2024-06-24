f97809533ea15d2f93dd7ba007b3360556670146
Aaron Howser <aaronhowser1@gmail.com>
2024-06-24 07:02:03 -0500

    Moved them

---
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/block/base/menu/ScreenTextures.kt
@@ -0,0 +1,30 @@
+package dev.aaronhowser.mods.geneticsresequenced.block.base.menu
+
+import dev.aaronhowser.mods.geneticsresequenced.util.OtherUtil
+
+object ScreenTextures {
+
+    object Backgrounds {
+        val AIR_DISPERSAL = OtherUtil.modResource("textures/gui/container/air_dispersal.png")
+        val BASIC = OtherUtil.modResource("textures/gui/container/basic_machine_bg.png")
+        val CELL_ANALYZER = OtherUtil.modResource("textures/gui/container/cell_analyzer.png")
+        val COAL_GENERATOR = OtherUtil.modResource("textures/gui/container/coal_generator.png")
+        val DNA_DECRYPTOR = OtherUtil.modResource("textures/gui/container/dna_decryptor.png")
+        val DNA_EXTRACTOR = OtherUtil.modResource("textures/gui/container/dna_extractor.png")
+        val INCUBATOR = OtherUtil.modResource("textures/gui/container/incubator.png")
+        val INCUBATOR_ADVANCED = OtherUtil.modResource("textures/gui/container/incubator_advanced.png")
+        val PLASMID_INFUSER = OtherUtil.modResource("textures/gui/container/plasmid_infuser.png")
+        val PLASMID_INJECTOR = OtherUtil.modResource("textures/gui/container/plasmid_injector.png")
+    }
+
+    object Elements {
+        val ARROW_DOWN = OtherUtil.modResource("arrow_down")
+        val ARROW_RIGHT = OtherUtil.modResource("arrow_right")
+        val BUBBLES = OtherUtil.modResource("bubbles")
+        val BURN = OtherUtil.modResource("burn")
+        val ENERGY = OtherUtil.modResource("energy")
+        val HEAT_HIGH = OtherUtil.modResource("heat_high")
+        val HEAT_LOW = OtherUtil.modResource("heat_low")
+    }
+
+}
