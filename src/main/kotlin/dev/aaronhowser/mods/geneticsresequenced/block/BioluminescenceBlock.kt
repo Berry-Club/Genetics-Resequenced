96060eb59c6ddfb74dd58bddb9ba85e90af80149
Aaron Howser <aaronhowser1@gmail.com>
2024-06-23 23:38:05 -0500

    BioluminescenceBlock

---
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/block/BioluminescenceBlock.kt
@@ -0,0 +1,39 @@
+package dev.aaronhowser.mods.geneticsresequenced.block
+
+import dev.aaronhowser.mods.geneticsresequenced.config.ServerConfig
+import net.minecraft.core.BlockPos
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.util.RandomSource
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.AirBlock
+import net.minecraft.world.level.block.state.BlockState
+
+class BioluminescenceBlock :
+    AirBlock(
+        Properties
+            .of()
+            .replaceable()
+            .noCollission()
+            .instabreak()
+            .noLootTable()
+            .air()
+            .lightLevel { 10 }
+    ) {
+
+    override fun onPlace(
+        pState: BlockState,
+        pLevel: Level,
+        pPos: BlockPos,
+        pOldState: BlockState,
+        pMovedByPiston: Boolean
+    ) {
+        pLevel.scheduleTick(pPos, this, ServerConfig.bioluminescenceDuration.get())
+        super.onPlace(pState, pLevel, pPos, pOldState, pMovedByPiston)
+    }
+
+    override fun tick(pState: BlockState, pLevel: ServerLevel, pPos: BlockPos, pRandom: RandomSource) {
+        pLevel.removeBlock(pPos, false)
+        super.tick(pState, pLevel, pPos, pRandom)
+    }
+
+}
--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/ModBlocks.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/ModBlocks.kt
@@ -1,6 +1,7 @@
 package dev.aaronhowser.mods.geneticsresequenced.registry
 
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
+import dev.aaronhowser.mods.geneticsresequenced.block.BioluminescenceBlock
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.state.BlockBehaviour
 import net.neoforged.neoforge.registries.DeferredBlock
@@ -10,7 +11,7 @@ object ModBlocks {
 
     val BLOCK_REGISTRY: DeferredRegister.Blocks = DeferredRegister.createBlocks(GeneticsResequenced.ID)
 
-    val BIOLUMINESCENCE_BLOCK: DeferredBlock<Block> = registerSimpleBlock("bioluminescence_block")
+    val BIOLUMINESCENCE_BLOCK: DeferredBlock<Block> = registerBlock("bioluminescence_block") { BioluminescenceBlock() }
     val COAL_GENERATOR: DeferredBlock<Block> = registerSimpleBlock("coal_generator")
     val CELL_ANALYZER: DeferredBlock<Block> = registerSimpleBlock("cell_analyzer")
     val DNA_EXTRACTOR: DeferredBlock<Block> = registerSimpleBlock("dna_extractor")
@@ -21,6 +22,17 @@ object ModBlocks {
     val INCUBATOR: DeferredBlock<Block> = registerSimpleBlock("incubator")
     val ADVANCED_INCUBATOR: DeferredBlock<Block> = registerSimpleBlock("advanced_incubator")
     val ANTI_FIELD_BLOCK: DeferredBlock<Block> = registerSimpleBlock("anti_field_block")
+
+    private fun registerBlock(
+        name: String,
+        supplier: () -> Block
+    ): DeferredBlock<Block> {
+        val block = BLOCK_REGISTRY.register(name, supplier)
+
+        ModItems.ITEM_REGISTRY.registerSimpleBlockItem(name, block)
+
+        return block
+    }
 
     private fun registerSimpleBlock(
         name: String
