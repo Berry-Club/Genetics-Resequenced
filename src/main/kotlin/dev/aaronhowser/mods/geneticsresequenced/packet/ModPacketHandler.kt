aa5f465f60669b5d2b6d3b447baf60215daa9269
Aaron Howser <aaronhowser1@gmail.com>
2024-06-24 15:31:10 -0500

    packet stuff

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/packet/ModPacketHandler.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/packet/ModPacketHandler.kt
@@ -1,24 +1,47 @@
 package dev.aaronhowser.mods.geneticsresequenced.packet
 
 import dev.aaronhowser.mods.geneticsresequenced.packet.server_to_client.GeneChangedPacket
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.phys.Vec3
+import net.neoforged.neoforge.network.PacketDistributor
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent
 import net.neoforged.neoforge.network.handling.DirectionalPayloadHandler
 
 object ModPacketHandler {
 
     fun registerPayloads(event: RegisterPayloadHandlersEvent) {
-
         val registrar = event.registrar("1")
 
         registrar.playToClient(
             GeneChangedPacket.TYPE,
             GeneChangedPacket.STREAM_CODEC,
             DirectionalPayloadHandler(
-                { packet, context -> ClientPayloadHandler.handleData(packet, context) },
-                { packet, context -> ServerPayloadHandler.handleData(packet, context) }
+                { packet, context -> packet.receiveMessage(context) },
+                { packet, context -> packet.receiveMessage(context) }
             )
         )
+    }
 
+    fun messageNearbyPlayers(packet: ModPacket, serverLevel: ServerLevel, origin: Vec3, radius: Double) {
+        for (player in serverLevel.players()) {
+            val distance = player.distanceToSqr(origin.x(), origin.y(), origin.z())
+            if (distance < radius * radius) {
+                messagePlayer(player, packet)
+            }
+        }
+    }
+
+    fun messagePlayer(player: ServerPlayer, packet: ModPacket) {
+        PacketDistributor.sendToPlayer(player, packet)
+    }
+
+    fun messageAllPlayers(packet: ModPacket) {
+        PacketDistributor.sendToAllPlayers(packet)
+    }
+
+    fun messageServer(packet: ModPacket) {
+        PacketDistributor.sendToServer(packet)
     }
 
 }
