05ac89dfb7c397794b970ef3aa26551053e1cec2
Aaron Howser <aaronhowser1@gmail.com>
2024-06-28 17:42:19 -0500

    Plasmid Injector

---
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/block/machine/plasmid_injector/PlasmidInjectorMenu.kt
@@ -0,0 +1,112 @@
+package dev.aaronhowser.mods.geneticsresequenced.block.machine.plasmid_injector
+
+import dev.aaronhowser.mods.geneticsresequenced.block.base.CraftingMachineBlockEntity
+import dev.aaronhowser.mods.geneticsresequenced.block.base.menu.MachineMenu
+import dev.aaronhowser.mods.geneticsresequenced.block.base.menu.ScreenTextures
+import dev.aaronhowser.mods.geneticsresequenced.datagen.ModLanguageProvider
+import dev.aaronhowser.mods.geneticsresequenced.datagen.ModLanguageProvider.Companion.toComponent
+import dev.aaronhowser.mods.geneticsresequenced.item.SyringeItem
+import dev.aaronhowser.mods.geneticsresequenced.registry.ModBlocks
+import dev.aaronhowser.mods.geneticsresequenced.registry.ModItems
+import dev.aaronhowser.mods.geneticsresequenced.registry.ModMenuTypes
+import dev.aaronhowser.mods.geneticsresequenced.util.OtherUtil.withColor
+import net.minecraft.ChatFormatting
+import net.minecraft.network.FriendlyByteBuf
+import net.minecraft.world.entity.player.Inventory
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.inventory.ContainerData
+import net.minecraft.world.inventory.ContainerLevelAccess
+import net.minecraft.world.inventory.SimpleContainerData
+import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent
+import net.neoforged.neoforge.items.SlotItemHandler
+
+
+class PlasmidInjectorMenu(
+    id: Int,
+    inventory: Inventory,
+    blockEntity: PlasmidInjectorBlockEntity,
+    private val containerData: ContainerData
+) : MachineMenu(
+    ModMenuTypes.PLASMID_INJECTOR.get(),
+    blockEntity,
+    id,
+    inventory
+) {
+
+    constructor(id: Int, inventory: Inventory, extraData: FriendlyByteBuf) :
+            this(
+                id,
+                inventory,
+                inventory.player.level().getBlockEntity(extraData.readBlockPos()) as PlasmidInjectorBlockEntity,
+                SimpleContainerData(CraftingMachineBlockEntity.SIMPLE_CONTAINER_SIZE)
+            )
+
+    init {
+        checkContainerSize(inventory, CraftingMachineBlockEntity.SIMPLE_CONTAINER_SIZE)
+
+        addPlayerInventory(inventory)
+        addPlayerHotbar(inventory)
+
+        val itemHandler = this.blockEntity.itemHandler
+
+        this.addSlot(SlotItemHandler(itemHandler, CraftingMachineBlockEntity.INPUT_SLOT_INDEX, 63, 36))
+        this.addSlot(SlotItemHandler(itemHandler, CraftingMachineBlockEntity.OUTPUT_SLOT_INDEX, 110, 36))
+        this.addSlot(SlotItemHandler(itemHandler, CraftingMachineBlockEntity.OVERCLOCK_SLOT_INDEX, 26, 47))
+
+        addDataSlots(containerData)
+    }
+
+
+    override fun stillValid(pPlayer: Player): Boolean {
+        return stillValid(
+            ContainerLevelAccess.create(level, blockEntity.blockPos),
+            pPlayer,
+            ModBlocks.PLASMID_INJECTOR.get()
+        )
+    }
+
+    private var progress: Int
+        get() = containerData.get(DATA_PROGRESS_INDEX)
+        set(value) {
+            containerData.set(DATA_PROGRESS_INDEX, value)
+        }
+
+    private var maxProgress: Int
+        get() = containerData.get(DATA_MAX_PROGRESS_INDEX)
+        set(value) {
+            containerData.set(DATA_MAX_PROGRESS_INDEX, value)
+        }
+
+    val isCrafting
+        get() = progress > 0
+
+    fun getScaledProgress(): Int {
+        val progressArrowSize = ScreenTextures.Elements.ArrowRight.Dimensions.WIDTH
+
+        return if (maxProgress == 0 || progress == 0) {
+            0
+        } else {
+            progress * progressArrowSize / maxProgress
+        }
+    }
+
+    companion object {
+        fun showTooltip(event: ItemTooltipEvent) {
+            val hoverStack = event.itemStack
+            if (hoverStack.item != ModItems.SYRINGE.get()) return
+
+            if (SyringeItem.isContaminated(hoverStack)) {
+                event.toolTip.add(
+                    2,
+                    ModLanguageProvider.Tooltips.INFUSER_CONTAMINATED
+                        .toComponent()
+                        .withColor(ChatFormatting.RED)
+                )
+            }
+        }
+
+        private const val DATA_PROGRESS_INDEX = 0
+        private const val DATA_MAX_PROGRESS_INDEX = 1
+    }
+
+}
---
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/block/machine/plasmid_injector/PlasmidInjectorScreen.kt
@@ -0,0 +1,127 @@
+package dev.aaronhowser.mods.geneticsresequenced.block.machine.plasmid_injector
+
+import com.mojang.blaze3d.systems.RenderSystem
+import dev.aaronhowser.mods.geneticsresequenced.block.base.menu.ScreenTextures
+import dev.aaronhowser.mods.geneticsresequenced.util.MouseUtil
+import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
+import net.minecraft.client.renderer.GameRenderer
+import net.minecraft.network.chat.Component
+import net.minecraft.world.entity.player.Inventory
+
+
+class PlasmidInjectorScreen(
+    pMenu: PlasmidInjectorMenu,
+    pPlayerInventory: Inventory,
+    pTitle: Component
+) : AbstractContainerScreen<PlasmidInjectorMenu>(pMenu, pPlayerInventory, pTitle) {
+
+    override fun renderBg(pGuiGraphics: GuiGraphics, pPartialTick: Float, pMouseX: Int, pMouseY: Int) {
+        RenderSystem.setShader { GameRenderer.getPositionTexShader() }
+        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f)
+        RenderSystem.setShaderTexture(0, ScreenTextures.Backgrounds.PLASMID_INFUSER)
+        val x = (width - imageWidth) / 2
+        val y = (height - imageHeight) / 2
+
+        pGuiGraphics.blit(
+            ScreenTextures.Backgrounds.PLASMID_INFUSER,
+            x, y,
+            0, 0,
+            ScreenTextures.Backgrounds.TEXTURE_SIZE,
+            ScreenTextures.Backgrounds.TEXTURE_SIZE
+        )
+
+        renderProgressArrow(pGuiGraphics, x, y)
+        renderEnergyInfo(pGuiGraphics, x, y)
+    }
+
+    private fun renderEnergyInfo(pGuiGraphics: GuiGraphics, x: Int, y: Int) {
+        val energyStorage = menu.blockEntity.energyStorage
+        val percent = energyStorage.energyStored.toFloat() / energyStorage.maxEnergyStored.toFloat()
+
+        pGuiGraphics.blitSprite(
+            ScreenTextures.Elements.Energy.TEXTURE,
+            ScreenTextures.Elements.Energy.TEXTURE_SIZE,
+            ScreenTextures.Elements.Energy.TEXTURE_SIZE,
+            0,
+            0,
+            x + ScreenTextures.Elements.Energy.Location.Default.X,
+            y + ScreenTextures.Elements.Energy.Location.Default.Y,
+            0,
+            ScreenTextures.Elements.Energy.TEXTURE_SIZE,
+            (ScreenTextures.Elements.Energy.TEXTURE_SIZE * percent).toInt()
+        )
+    }
+
+    override fun renderLabels(pGuiGraphics: GuiGraphics, pMouseX: Int, pMouseY: Int) {
+        val x = (width - imageWidth) / 2
+        val y = (height - imageHeight) / 2
+        renderEnergyAreaTooltip(pGuiGraphics, x, y, pMouseX, pMouseY)
+
+        super.renderLabels(pGuiGraphics, pMouseX, pMouseY)
+    }
+
+    private fun renderEnergyAreaTooltip(pGuiGraphics: GuiGraphics, x: Int, y: Int, pMouseX: Int, pMouseY: Int) {
+        if (isMouseOver(
+                pMouseX, pMouseY,
+                x, y,
+                ScreenTextures.Elements.Energy.Location.Default.X,
+                ScreenTextures.Elements.Energy.Location.Default.Y,
+                ScreenTextures.Elements.Energy.Dimensions.WIDTH,
+                ScreenTextures.Elements.Energy.Dimensions.HEIGHT
+            )
+        ) {
+            val energyStorage = menu.blockEntity.energyStorage
+
+            pGuiGraphics.renderComponentTooltip(
+                Minecraft.getInstance().font,
+                Component
+                    .literal(energyStorage.energyStored.toString() + "/" + energyStorage.maxEnergyStored + " FE")
+                    .toFlatList(),
+                pMouseX - x,
+                pMouseY - y
+            )
+        }
+
+    }
+
+    @Suppress("SameParameterValue")
+    private fun isMouseOver(
+        mouseX: Int, mouseY: Int,
+        x: Int, y: Int,
+        topLeftX: Int,
+        topLeftY: Int,
+        width: Int,
+        height: Int
+    ): Boolean {
+        return MouseUtil.isMouseOver(
+            mouseX, mouseY,
+            x + topLeftX, y + topLeftY,
+            width, height
+        )
+    }
+
+    private fun renderProgressArrow(pGuiGraphics: GuiGraphics, x: Int, y: Int) {
+        if (!menu.isCrafting) return
+
+        pGuiGraphics.blitSprite(
+            ScreenTextures.Elements.ArrowRight.TEXTURE,
+            ScreenTextures.Elements.ArrowRight.TEXTURE_SIZE,
+            ScreenTextures.Elements.ArrowRight.TEXTURE_SIZE,
+            0,
+            0,
+            x + ScreenTextures.Elements.ArrowRight.Position.Blood.X,
+            y + ScreenTextures.Elements.ArrowRight.Position.Blood.Y,
+            menu.getScaledProgress(),
+            ScreenTextures.Elements.ArrowRight.TEXTURE_SIZE
+        )
+    }
+
+    override fun render(pGuiGraphics: GuiGraphics, pMouseX: Int, pMouseY: Int, pPartialTick: Float) {
+        super.renderBackground(pGuiGraphics, pMouseX, pMouseY, pPartialTick)
+        super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick)
+        super.renderTooltip(pGuiGraphics, pMouseX, pMouseY)
+    }
+
+}
