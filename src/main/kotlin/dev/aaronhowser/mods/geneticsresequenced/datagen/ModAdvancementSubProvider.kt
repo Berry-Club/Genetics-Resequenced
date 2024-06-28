f8d3903f04bee745cd80ef43f8711c82346fccb8
Aaron Howser <aaronhowser1@gmail.com>
2024-06-28 12:21:11 -0500

    Finished advancement datagen

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/ModAdvancementSubProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/ModAdvancementSubProvider.kt
@@ -1,19 +1,21 @@
 package dev.aaronhowser.mods.geneticsresequenced.datagen
 
 import dev.aaronhowser.mods.geneticsresequenced.datagen.ModLanguageProvider.Companion.toComponent
+import dev.aaronhowser.mods.geneticsresequenced.item.components.SpecificEntityItemComponent
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModBlocks
+import dev.aaronhowser.mods.geneticsresequenced.registry.ModDataComponents
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModItems
 import dev.aaronhowser.mods.geneticsresequenced.util.OtherUtil
-import net.minecraft.advancements.Advancement
-import net.minecraft.advancements.AdvancementHolder
-import net.minecraft.advancements.AdvancementRequirements
-import net.minecraft.advancements.AdvancementType
+import net.minecraft.advancements.*
+import net.minecraft.advancements.critereon.ImpossibleTrigger
 import net.minecraft.advancements.critereon.InventoryChangeTrigger
 import net.minecraft.core.HolderLookup
 import net.minecraft.network.chat.Component
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.Items
 import net.neoforged.neoforge.common.data.AdvancementProvider
 import net.neoforged.neoforge.common.data.ExistingFileHelper
+import java.util.*
 import java.util.function.Consumer
 
 class ModAdvancementSubProvider : AdvancementProvider.AdvancementGenerator {
@@ -76,6 +78,235 @@ class ModAdvancementSubProvider : AdvancementProvider.AdvancementGenerator {
                 .build(guide("cell_analyzer"))
                 .add()
 
+        val dnaExtractor =
+            Advancement.Builder.advancement()
+                .parent(cellAnalyzer)
+                .display(
+                    ModBlocks.DNA_EXTRACTOR.get(),
+                    ModLanguageProvider.Advancements.EXTRACTOR_TITLE.toComponent(),
+                    ModLanguageProvider.Advancements.EXTRACTOR_DESC.toComponent(),
+                    null,
+                    AdvancementType.TASK,
+                    true, true, false
+                )
+                .addCriterion(
+                    "dna_extractor",
+                    InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.DNA_EXTRACTOR.get())
+                )
+                .addCriterion(
+                    "dna_helix",
+                    InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.DNA_HELIX.get())
+                )
+                .requirements(AdvancementRequirements.Strategy.OR)
+                .build(guide("dna_extractor"))
+                .add()
+
+        val dnaDecryptor =
+            Advancement.Builder.advancement()
+                .parent(dnaExtractor)
+                .display(
+                    ModBlocks.DNA_DECRYPTOR.get(),
+                    ModLanguageProvider.Advancements.DECRYPTOR_TITLE.toComponent(),
+                    ModLanguageProvider.Advancements.DECRYPTOR_DESC.toComponent(),
+                    null,
+                    AdvancementType.TASK,
+                    true, true, false
+                )
+                .addCriterion(
+                    "dna_decryptor",
+                    InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.DNA_DECRYPTOR.get())
+                )
+                .build(guide("dna_decryptor"))
+                .add()
+
+        val decryptDna =
+            Advancement.Builder.advancement()
+                .parent(dnaDecryptor)
+                .display(
+                    ModItems.DNA_HELIX.get(),
+                    ModLanguageProvider.Advancements.DECRYPT_TITLE.toComponent(),
+                    ModLanguageProvider.Advancements.DECRYPT_DESC.toComponent(),
+                    null,
+                    AdvancementType.TASK,
+                    true, true, false
+                )
+                .addCriterion(
+                    "impossible",
+                    CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance())
+                )
+                .build(guide("decrypt_dna"))
+                .add()
+
+        val blackDeath =
+            Advancement.Builder.advancement()
+                .parent(decryptDna)
+                .display(
+                    Items.WITHER_ROSE,
+                    ModLanguageProvider.Advancements.BLACK_DEATH_TITLE.toComponent(),
+                    ModLanguageProvider.Advancements.BLACK_DEATH_DESC.toComponent(),
+                    null,
+                    AdvancementType.TASK,
+                    true, true, false
+                )
+                .addCriterion(
+                    "impossible",
+                    CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance())
+                )
+                .build(guide("black_death"))
+                .add()
+
+        val plasmidInfuser =
+            Advancement.Builder.advancement()
+                .parent(dnaExtractor)
+                .display(
+                    ModBlocks.PLASMID_INFUSER.get(),
+                    ModLanguageProvider.Advancements.INFUSER_TITLE.toComponent(),
+                    ModLanguageProvider.Advancements.INFUSER_DESC.toComponent(),
+                    null,
+                    AdvancementType.TASK,
+                    true, true, false
+                )
+                .addCriterion(
+                    "plasmid_infuser",
+                    InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.PLASMID_INFUSER.get())
+                )
+                .build(guide("plasmid_infuser"))
+                .add()
+
+        val plasmidInjector =
+            Advancement.Builder.advancement()
+                .parent(plasmidInfuser)
+                .display(
+                    ModBlocks.PLASMID_INJECTOR.get(),
+                    ModLanguageProvider.Advancements.INJECTOR_TITLE.toComponent(),
+                    ModLanguageProvider.Advancements.INJECTOR_DESC.toComponent(),
+                    null,
+                    AdvancementType.TASK,
+                    true, true, false
+                )
+                .addCriterion(
+                    "plasmid_injector",
+                    InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.PLASMID_INJECTOR.get())
+                )
+                .build(guide("plasmid_injector"))
+                .add()
+
+        val getGene =
+            Advancement.Builder.advancement()
+                .parent(plasmidInjector)
+                .display(
+                    ModItems.SYRINGE.toStack().apply {
+                        set(
+                            ModDataComponents.SPECIFIC_ENTITY_COMPONENT,
+                            SpecificEntityItemComponent(
+                                UUID.randomUUID(),
+                                "A Fake Mob"
+                            )
+                        )
+                    },
+                    ModLanguageProvider.Advancements.GET_GENE_TITLE.toComponent(),
+                    ModLanguageProvider.Advancements.GET_GENE_DESC.toComponent(),
+                    null,
+                    AdvancementType.TASK,
+                    true, true, false
+                )
+                .addCriterion(
+                    "impossible",
+                    CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance())
+                )
+                .build(guide("get_gene"))
+                .add()
+
+        val getFlight =
+            Advancement.Builder.advancement()
+                .parent(getGene)
+                .display(
+                    Items.ELYTRA,
+                    ModLanguageProvider.Advancements.FLIGHT_TITLE.toComponent(),
+                    ModLanguageProvider.Advancements.FLIGHT_DESC.toComponent(),
+                    null,
+                    AdvancementType.TASK,
+                    true, true, false
+                )
+                .addCriterion(
+                    "impossible",
+                    CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance())
+                )
+                .build(guide("get_flight"))
+                .add()
+
+        val getAllScares =
+            Advancement.Builder.advancement()
+                .parent(getGene)
+                .display(
+                    Items.JACK_O_LANTERN,
+                    ModLanguageProvider.Advancements.SCARE_TITLE.toComponent(),
+                    ModLanguageProvider.Advancements.SCARE_DESC.toComponent(),
+                    null,
+                    AdvancementType.TASK,
+                    true, true, false
+                )
+                .addCriterion(
+                    "impossible",
+                    CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance())
+                )
+                .build(guide("get_all_scare_genes"))
+                .add()
+
+        val getCringe =
+            Advancement.Builder.advancement()
+                .parent(getGene)
+                .display(
+                    Items.SKELETON_SKULL,
+                    ModLanguageProvider.Advancements.CRINGE_TITLE.toComponent(),
+                    ModLanguageProvider.Advancements.CRINGE_DESC.toComponent(),
+                    null,
+                    AdvancementType.TASK,
+                    true, true, false
+                )
+                .addCriterion(
+                    "impossible",
+                    CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance())
+                )
+                .build(guide("get_cringe"))
+                .add()
+
+        val getMilked =
+            Advancement.Builder.advancement()
+                .parent(getGene)
+                .display(
+                    Items.MILK_BUCKET,
+                    ModLanguageProvider.Advancements.GET_MILKED_TITLE.toComponent(),
+                    ModLanguageProvider.Advancements.GET_MILKED_DESC.toComponent(),
+                    null,
+                    AdvancementType.TASK,
+                    true, true, false
+                )
+                .addCriterion(
+                    "impossible",
+                    CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance())
+                )
+                .build(guide("get_milked"))
+                .add()
+
+        val triggerSlimyDeath =
+            Advancement.Builder.advancement()
+                .parent(getGene)
+                .display(
+                    Items.SLIME_BALL,
+                    ModLanguageProvider.Advancements.SLIMY_TITLE.toComponent(),
+                    ModLanguageProvider.Advancements.SLIMY_DESC.toComponent(),
+                    null,
+                    AdvancementType.TASK,
+                    true, true, false
+                )
+                .addCriterion(
+                    "impossible",
+                    CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance())
+                )
+                .build(guide("trigger_slimy_death"))
+                .add()
+
         val syringe =
             Advancement.Builder.advancement()
                 .parent(root)
@@ -99,6 +330,23 @@ class ModAdvancementSubProvider : AdvancementProvider.AdvancementGenerator {
                 .build(guide("syringe"))
                 .add()
 
+        val bloodPurifier =
+            Advancement.Builder.advancement()
+                .parent(syringe)
+                .display(
+                    ModBlocks.BLOOD_PURIFIER.get(),
+                    ModLanguageProvider.Advancements.PURIFIER_TITLE.toComponent(),
+                    ModLanguageProvider.Advancements.PURIFIER_DESC.toComponent(),
+                    null,
+                    AdvancementType.TASK,
+                    true, true, false
+                )
+                .addCriterion(
+                    "blood_purifier",
+                    InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.BLOOD_PURIFIER.get())
+                )
+                .build(guide("blood_purifier"))
+                .add()
 
     }
 
