5fbcaa18986f0cce5c69da5fd2be7705babe4ab0
Aaron Howser <aaronhowser1@gmail.com>
2024-06-29 13:42:31 -0500

    Work on item models

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/model/ModItemModelProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/model/ModItemModelProvider.kt
@@ -3,10 +3,8 @@ package dev.aaronhowser.mods.geneticsresequenced.datagen.model
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModItems
 import net.minecraft.data.PackOutput
-import net.neoforged.neoforge.client.model.generators.ItemModelBuilder
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider
 import net.neoforged.neoforge.common.data.ExistingFileHelper
-import net.neoforged.neoforge.registries.DeferredItem
 
 class ModItemModelProvider(
     output: PackOutput,
@@ -14,18 +12,20 @@ class ModItemModelProvider(
 ) : ItemModelProvider(output, GeneticsResequenced.ID, existingFileHelper) {
 
     override fun registerModels() {
-        simpleItem(ModItems.SCRAPER)
-//        simpleItem(ModItems.SYRINGE)
-//        simpleItem(ModItems.METAL_SYRINGE)
-        simpleItem(ModItems.ORGANIC_MATTER)
-        simpleItem(ModItems.CELL)
-        simpleItem(ModItems.GMO_CELL)
-        simpleItem(ModItems.DNA_HELIX)
-        simpleItem(ModItems.PLASMID)
-        simpleItem(ModItems.ANTI_PLASMID)
-        simpleItem(ModItems.OVERCLOCKER)
-        simpleItem(ModItems.ANTI_FIELD_ORB)
-        simpleItem(ModItems.DRAGON_HEALTH_CRYSTAL)
+
+        syringe()
+        metalSyringe()
+
+        basicItem(ModItems.SCRAPER.get())
+        basicItem(ModItems.ORGANIC_MATTER.get())
+        basicItem(ModItems.CELL.get())
+        basicItem(ModItems.GMO_CELL.get())
+        basicItem(ModItems.DNA_HELIX.get())
+        basicItem(ModItems.PLASMID.get())
+        basicItem(ModItems.ANTI_PLASMID.get())
+        basicItem(ModItems.OVERCLOCKER.get())
+        basicItem(ModItems.ANTI_FIELD_ORB.get())
+        basicItem(ModItems.DRAGON_HEALTH_CRYSTAL.get())
 
         this.withExistingParent(
             ModItems.FRIENDLY_SLIME_SPAWN_EGG.id.path,
@@ -34,14 +34,11 @@ class ModItemModelProvider(
 
     }
 
-    private fun simpleItem(item: DeferredItem<*>): ItemModelBuilder {
-        return withExistingParent(
-            item.id.path,
-            mcLoc("item/generated") //Aiming at
-        ).texture(
-            "layer0",
-            modLoc("item/${item.id.path}")
-        )
+    private fun syringe() {
+
     }
 
+    private fun metalSyringe() {
+
+    }
 }
