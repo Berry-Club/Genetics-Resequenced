46f5ff8f2cb76ef3cb1cdf96a4087f89b1b26504
Aaron Howser <aaronhowser1@gmail.com>
2024-07-01 12:12:29 -0500

    minor refactor

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/effect/BleedEffect.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/effect/BleedEffect.kt
@@ -1,14 +1,15 @@
 package dev.aaronhowser.mods.geneticsresequenced.effect
 
 import dev.aaronhowser.mods.geneticsresequenced.util.OtherUtil
+import net.minecraft.core.Holder
 import net.minecraft.core.registries.Registries
 import net.minecraft.resources.ResourceKey
 import net.minecraft.world.damagesource.DamageSource
 import net.minecraft.world.damagesource.DamageType
-import net.minecraft.world.damagesource.DamageTypes
 import net.minecraft.world.effect.MobEffect
 import net.minecraft.world.effect.MobEffectCategory
 import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.level.Level
 
 class BleedEffect : MobEffect(
     MobEffectCategory.HARMFUL,
@@ -23,15 +24,10 @@ class BleedEffect : MobEffect(
     override fun applyEffectTick(pLivingEntity: LivingEntity, pAmplifier: Int): Boolean {
         if (pLivingEntity.level().isClientSide) return false
 
-        val damageTypes = pLivingEntity.level().damageSources().damageTypes
-        val damageType = damageTypes
-            .getHolder(BLEED_DAMAGE)
-            .orElse(
-                damageTypes
-                    .getHolderOrThrow(DamageTypes.MAGIC)
-            )
-
-        pLivingEntity.hurt(DamageSource(damageType), 1f)
+        pLivingEntity.hurt(
+            DamageSource(getHolder(pLivingEntity.level())),
+            1f
+        )
 
         return true
     }
@@ -39,6 +35,10 @@ class BleedEffect : MobEffect(
     companion object {
         val BLEED_DAMAGE: ResourceKey<DamageType> =
             ResourceKey.create(Registries.DAMAGE_TYPE, OtherUtil.modResource("bleed"))
+
+        fun getHolder(level: Level): Holder.Reference<DamageType> {
+            return level.registryAccess().registry(Registries.DAMAGE_TYPE).get().getHolderOrThrow(BLEED_DAMAGE)
+        }
     }
 
 }
