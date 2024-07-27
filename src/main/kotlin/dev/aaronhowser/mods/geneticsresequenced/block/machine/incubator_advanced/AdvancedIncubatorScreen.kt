b3c31c1966e3633ca93a106718d443ca282023ef
Aaron Howser <aaronhowser1@gmail.com>
2024-07-26 22:51:31 -0500

    AdvancedIncubatorScreen is now MachineScreen

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/block/machine/incubator_advanced/AdvancedIncubatorScreen.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/block/machine/incubator_advanced/AdvancedIncubatorScreen.kt
@@ -1,87 +1,30 @@
 package dev.aaronhowser.mods.geneticsresequenced.block.machine.incubator_advanced
 
+import dev.aaronhowser.mods.geneticsresequenced.block.base.menu.MachineScreen
 import dev.aaronhowser.mods.geneticsresequenced.block.base.menu.ScreenTextures
-import dev.aaronhowser.mods.geneticsresequenced.util.MouseUtil
-import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.GuiGraphics
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
 import net.minecraft.network.chat.Component
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.entity.player.Inventory
 
 class AdvancedIncubatorScreen(
     pMenu: AdvancedIncubatorMenu,
     pPlayerInventory: Inventory,
     pTitle: Component
-) : AbstractContainerScreen<AdvancedIncubatorMenu>(pMenu, pPlayerInventory, pTitle) {
+) : MachineScreen<AdvancedIncubatorMenu>(pMenu, pPlayerInventory, pTitle) {
 
     companion object {
         private const val FAST_BUBBLE_SPEED = 12
         private const val SLOW_BUBBLE_SPEED = 12 * 3
     }
 
+    override val backgroundTexture: ResourceLocation = ScreenTextures.Backgrounds.INCUBATOR
+
     override fun renderBg(pGuiGraphics: GuiGraphics, pPartialTick: Float, pMouseX: Int, pMouseY: Int) {
-        pGuiGraphics.blit(
-            ScreenTextures.Backgrounds.INCUBATOR_ADVANCED,
-            leftPos, topPos,
-            0, 0,
-            ScreenTextures.Backgrounds.TEXTURE_SIZE,
-            ScreenTextures.Backgrounds.TEXTURE_SIZE
-        )
+        super.renderBg(pGuiGraphics, pPartialTick, pMouseX, pMouseY)
 
         renderHeat(pGuiGraphics, leftPos, topPos)
-        renderProgressArrow(pGuiGraphics, leftPos, topPos)
         renderBubble(pGuiGraphics, leftPos, topPos)
-        renderEnergyInfo(pGuiGraphics, leftPos, topPos)
-    }
-
-    private fun renderEnergyInfo(pGuiGraphics: GuiGraphics, x: Int, y: Int) {
-        val energyStorage = menu.blockEntity.energyStorage
-        val percent = energyStorage.energyStored.toFloat() / energyStorage.maxEnergyStored.toFloat()
-
-        pGuiGraphics.blitSprite(
-            ScreenTextures.Elements.Energy.TEXTURE,
-            ScreenTextures.Elements.Energy.TEXTURE_SIZE,
-            ScreenTextures.Elements.Energy.TEXTURE_SIZE,
-            0,
-            0,
-            x + ScreenTextures.Elements.Energy.Location.Incubator.X,
-            y + ScreenTextures.Elements.Energy.Location.Incubator.Y,
-            0,
-            ScreenTextures.Elements.Energy.TEXTURE_SIZE,
-            (ScreenTextures.Elements.Energy.TEXTURE_SIZE * percent).toInt()
-        )
-    }
-
-    override fun renderLabels(pGuiGraphics: GuiGraphics, pMouseX: Int, pMouseY: Int) {
-        val x = (width - imageWidth) / 2
-        val y = (height - imageHeight) / 2
-
-        renderEnergyAreaTooltip(pGuiGraphics, x, y, pMouseX, pMouseY)
-        super.renderLabels(pGuiGraphics, pMouseX, pMouseY)
-    }
-
-    private fun renderEnergyAreaTooltip(pGuiGraphics: GuiGraphics, x: Int, y: Int, pMouseX: Int, pMouseY: Int) {
-        if (isMouseOver(
-                pMouseX, pMouseY,
-                x, y,
-                ScreenTextures.Elements.Energy.Location.Incubator.X,
-                ScreenTextures.Elements.Energy.Location.Incubator.Y,
-                ScreenTextures.Elements.Energy.Dimensions.WIDTH,
-                ScreenTextures.Elements.Energy.Dimensions.HEIGHT
-            )
-        ) {
-            val energyStorage = menu.blockEntity.energyStorage
-
-            pGuiGraphics.renderComponentTooltip(
-                Minecraft.getInstance().font,
-                Component
-                    .literal(energyStorage.energyStored.toString() + "/" + energyStorage.maxEnergyStored + " FE")
-                    .toFlatList(),
-                pMouseX - x,
-                pMouseY - y
-            )
-        }
-
     }
 
     override fun mouseClicked(pMouseX: Double, pMouseY: Double, pButton: Int): Boolean {
@@ -106,37 +49,17 @@ class AdvancedIncubatorScreen(
         )
     }
 
-    private fun isMouseOver(
-        mouseX: Int, mouseY: Int,
-        x: Int, y: Int,
-        topLeftX: Int,
-        topLeftY: Int,
-        width: Int,
-        height: Int
-    ): Boolean {
-        return MouseUtil.isMouseOver(
-            mouseX, mouseY,
-            x + topLeftX, y + topLeftY,
-            width, height
-        )
-    }
+    override val energyPosLeft: Int = ScreenTextures.Elements.Energy.Location.Incubator.X
+    override val energyPosTop: Int = ScreenTextures.Elements.Energy.Location.Incubator.Y
 
     //FIXME: Make it fill up instead of emptying
-    private fun renderProgressArrow(pGuiGraphics: GuiGraphics, x: Int, y: Int) {
-        if (!menu.isCrafting) return
-
-        pGuiGraphics.blitSprite(
-            ScreenTextures.Elements.ArrowDown.TEXTURE,
-            ScreenTextures.Elements.ArrowDown.TEXTURE_SIZE,
-            ScreenTextures.Elements.ArrowDown.TEXTURE_SIZE,
-            0,
-            0,
-            x + ScreenTextures.Elements.ArrowDown.Position.X,
-            y + ScreenTextures.Elements.ArrowDown.Position.Y,
-            ScreenTextures.Elements.ArrowDown.Dimensions.WIDTH,
-            menu.getScaledProgress()
-        )
-    }
+    override val arrowTexture: ResourceLocation = ScreenTextures.Elements.ArrowDown.TEXTURE
+    override val arrowTextureSize: Int = ScreenTextures.Elements.ArrowDown.TEXTURE_SIZE
+    override val arrowPosLeft: Int = ScreenTextures.Elements.ArrowDown.Position.X
+    override val arrowPosTop: Int = ScreenTextures.Elements.ArrowDown.Position.Y
+    override fun shouldRenderProgressArrow(): Boolean = menu.isCrafting
+    override fun progressArrowWidth(): Int = ScreenTextures.Elements.ArrowDown.Dimensions.WIDTH
+    override fun progressArrowHeight(): Int = menu.getScaledProgress()
 
     private var bubblePosProgress = 0
     private var bubblePos = 0
@@ -194,11 +117,4 @@ class AdvancedIncubatorScreen(
             ScreenTextures.Elements.Heat.Dimensions.HEIGHT
         )
     }
-
-    override fun render(pGuiGraphics: GuiGraphics, pMouseX: Int, pMouseY: Int, pPartialTick: Float) {
-        super.renderBackground(pGuiGraphics, pMouseX, pMouseY, pPartialTick)
-        super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick)
-        super.renderTooltip(pGuiGraphics, pMouseX, pMouseY)
-    }
-
 }
