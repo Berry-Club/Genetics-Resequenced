9f1f36128cf0ecdc1f63339b607cfd590b3dc57e
Aaron Howser <aaronhowser1@gmail.com>
2024-09-16 11:47:19 -0500

    Revert "Revert "progress""
    This reverts commit c14ce3a7d0ad9f95ea547bbf62f912e2fd1b7be8.

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/api/genes/Gene.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/api/genes/Gene.kt
@@ -85,7 +85,7 @@ data class Gene(
 
     val id = OtherUtil.modResource("gene")
 
-    private val requiredGenes: MutableSet<Gene> = mutableSetOf()
+    private val requiredGenes: MutableSet<Holder<Gene>> = mutableSetOf()
 
     fun isMutation(registries: HolderLookup.Provider): Boolean {
         return GeneRegistry.getAllGeneHolders(registries)
@@ -93,15 +93,15 @@ data class Gene(
             .not()
     }
 
-    fun addRequiredGenes(genes: Collection<Gene>) {
+    fun addRequiredGenes(genes: Collection<Holder<Gene>>) {
         requiredGenes.addAll(genes)
     }
 
-    fun removeRequiredGenes(genes: Collection<Gene>) {
+    fun removeRequiredGenes(genes: Collection<Holder<Gene>>) {
         requiredGenes.removeAll(genes.toSet())
     }
 
-    fun getRequiredGenes(): Set<Gene> {
+    fun getRequiredGeneHolders(): Set<Holder<Gene>> {
         return requiredGenes.toSet()
     }
 
