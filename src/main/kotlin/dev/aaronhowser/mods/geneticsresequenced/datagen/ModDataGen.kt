fb868657cacc3fe1f3b1c88791432ab8b757727f
Aaron Howser <aaronhowser1@gmail.com>
2024-09-16 11:34:20 -0500

    Revert "progress with datagen"
    This reverts commit 73731ae419aab3f42083a728114238442bb82d13.

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/ModDataGen.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/ModDataGen.kt
@@ -18,7 +18,6 @@ import net.minecraft.data.PackOutput
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.neoforge.common.data.AdvancementProvider
-import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider
 import net.neoforged.neoforge.common.data.ExistingFileHelper
 import net.neoforged.neoforge.data.event.GatherDataEvent
 import java.util.concurrent.CompletableFuture
@@ -88,16 +87,6 @@ object ModDataGen {
         val modonomiconEnUsProvider = generator.addProvider(
             event.includeClient(),
             EnUsProvider(output, modonomiconEnUsCache)
-        )
-
-        val genesProvider = generator.addProvider(
-            event.includeServer(),
-            DatapackBuiltinEntriesProvider(
-                output,
-                lookupProvider,
-                ModGeneProvider(),
-                setOf(GeneticsResequenced.ID)
-            )
         )
 
     }
