88521d06ecb3b38290d05dd1ed9edac8ad7192bf
Aaron Howser <aaronhowser1@gmail.com>
2024-09-16 11:34:17 -0500

    Revert "progress"
    This reverts commit e5971f9a35df3a2ecd483a19588428b272bc776f.

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/attachment/GenesData.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/attachment/GenesData.kt
@@ -6,31 +6,28 @@ import dev.aaronhowser.mods.geneticsresequenced.api.genes.Gene
 import dev.aaronhowser.mods.geneticsresequenced.api.genes.GeneRegistry
 import dev.aaronhowser.mods.geneticsresequenced.config.ServerConfig
 import dev.aaronhowser.mods.geneticsresequenced.event.CustomEvents
-import dev.aaronhowser.mods.geneticsresequenced.gene.ModGenes
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModAttachmentTypes
 import net.minecraft.core.Holder
 import net.minecraft.core.HolderLookup
-import net.minecraft.core.HolderSet
-import net.minecraft.resources.ResourceKey
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.player.Player
 import thedarkcolour.kotlinforforge.neoforge.forge.FORGE_BUS
 
 
 data class GenesData(
-    val genes: HolderSet<Gene>
+    val genes: Set<Holder<Gene>>
 ) {
-    constructor() : this(HolderSet.empty())
-    constructor(set: Set<Holder<Gene>>) : this(HolderSet.direct(set.toList()))
+    constructor() : this(emptySet())
 
     companion object {
 
         val CODEC: Codec<GenesData> = Gene.CODEC.listOf().xmap(
-            (::GenesData)
-        ) { it.genes.toList() }
+            { GenesData(it.toSet()) },
+            { it.genes.toList() }
+        )
 
         var LivingEntity.geneHolders: Set<Holder<Gene>>
-            get() = this.getData(ModAttachmentTypes.GENE_CONTAINER).genes.toSet()
+            get() = this.getData(ModAttachmentTypes.GENE_CONTAINER).genes
             private set(value) {
                 this.setData(ModAttachmentTypes.GENE_CONTAINER, GenesData(value))
             }
@@ -44,12 +41,7 @@ data class GenesData(
                 return false
             }
 
-            if (
-                this is Player
-                && newGene.isNegative
-                && ServerConfig.disableGivingPlayersNegativeGenes.get()
-                && newGeneHolder.key != ModGenes.CRINGE
-            ) {
+            if (this is Player && newGene.isNegative && ServerConfig.disableGivingPlayersNegativeGenes.get() && newGene != ModGenes.CRINGE.get()) {
                 GeneticsResequenced.LOGGER.debug(
                     "Tried to give negative gene $newGene to player $this, but \"disableGivingPlayersNegativeGenes\" is true in the server config."
                 )
@@ -61,7 +53,7 @@ data class GenesData(
                 return false
             }
 
-            val eventPre = CustomEvents.GeneChangeEvent.Pre(this, newGeneHolder, true)
+            val eventPre = CustomEvents.GeneChangeEvent.Pre(this, newGene, true)
             val wasCanceled = FORGE_BUS.post(eventPre).isCanceled
             if (wasCanceled) {
                 GeneticsResequenced.LOGGER.debug("Event was canceled: $eventPre")
@@ -70,7 +62,7 @@ data class GenesData(
 
             this.geneHolders += newGeneHolder
 
-            val eventPost = CustomEvents.GeneChangeEvent.Post(this, newGeneHolder, true)
+            val eventPost = CustomEvents.GeneChangeEvent.Post(this, newGene, true)
             FORGE_BUS.post(eventPost)
 
             return true
@@ -78,8 +70,9 @@ data class GenesData(
 
         fun LivingEntity.removeGene(removedGeneHolder: Holder<Gene>): Boolean {
             if (!this.hasGene(removedGeneHolder)) return false
+            val removedGene = removedGeneHolder.value()
 
-            val eventPre = CustomEvents.GeneChangeEvent.Pre(this, removedGeneHolder, false)
+            val eventPre = CustomEvents.GeneChangeEvent.Pre(this, removedGene, false)
             val wasCanceled = FORGE_BUS.post(eventPre).isCanceled
             if (wasCanceled) {
                 GeneticsResequenced.LOGGER.debug("Event was canceled: $eventPre")
@@ -88,7 +81,7 @@ data class GenesData(
 
             this.geneHolders -= removedGeneHolder
 
-            val eventPost = CustomEvents.GeneChangeEvent.Post(this, removedGeneHolder, false)
+            val eventPost = CustomEvents.GeneChangeEvent.Post(this, removedGene, false)
             FORGE_BUS.post(eventPost)
 
             return true
@@ -96,10 +89,6 @@ data class GenesData(
 
         fun LivingEntity.hasGene(gene: Holder<Gene>): Boolean {
             return gene in this.geneHolders
-        }
-
-        fun LivingEntity.hasGene(geneKey: ResourceKey<Gene>): Boolean {
-            return this.geneHolders.any { it.key == geneKey }
         }
 
         fun LivingEntity.removeAllGenes() {
