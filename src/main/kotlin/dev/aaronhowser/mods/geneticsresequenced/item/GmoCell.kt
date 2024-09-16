0c3bf813ca4dda9e0ddba02e5634bc9b2f9b6a20
Aaron Howser <aaronhowser1@gmail.com>
2024-09-16 10:53:46 -0500

    progress

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/item/GmoCell.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/item/GmoCell.kt
@@ -3,8 +3,10 @@ package dev.aaronhowser.mods.geneticsresequenced.item
 import dev.aaronhowser.mods.geneticsresequenced.api.genes.Gene
 import dev.aaronhowser.mods.geneticsresequenced.datagen.ModLanguageProvider
 import dev.aaronhowser.mods.geneticsresequenced.datagen.ModLanguageProvider.Companion.toComponent
+import dev.aaronhowser.mods.geneticsresequenced.util.ClientUtil
 import dev.aaronhowser.mods.geneticsresequenced.util.OtherUtil.withColor
 import net.minecraft.ChatFormatting
+import net.minecraft.core.Holder
 import net.minecraft.network.chat.Component
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.item.Item
@@ -18,7 +20,7 @@ class GmoCell : Item(Properties()) {
         fun setDetails(
             itemStack: ItemStack,
             entityType: EntityType<*>,
-            gene: Gene,
+            gene: Holder<Gene>,
         ) {
 
             EntityDnaItem.setEntityType(itemStack, entityType)
@@ -49,11 +51,11 @@ class GmoCell : Item(Properties()) {
             pTooltipComponents.add(noEntityComponent)
         }
 
-        val gene = DnaHelixItem.getGene(pStack)
-        if (gene != null) {
+        val geneHolder = DnaHelixItem.getGene(pStack)
+        if (geneHolder != null) {
             val geneComponent =
                 ModLanguageProvider.Tooltips.GENE
-                    .toComponent(gene.nameComponent)
+                    .toComponent(geneHolder.value().nameComponent(ClientUtil.localRegistryAccess!!))
                     .withColor(ChatFormatting.GRAY)
             pTooltipComponents.add(geneComponent)
         } else {
