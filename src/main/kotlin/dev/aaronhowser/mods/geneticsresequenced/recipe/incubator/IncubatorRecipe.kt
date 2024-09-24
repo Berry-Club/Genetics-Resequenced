289013a577bfc34e64ed9a679522ffad7e49eb8e
Aaron Howser <aaronhowser1@gmail.com>
2024-09-23 21:43:17 -0500

    IncubatorRecipe now requires ingredients

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/recipe/incubator/IncubatorRecipe.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/recipe/incubator/IncubatorRecipe.kt
@@ -1,7 +1,17 @@
 package dev.aaronhowser.mods.geneticsresequenced.recipe.incubator
 
+import net.minecraft.core.NonNullList
+import net.minecraft.world.item.crafting.Ingredient
 import net.minecraft.world.item.crafting.Recipe
 
 abstract class IncubatorRecipe : Recipe<IncubatorRecipeInput> {
     override fun canCraftInDimensions(p0: Int, p1: Int): Boolean = true
+
+    abstract val ingredients: List<Ingredient>
+    override fun getIngredients(): NonNullList<Ingredient> {
+        val list = NonNullList.create<Ingredient>()
+        list.addAll(ingredients)
+
+        return list
+    }
 }
