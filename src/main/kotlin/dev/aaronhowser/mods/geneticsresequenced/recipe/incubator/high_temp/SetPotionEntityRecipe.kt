9d99ac11d35608a960618e48f1842519dd8e344b
Aaron Howser <aaronhowser1@gmail.com>
2024-09-23 22:16:56 -0500

    SetPotionEntityRecipe has ingredients now

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/recipe/incubator/high_temp/SetPotionEntityRecipe.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/recipe/incubator/high_temp/SetPotionEntityRecipe.kt
@@ -1,34 +1,38 @@
 package dev.aaronhowser.mods.geneticsresequenced.recipe.incubator.high_temp
 
 import com.mojang.serialization.MapCodec
+import dev.aaronhowser.mods.geneticsresequenced.datagen.tag.ModPotionTagsProvider
 import dev.aaronhowser.mods.geneticsresequenced.item.EntityDnaItem
+import dev.aaronhowser.mods.geneticsresequenced.recipe.PotionTagIngredient
 import dev.aaronhowser.mods.geneticsresequenced.recipe.incubator.IncubatorRecipe
 import dev.aaronhowser.mods.geneticsresequenced.recipe.incubator.IncubatorRecipeInput
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModItems
-import dev.aaronhowser.mods.geneticsresequenced.registry.ModPotions
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModRecipeSerializers
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModRecipeTypes
-import dev.aaronhowser.mods.geneticsresequenced.util.OtherUtil
 import net.minecraft.core.HolderLookup
 import net.minecraft.network.RegistryFriendlyByteBuf
 import net.minecraft.network.codec.StreamCodec
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.Items
+import net.minecraft.world.item.crafting.Ingredient
 import net.minecraft.world.item.crafting.RecipeSerializer
 import net.minecraft.world.item.crafting.RecipeType
 import net.minecraft.world.level.Level
 
 class SetPotionEntityRecipe : IncubatorRecipe() {
 
+    private val cellIngredient = Ingredient.of(ModItems.CELL.get())
+    private val potionIngredient = PotionTagIngredient(ModPotionTagsProvider.CAN_HAVE_ENTITY).toVanilla()
+
+    override val ingredients: List<Ingredient> = listOf(cellIngredient, potionIngredient)
+
     override fun matches(input: IncubatorRecipeInput, level: Level): Boolean {
-        val topItem = input.getTopItem()
-        val bottomItem = input.getBottomItem()
+        val cellStack = input.getTopItem()
+        val potionStack = input.getBottomItem()
 
-        if (topItem.item != ModItems.CELL.get()) return false
-        if (bottomItem.item != Items.POTION) return false
+        if (!cellIngredient.test(cellStack)) return false
+        if (!potionIngredient.test(potionStack)) return false
 
-        val inputPotion = OtherUtil.getPotion(bottomItem)
-        return inputPotion == ModPotions.CELL_GROWTH || inputPotion == ModPotions.MUTATION
+        return EntityDnaItem.hasEntity(cellStack)
     }
 
     override fun assemble(input: IncubatorRecipeInput, lookup: HolderLookup.Provider): ItemStack {
