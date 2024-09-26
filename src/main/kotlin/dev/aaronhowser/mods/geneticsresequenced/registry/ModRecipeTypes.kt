c05943444f8ee53139a2020e13b8c240194c4625
Aaron Howser <aaronhowser1@gmail.com>
2024-09-25 23:13:57 -0500

    refactor

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/ModRecipeTypes.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/ModRecipeTypes.kt
@@ -3,45 +3,47 @@ package dev.aaronhowser.mods.geneticsresequenced.registry
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
 import dev.aaronhowser.mods.geneticsresequenced.recipe.crafting.SetAntiPlasmidRecipe
 import dev.aaronhowser.mods.geneticsresequenced.recipe.crafting.UnsetAntiPlasmidRecipe
+import dev.aaronhowser.mods.geneticsresequenced.recipe.incubator.*
 import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.world.item.crafting.Recipe
 import net.minecraft.world.item.crafting.RecipeType
 import net.neoforged.neoforge.registries.DeferredHolder
 import net.neoforged.neoforge.registries.DeferredRegister
+import java.util.function.Supplier
 
 object ModRecipeTypes {
 
     val RECIPE_TYPES_REGISTRY: DeferredRegister<RecipeType<*>> =
         DeferredRegister.create(BuiltInRegistries.RECIPE_TYPE, GeneticsResequenced.ID)
 
-    val SET_ANTI_PLASMID: DeferredHolder<RecipeType<*>, RecipeType<*>> =
-        registerRecipeType("set_anti_plasmid") { object : RecipeType<SetAntiPlasmidRecipe> {} }
+    val SET_ANTI_PLASMID: DeferredHolder<RecipeType<*>, RecipeType<SetAntiPlasmidRecipe>> =
+        registerRecipeType("set_anti_plasmid")
 
-    val UNSET_ANTI_PLASMID: DeferredHolder<RecipeType<*>, RecipeType<*>> =
-        registerRecipeType("unset_anti_plasmid") { object : RecipeType<UnsetAntiPlasmidRecipe> {} }
+    val UNSET_ANTI_PLASMID: DeferredHolder<RecipeType<*>, RecipeType<UnsetAntiPlasmidRecipe>> =
+        registerRecipeType("unset_anti_plasmid")
 
-    val GMO: DeferredHolder<RecipeType<*>, RecipeType<*>> =
-        registerRecipeType("gmo") { object : RecipeType<SetAntiPlasmidRecipe> {} }
+    val GMO: DeferredHolder<RecipeType<*>, RecipeType<GmoRecipe>> =
+        registerRecipeType("incubator_gmo")
 
-    val SET_POTION_ENTITY: DeferredHolder<RecipeType<*>, RecipeType<*>> =
-        registerRecipeType("set_potion_entity") { object : RecipeType<SetAntiPlasmidRecipe> {} }
+    val SET_POTION_ENTITY: DeferredHolder<RecipeType<*>, RecipeType<SetPotionEntityRecipe>> =
+        registerRecipeType("set_potion_entity")
 
-    val DUPE_CELL: DeferredHolder<RecipeType<*>, RecipeType<*>> =
-        registerRecipeType("dupe_cell") { object : RecipeType<SetAntiPlasmidRecipe> {} }
+    val DUPE_CELL: DeferredHolder<RecipeType<*>, RecipeType<DupeCellRecipe>> =
+        registerRecipeType("dupe_cell")
 
-    val VIRUS: DeferredHolder<RecipeType<*>, RecipeType<*>> =
-        registerRecipeType("virus") { object : RecipeType<SetAntiPlasmidRecipe> {} }
+    val VIRUS: DeferredHolder<RecipeType<*>, RecipeType<VirusRecipe>> =
+        registerRecipeType("incubator_virus")
 
-    val BLACK_DEATH: DeferredHolder<RecipeType<*>, RecipeType<*>> =
-        registerRecipeType("black_death") { object : RecipeType<SetAntiPlasmidRecipe> {} }
+    val BLACK_DEATH: DeferredHolder<RecipeType<*>, RecipeType<BlackDeathRecipe>> =
+        registerRecipeType("black_death")
 
-    val BASIC_INCUBATOR: DeferredHolder<RecipeType<*>, RecipeType<*>> =
-        registerRecipeType("incubator") { object : RecipeType<SetAntiPlasmidRecipe> {} }
+    val BASIC_INCUBATOR: DeferredHolder<RecipeType<*>, RecipeType<BasicIncubatorRecipe>> =
+        registerRecipeType("basic_incubator")
 
-    private fun registerRecipeType(
-        name: String,
-        factory: () -> RecipeType<*>
-    ): DeferredHolder<RecipeType<*>, RecipeType<*>> {
-        return RECIPE_TYPES_REGISTRY.register(name, factory)
+    private fun <T : Recipe<*>> registerRecipeType(
+        name: String
+    ): DeferredHolder<RecipeType<*>, RecipeType<T>> {
+        return RECIPE_TYPES_REGISTRY.register(name, Supplier { object : RecipeType<T> {} })
     }
 
 
