f9d11568d62b2d10174708a2ab3eac3766fe95a8
Aaron Howser <aaronhowser1@gmail.com>
2024-09-25 20:07:24 -0500

    Revert "Made AbstractEmiIncubatorRecipe not use brewing background"
    This reverts commit 5d2069bf81638e7fe361578f3860fb234e232c56.

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/compatibility/emi/recipe/machine/incubator/AbstractEmiIncubatorRecipe.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/compatibility/emi/recipe/machine/incubator/AbstractEmiIncubatorRecipe.kt
@@ -3,17 +3,25 @@ package dev.aaronhowser.mods.geneticsresequenced.compatibility.emi.recipe.machin
 import dev.emi.emi.api.recipe.EmiRecipe
 import dev.emi.emi.api.recipe.EmiRecipeCategory
 import dev.emi.emi.api.recipe.VanillaEmiRecipeCategories
-import dev.emi.emi.api.render.EmiTexture
 import dev.emi.emi.api.stack.EmiIngredient
 import dev.emi.emi.api.stack.EmiStack
 import dev.emi.emi.api.widget.WidgetHolder
 import net.minecraft.network.chat.Component
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.Items
 
 abstract class AbstractEmiIncubatorRecipe : EmiRecipe {
+
+    companion object {
+        val BACKGROUND: ResourceLocation =
+            ResourceLocation.withDefaultNamespace("textures/gui/container/brewing_stand.png")
+    }
 
     abstract val ingredient: EmiIngredient
     abstract val input: EmiIngredient
     abstract val output: EmiStack
+
+    open val tertiaryItem: EmiStack = EmiStack.of(Items.BLAZE_POWDER)
 
     override fun getCategory(): EmiRecipeCategory {
         return VanillaEmiRecipeCategories.BREWING
@@ -28,20 +36,24 @@ abstract class AbstractEmiIncubatorRecipe : EmiRecipe {
     }
 
     override fun getDisplayWidth(): Int {
-        return 102
+        return 120
     }
 
     override fun getDisplayHeight(): Int {
-        return 18
+        return 61
     }
 
     open val tooltips: List<Component> = emptyList()
 
     override fun addWidgets(widgets: WidgetHolder) {
-        widgets.addSlot(input, 0, 0)
-        widgets.addSlot(ingredient, 24, 0)
-        widgets.addTexture(EmiTexture.EMPTY_ARROW, 48, 1)
-        widgets.addSlot(output, 78, 0).recipeContext(this)
+        widgets.addTexture(BACKGROUND, 0, 0, 103, 61, 16, 14)
+        widgets.addAnimatedTexture(BACKGROUND, 81, 2, 9, 28, 176, 0, 1000 * 20, false, false, false)
+        widgets.addAnimatedTexture(BACKGROUND, 47, 0, 12, 29, 185, 0, 700, false, true, false)
+        widgets.addTexture(BACKGROUND, 44, 30, 18, 4, 176, 29);
+        widgets.addSlot(tertiaryItem, 0, 2).drawBack(false)
+        widgets.addSlot(input, 39, 36).drawBack(false)
+        widgets.addSlot(ingredient, 62, 2).drawBack(false)
+        widgets.addSlot(output, 85, 36).drawBack(false).recipeContext(this)
 
         widgets.addTooltipText(tooltips, 0, 0, displayWidth, displayHeight)
     }
