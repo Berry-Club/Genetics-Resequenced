b2f7a8e83d9bfe6581f40c3abbb4ffff36133da6
Aaron Howser <aaronhowser1@gmail.com>
2024-12-31 07:11:51 -0600

    Revert "working on getting automatic gene requirements"
    This reverts commit 7a9b5903d3ea7791c9e7dd276fe63119d2515697.

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/modonomicon/entries/GeneEntryProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/modonomicon/entries/GeneEntryProvider.kt
@@ -1,7 +1,7 @@
 package dev.aaronhowser.mods.geneticsresequenced.datagen.modonomicon.entries
 
 import com.klikli_dev.modonomicon.api.datagen.CategoryProviderBase
-import dev.aaronhowser.mods.geneticsresequenced.datagen.gene.ModGeneRequirementsProvider
+import dev.aaronhowser.mods.geneticsresequenced.data.GeneRequirements
 import dev.aaronhowser.mods.geneticsresequenced.gene.Gene
 import dev.aaronhowser.mods.geneticsresequenced.item.PlasmidItem
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModGenes.getHolderOrThrow
@@ -67,18 +67,16 @@ abstract class GeneEntryProvider : BaseEntryProvider {
 
         spotlightPageText.append(canMobsHaveText)
 
-        val requiredGenes = ModGeneRequirementsProvider.DEFAULT[geneHolder.key!!]
+        //FIXME: This doesn't work
+        val geneRequirements = GeneRequirements.getGeneRequiredGeneHolders(geneHolder, registries())
 
-        if (!requiredGenes.isNullOrEmpty()) {
-
+        if (geneRequirements.isNotEmpty()) {
             spotlightPageText.append("\n\n")
-            spotlightPageText.append("By default, this Gene ${bad("requires")} the following Genes to be present:")
-
-            for (requiredGene in requiredGenes) {
-                val requiredGeneHolder = requiredGene.getHolderOrThrow(registries())
-                val requiredGeneName = Gene.getNameComponent(requiredGeneHolder)
-
-                spotlightPageText.append("\n - ${requiredGeneName.string}")
+            spotlightPageText.append("This Gene ${bad("requires")} the following Genes to be present:")
+            spotlightPageText.append("\n")
+            geneRequirements.forEach {
+                spotlightPageText.append(" - ${it.key}")
+                spotlightPageText.append("\n")
             }
         }
 
