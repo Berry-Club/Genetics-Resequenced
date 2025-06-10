75e09d7c04bfa91ba38b47422f02c0f45103c8b9
aaron <aaronhowser1@gmail.com>
2025-06-10 09:36:39 -0500

    refactor

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/ModItems.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/ModItems.kt
@@ -12,32 +12,20 @@ object ModItems {
 
 	val ITEM_REGISTRY: DeferredRegister.Items = DeferredRegister.createItems(GeneticsResequenced.ID)
 
-	val SCRAPER: DeferredItem<ScraperItem> =
-		ITEM_REGISTRY.registerItem("scraper") { ScraperItem() }
-	val SYRINGE: DeferredItem<SyringeItem> =
-		ITEM_REGISTRY.registerItem("syringe") { SyringeItem() }
-	val METAL_SYRINGE: DeferredItem<MetalSyringeItem> =
-		ITEM_REGISTRY.registerItem("metal_syringe") { MetalSyringeItem() }
-	val GENE_CHECKER: DeferredItem<GeneCheckerItem> =
-		ITEM_REGISTRY.registerItem("gene_checker") { GeneCheckerItem() }
-	val ORGANIC_MATTER: DeferredItem<EntityDnaItem> =
-		ITEM_REGISTRY.registerItem("organic_matter") { EntityDnaItem() }
-	val CELL: DeferredItem<EntityDnaItem> =
-		ITEM_REGISTRY.registerItem("cell") { EntityDnaItem() }
-	val GMO_CELL: DeferredItem<GmoCell> =
-		ITEM_REGISTRY.registerItem("gmo_cell") { GmoCell() }
-	val DNA_HELIX: DeferredItem<DnaHelixItem> =
-		ITEM_REGISTRY.registerItem("dna_helix") { DnaHelixItem() }
-	val PLASMID: DeferredItem<PlasmidItem> =
-		ITEM_REGISTRY.registerItem("plasmid") { PlasmidItem() }
-	val ANTI_PLASMID: DeferredItem<AntiPlasmidItem> =
-		ITEM_REGISTRY.registerItem("anti_plasmid") { AntiPlasmidItem() }
+	val SCRAPER: DeferredItem<ScraperItem> = registerNoArg("scraper", ::ScraperItem)
+	val SYRINGE: DeferredItem<SyringeItem> = registerNoArg("syringe", ::SyringeItem)
+	val METAL_SYRINGE: DeferredItem<MetalSyringeItem> = registerNoArg("metal_syringe", ::MetalSyringeItem)
+	val GENE_CHECKER: DeferredItem<GeneCheckerItem> = registerNoArg("gene_checker", ::GeneCheckerItem)
+	val ORGANIC_MATTER: DeferredItem<EntityDnaItem> = registerNoArg("organic_matter", ::EntityDnaItem)
+	val CELL: DeferredItem<EntityDnaItem> = registerNoArg("cell", ::EntityDnaItem)
+	val GMO_CELL: DeferredItem<GmoCell> = registerNoArg("gmo_cell", ::GmoCell)
+	val DNA_HELIX: DeferredItem<DnaHelixItem> = registerNoArg("dna_helix", ::DnaHelixItem)
+	val PLASMID: DeferredItem<PlasmidItem> = registerNoArg("plasmid", ::PlasmidItem)
+	val ANTI_PLASMID: DeferredItem<AntiPlasmidItem> = registerNoArg("anti_plasmid", ::AntiPlasmidItem)
 	val OVERCLOCKER: DeferredItem<Item> =
 		ITEM_REGISTRY.registerSimpleItem("overclocker", Item.Properties().stacksTo(8))
-	val ANTI_FIELD_ORB: DeferredItem<AntiFieldOrbItem> =
-		ITEM_REGISTRY.registerItem("anti_field_orb") { AntiFieldOrbItem() }
-	val DRAGON_HEALTH_CRYSTAL: DeferredItem<DragonHealthCrystal> =
-		ITEM_REGISTRY.registerItem("dragon_health_crystal") { DragonHealthCrystal() }
+	val ANTI_FIELD_ORB: DeferredItem<AntiFieldOrbItem> = registerNoArg("anti_field_orb", ::AntiFieldOrbItem)
+	val DRAGON_HEALTH_CRYSTAL: DeferredItem<DragonHealthCrystal> = registerNoArg("dragon_health_crystal", ::DragonHealthCrystal)
 	val FRIENDLY_SLIME_SPAWN_EGG: DeferredItem<SpawnEggItem> =
 		ITEM_REGISTRY.registerItem("support_slime_spawn_egg") {
 			DeferredSpawnEggItem(
@@ -47,5 +35,9 @@ object ModItems {
 				Item.Properties()
 			)
 		}
+
+	private fun <T : Item> registerNoArg(name: String, item: () -> T): DeferredItem<T> {
+		return ITEM_REGISTRY.registerItem(name) { item() }
+	}
 
 }
