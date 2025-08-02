e3a4dad994f229ec01f23ce04ddce707a16c32f7
aaron <aaronhowser1@gmail.com>
2025-08-02 02:37:32 -0500

    refactor: update block registration to use constructor references

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/ModBlocks.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/ModBlocks.kt
@@ -21,28 +21,28 @@ object ModBlocks {
 	val BLOCK_REGISTRY: DeferredRegister.Blocks = DeferredRegister.createBlocks(GeneticsResequenced.ID)
 
 	val BIOLUMINESCENCE_BLOCK: DeferredBlock<BioluminescenceBlock> =
-		registerBlock("bioluminescence") { BioluminescenceBlock() }
+		registerBlock("bioluminescence", ::BioluminescenceBlock)
 	val ANTI_FIELD_BLOCK: DeferredBlock<AntiFieldBlock> =
-		registerBlock("anti_field_block") { AntiFieldBlock() }
+		registerBlock("anti_field_block", ::AntiFieldBlock)
 
 	val COAL_GENERATOR: DeferredBlock<CoalGeneratorBlock> =
-		registerBlock("coal_generator") { CoalGeneratorBlock() }
+		registerBlock("coal_generator", ::CoalGeneratorBlock)
 	val CELL_ANALYZER: DeferredBlock<CellAnalyzerBlock> =
-		registerBlock("cell_analyzer") { CellAnalyzerBlock() }
+		registerBlock("cell_analyzer", ::CellAnalyzerBlock)
 	val DNA_EXTRACTOR: DeferredBlock<DnaExtractorBlock> =
-		registerBlock("dna_extractor") { DnaExtractorBlock() }
+		registerBlock("dna_extractor", ::DnaExtractorBlock)
 	val DNA_DECRYPTOR: DeferredBlock<DnaDecryptorBlock> =
-		registerBlock("dna_decryptor") { DnaDecryptorBlock() }
+		registerBlock("dna_decryptor", ::DnaDecryptorBlock)
 	val BLOOD_PURIFIER: DeferredBlock<BloodPurifierBlock> =
-		registerBlock("blood_purifier") { BloodPurifierBlock() }
+		registerBlock("blood_purifier", ::BloodPurifierBlock)
 	val PLASMID_INFUSER: DeferredBlock<PlasmidInfuserBlock> =
-		registerBlock("plasmid_infuser") { PlasmidInfuserBlock() }
+		registerBlock("plasmid_infuser", ::PlasmidInfuserBlock)
 	val PLASMID_INJECTOR: DeferredBlock<PlasmidInjectorBlock> =
-		registerBlock("plasmid_injector") { PlasmidInjectorBlock() }
+		registerBlock("plasmid_injector", ::PlasmidInjectorBlock)
 	val INCUBATOR: DeferredBlock<IncubatorBlock> =
-		registerBlock("incubator") { IncubatorBlock() }
+		registerBlock("incubator", ::IncubatorBlock)
 	val ADVANCED_INCUBATOR: DeferredBlock<Block> =
-		registerBlock("advanced_incubator") { AdvancedIncubatorBlock() }
+		registerBlock("advanced_incubator", ::AdvancedIncubatorBlock)
 
 	private fun <T : Block> registerBlock(
 		name: String,
