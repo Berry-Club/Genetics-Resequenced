0bfd68247448f0b423404d8a1f091ecd1bdddc12
aaron <aaronhowser1@gmail.com>
2025-11-13 23:47:33 -0600

    progress

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/block/block_entity/PlasmidInjectorBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/block/block_entity/PlasmidInjectorBlockEntity.kt
@@ -1,12 +1,18 @@
 package dev.aaronhowser.mods.geneticsresequenced.block.block_entity
 
 import dev.aaronhowser.mods.geneticsresequenced.block.base.CraftingMachineBlockEntity
+import dev.aaronhowser.mods.geneticsresequenced.item.PlasmidItem
+import dev.aaronhowser.mods.geneticsresequenced.item.SyringeItem
+import dev.aaronhowser.mods.geneticsresequenced.item.SyringeItem.Companion.isSyringe
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModBlockEntityTypes
+import dev.aaronhowser.mods.geneticsresequenced.registry.ModItems
 import net.minecraft.core.BlockPos
 import net.minecraft.world.entity.player.Inventory
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.inventory.AbstractContainerMenu
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.block.state.BlockState
+import net.neoforged.neoforge.items.wrapper.InvWrapper
 import java.util.function.IntSupplier
 
 class PlasmidInjectorBlockEntity(
@@ -14,16 +20,62 @@ class PlasmidInjectorBlockEntity(
 	blockState: BlockState
 ) : CraftingMachineBlockEntity(ModBlockEntityTypes.PLASMID_INJECTOR.get(), pos, blockState) {
 
-	override val baseEnergyCostPerTick: IntSupplier = IntSupplier { 32}
+	override val baseEnergyCostPerTick: IntSupplier = IntSupplier { 32 }
 	override val maxEnergy: Int = 60_000
 	override val energyTransferRate: Int = 256
 
+	override val invWrapper: InvWrapper = object : InvWrapper(container) {
+		override fun isItemValid(slot: Int, stack: ItemStack): Boolean {
+			return when (slot) {
+				INPUT_SLOT_INDEX -> {
+					val isIncompletePlasmid = stack.`is`(ModItems.PLASMID) && PlasmidItem.isComplete(stack)
+					isIncompletePlasmid || (stack.`is`(ModItems.ANTI_PLASMID))
+				}
+
+				OUTPUT_SLOT_INDEX ->
+					SyringeItem.hasBlood(stack) && !SyringeItem.isContaminated(stack)
+
+				OVERCLOCK_SLOT_INDEX ->
+					stack.item == ModItems.OVERCLOCKER.get()
+
+				else -> false
+			}
+		}
+	}
+
 	override fun hasRecipe(): Boolean {
-		TODO("Not yet implemented")
+		val plasmidStack = invWrapper.getStackInSlot(INPUT_SLOT_INDEX)
+		val syringeStack = invWrapper.getStackInSlot(OUTPUT_SLOT_INDEX)
+
+		if (!syringeStack.isSyringe()) return false
+
+		if (plasmidStack.`is`(ModItems.PLASMID)) {
+			val plasmidGene = PlasmidItem.getGene(plasmidStack) ?: return false
+			if (!PlasmidItem.isComplete(plasmidStack)) return false
+			return SyringeItem.canAddGene(syringeStack, plasmidGene)
+		}
+
+		if (plasmidStack.`is`(ModItems.ANTI_PLASMID)) {
+			val antiPlasmidAntigene = PlasmidItem.getGene(plasmidStack) ?: return false
+			return SyringeItem.canAddAntigene(syringeStack, antiPlasmidAntigene)
+		}
+
+		return false
 	}
 
 	override fun craftItem() {
-		TODO("Not yet implemented")
+		val plasmidStack = invWrapper.getStackInSlot(INPUT_SLOT_INDEX)
+		val syringeStack = invWrapper.getStackInSlot(OUTPUT_SLOT_INDEX)
+
+		val plasmidGene = PlasmidItem.getGene(plasmidStack) ?: return
+
+		if (plasmidStack.`is`(ModItems.PLASMID)) {
+			SyringeItem.addGene(syringeStack, plasmidGene)
+		} else if (plasmidStack.`is`(ModItems.ANTI_PLASMID)) {
+			SyringeItem.addAntigene(syringeStack, plasmidGene)
+		}
+
+		invWrapper.extractItem(INPUT_SLOT_INDEX, 1, false)
 	}
 
 	override fun createMenu(containerId: Int, playerInventory: Inventory, player: Player): AbstractContainerMenu? {
