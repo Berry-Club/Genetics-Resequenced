af8af0c487d8ce5964afdcb079c2ae921681d8ec
aaron <aaronhowser1@gmail.com>
2025-11-13 19:25:25 -0600

    progress

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/menu/MachineScreen.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/menu/MachineScreen.kt
@@ -2,18 +2,27 @@ package dev.aaronhowser.mods.geneticsresequenced.menu
 
 import dev.aaronhowser.mods.aaron.menu.BaseScreen
 import dev.aaronhowser.mods.geneticsresequenced.menu.components.EnergyBar
+import dev.aaronhowser.mods.geneticsresequenced.menu.components.ProgressArrow
 import net.minecraft.network.chat.Component
 import net.minecraft.world.entity.player.Inventory
-import net.minecraft.world.inventory.AbstractContainerMenu
 
-abstract class MachineScreen<T : AbstractContainerMenu>(
+abstract class MachineScreen<T : MachineMenu>(
 	menu: T,
 	playerInventory: Inventory,
 	title: Component
 ) : BaseScreen<T>(menu, playerInventory, title) {
 
 	protected lateinit var energyBar: EnergyBar
-	protected
+	protected open val energyPosLeft: Int = 7
+	protected open val energyPosTop: Int = 14
+
+	protected lateinit var progressArrow: ProgressArrow
+	protected open val arrowDirection: ProgressArrow.ArrowDirection = ProgressArrow.ArrowDirection.RIGHT
+	protected open val arrowLeftPos: Int = 83
+	protected open val arrowTopPos: Int = 43
+
+	protected open fun arrowPercentDone(): Float = menu.getPercentDone()
+	protected open fun shouldRenderProgressArrow(): Boolean = true
 
 	override fun baseInit() {
 		super.baseInit()
