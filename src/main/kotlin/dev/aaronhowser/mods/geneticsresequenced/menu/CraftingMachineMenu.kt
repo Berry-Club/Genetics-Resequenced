6ff4fb088c51b835d0123da75075ecf79f9d82c2
aaron <aaronhowser1@gmail.com>
2025-11-16 17:45:41 -0600

    outputSlot

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/menu/CraftingMachineMenu.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/menu/CraftingMachineMenu.kt
@@ -1,6 +1,7 @@
 package dev.aaronhowser.mods.geneticsresequenced.menu
 
 import dev.aaronhowser.mods.aaron.menu.components.FilteredSlot
+import dev.aaronhowser.mods.aaron.menu.components.OutputSlot
 import dev.aaronhowser.mods.geneticsresequenced.block.base.CraftingMachineBlockEntity
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModItems
 import net.minecraft.world.Container
@@ -32,7 +33,7 @@ abstract class CraftingMachineMenu(
 
 	override fun addSlots() {
 		val inputSlot = FilteredSlot(machineContainer, CraftingMachineBlockEntity.INPUT_SLOT_INDEX, 63, 42, ::inputFilter)
-		val outputSlot = Slot(machineContainer, CraftingMachineBlockEntity.OUTPUT_SLOT_INDEX, 110, 42)
+		val outputSlot = OutputSlot(machineContainer, CraftingMachineBlockEntity.OUTPUT_SLOT_INDEX, 110, 42)
 		val overclockSlot = FilteredSlot(machineContainer, CraftingMachineBlockEntity.OVERCLOCK_SLOT_INDEX, 26, 54) { it.`is`(ModItems.OVERCLOCKER) }
 
 		this.addSlot(inputSlot)
