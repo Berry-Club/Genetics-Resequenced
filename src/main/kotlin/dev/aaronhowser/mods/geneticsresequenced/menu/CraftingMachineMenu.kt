ae215b5f2d9fb0d55ddc58800f1f765f24fb53cf
aaron <aaronhowser1@gmail.com>
2025-11-16 18:55:39 -0600

    auto cleanup

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/menu/CraftingMachineMenu.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/menu/CraftingMachineMenu.kt
@@ -9,7 +9,6 @@ import net.minecraft.world.entity.player.Inventory
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.inventory.ContainerData
 import net.minecraft.world.inventory.MenuType
-import net.minecraft.world.inventory.Slot
 import net.minecraft.world.item.ItemStack
 
 abstract class CraftingMachineMenu(
--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/menu/plasmid_infuser/PlasmidInfuserMenu.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/menu/plasmid_infuser/PlasmidInfuserMenu.kt
@@ -1,7 +1,6 @@
 package dev.aaronhowser.mods.geneticsresequenced.menu.plasmid_infuser
 
 import dev.aaronhowser.mods.aaron.menu.components.FilteredSlot
-import dev.aaronhowser.mods.aaron.menu.components.OutputSlot
 import dev.aaronhowser.mods.geneticsresequenced.block.base.CraftingMachineBlockEntity
 import dev.aaronhowser.mods.geneticsresequenced.block.base.MachineBlockEntity
 import dev.aaronhowser.mods.geneticsresequenced.menu.CraftingMachineMenu
