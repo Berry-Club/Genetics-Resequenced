8d3e0445afff1491c18e06164e093503a92b1dc7
aaron <aaronhowser1@gmail.com>
2025-11-16 20:50:43 -0600

    move slot

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/menu/advanced_incubator/AdvancedIncubatorMenu.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/menu/advanced_incubator/AdvancedIncubatorMenu.kt
@@ -63,7 +63,7 @@ class AdvancedIncubatorMenu(
 		val leftBottleSlot = FilteredSlot(machineContainer, AdvancedIncubatorBlockEntity.LEFT_BOTTLE_SLOT_INDEX, 60, 55) { AbstractIncubatorRecipe.isValidBottomIngredient(level, it) }
 		val middleBottleSlot = FilteredSlot(machineContainer, AdvancedIncubatorBlockEntity.MIDDLE_BOTTLE_SLOT_INDEX, 83, 62) { AbstractIncubatorRecipe.isValidBottomIngredient(level, it) }
 		val rightBottleSlot = FilteredSlot(machineContainer, AdvancedIncubatorBlockEntity.RIGHT_BOTTLE_SLOT_INDEX, 106, 55) { AbstractIncubatorRecipe.isValidBottomIngredient(level, it) }
-		val chorusSlot = FilteredSlot(machineContainer, AdvancedIncubatorBlockEntity.CHORUS_SLOT_INDEX, 141, 30) { it.`is`(Items.CHORUS_FRUIT) }
+		val chorusSlot = FilteredSlot(machineContainer, AdvancedIncubatorBlockEntity.CHORUS_SLOT_INDEX, 141, 38) { it.`is`(Items.CHORUS_FRUIT) }
 
 		val overclockerSlot = FilteredSlot(machineContainer, AdvancedIncubatorBlockEntity.OVERCLOCKER_SLOT_INDEX, 141, 60) { it.`is`(ModItems.OVERCLOCKER) }
 
