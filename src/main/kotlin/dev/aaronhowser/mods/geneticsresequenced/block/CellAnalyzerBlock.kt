1950f3d70ab1fcb9ea3d1b25343eec7ae765d325
aaron <aaronhowser1@gmail.com>
2025-11-16 18:08:50 -0600

    fix

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/block/CellAnalyzerBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/block/CellAnalyzerBlock.kt
@@ -1,7 +1,7 @@
 package dev.aaronhowser.mods.geneticsresequenced.block
 
 import dev.aaronhowser.mods.geneticsresequenced.block.base.MachineBlock
-import dev.aaronhowser.mods.geneticsresequenced.block.block_entity.BloodPurifierBlockEntity
+import dev.aaronhowser.mods.geneticsresequenced.block.block_entity.CellAnalyzerBlockEntity
 import net.minecraft.core.BlockPos
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
@@ -9,7 +9,7 @@ import net.minecraft.world.level.block.state.BlockState
 class CellAnalyzerBlock : MachineBlock() {
 
 	override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
-		return BloodPurifierBlockEntity(pos, state)
+		return CellAnalyzerBlockEntity(pos, state)
 	}
 
 }
