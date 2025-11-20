11d36ee41c5d1a09e4c09c39e1a6815f87ff8e1c
aaron <aaronhowser1@gmail.com>
2025-11-20 04:53:23 -0600

    feat: update aaron library version and refactor block and item registries

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/ModDataComponents.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/ModDataComponents.kt
@@ -1,6 +1,5 @@
 package dev.aaronhowser.mods.geneticsresequenced.registry
 
-import com.mojang.serialization.Codec
 import dev.aaronhowser.mods.aaron.registry.AaronDataComponentRegistry
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
 import dev.aaronhowser.mods.geneticsresequenced.gene.Gene
@@ -24,18 +23,18 @@ object ModDataComponents : AaronDataComponentRegistry() {
 	override fun getDataComponentRegistry(): DeferredRegister.DataComponents = DATA_COMPONENT_REGISTRY
 
 	val ENTITY_TYPE: DeferredHolder<DataComponentType<*>, DataComponentType<EntityType<*>>> =
-		DATA_COMPONENT_REGISTRY.registerComponentType("entity_type") {
-			it
-				.persistent(BuiltInRegistries.ENTITY_TYPE.byNameCodec())
-				.networkSynchronized(ByteBufCodecs.registry(Registries.ENTITY_TYPE))
-		}
+		register(
+			"entity_type",
+			BuiltInRegistries.ENTITY_TYPE.byNameCodec(),
+			ByteBufCodecs.registry(Registries.ENTITY_TYPE)
+		)
 
 	val SPECIFIC_ENTITY: DeferredHolder<DataComponentType<*>, DataComponentType<SpecificEntityItemComponent>> =
-		DATA_COMPONENT_REGISTRY.registerComponentType("specific_entity") {
-			it
-				.persistent(SpecificEntityItemComponent.CODEC)
-				.networkSynchronized(SpecificEntityItemComponent.STREAM_CODEC)
-		}
+		register(
+			"specific_entity",
+			SpecificEntityItemComponent.CODEC,
+			SpecificEntityItemComponent.STREAM_CODEC
+		)
 
 	val IS_ACTIVE: DeferredHolder<DataComponentType<*>, DataComponentType<Boolean>> =
 		boolean("is_active")
@@ -47,18 +46,10 @@ object ModDataComponents : AaronDataComponentRegistry() {
 		boolean("is_infinity_arrow")
 
 	val DRAGON_HEALTH_CRYSTAL_DAMAGE: DeferredHolder<DataComponentType<*>, DataComponentType<Float>> =
-		DATA_COMPONENT_REGISTRY.registerComponentType("dragon_health_crystal_damage") {
-			it
-				.persistent(Codec.FLOAT)
-				.networkSynchronized(ByteBufCodecs.FLOAT)
-		}
+		float("dragon_health_crystal_damage")
 
 	val GENE: DeferredHolder<DataComponentType<*>, DataComponentType<Holder<Gene>>> =
-		DATA_COMPONENT_REGISTRY.registerComponentType("gene") {
-			it
-				.persistent(Gene.CODEC)
-				.networkSynchronized(Gene.STREAM_CODEC)
-		}
+		register("gene", Gene.CODEC, Gene.STREAM_CODEC)
 
 	val GENE_SET: DeferredHolder<DataComponentType<*>, DataComponentType<HolderSet<Gene>>> =
 		geneList("genes")
@@ -67,24 +58,18 @@ object ModDataComponents : AaronDataComponentRegistry() {
 		geneList("antigenes")
 
 	val PLASMID_PROGRESS: DeferredHolder<DataComponentType<*>, DataComponentType<PlasmidProgressItemComponent>> =
-		DATA_COMPONENT_REGISTRY.registerComponentType("plasmid_progress") {
-			it
-				.persistent(PlasmidProgressItemComponent.CODEC)
-				.networkSynchronized(PlasmidProgressItemComponent.STREAM_CODEC)
-		}
+		register(
+			"plasmid_progress",
+			PlasmidProgressItemComponent.CODEC,
+			PlasmidProgressItemComponent.STREAM_CODEC
+		)
 
-	private fun geneList(name: String): DeferredHolder<DataComponentType<*>, DataComponentType<HolderSet<Gene>>> =
-		DATA_COMPONENT_REGISTRY.registerComponentType(name) {
-			it
-				.persistent(Gene.HOLDER_SET_CODEC)
-				.networkSynchronized(Gene.HOLDER_SET_STREAM_CODEC)
-		}
-
-	private fun boolean(name: String): DeferredHolder<DataComponentType<*>, DataComponentType<Boolean>> =
-		DATA_COMPONENT_REGISTRY.registerComponentType(name) {
-			it
-				.persistent(Codec.BOOL)
-				.networkSynchronized(ByteBufCodecs.BOOL)
-		}
+	private fun geneList(name: String): DeferredHolder<DataComponentType<*>, DataComponentType<HolderSet<Gene>>> {
+		return register(
+			name,
+			Gene.HOLDER_SET_CODEC,
+			Gene.HOLDER_SET_STREAM_CODEC
+		)
+	}
 
 }
