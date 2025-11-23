33efafb27683c2c82706d8acfc0c206c75eff38a
aaron <aaronhowser1@gmail.com>
2025-11-23 06:23:33 -0600

    >:(

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/loot/ModBlockLootTablesSubProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/loot/ModBlockLootTablesSubProvider.kt
@@ -5,27 +5,41 @@ import net.minecraft.core.HolderLookup
 import net.minecraft.data.loot.BlockLootSubProvider
 import net.minecraft.world.flag.FeatureFlags
 import net.minecraft.world.level.block.Block
+import net.neoforged.neoforge.registries.DeferredHolder
 
 class ModBlockLootTablesSubProvider(
 	provider: HolderLookup.Provider
 ) : BlockLootSubProvider(setOf(), FeatureFlags.REGISTRY.allFlags(), provider) {
 
 	override fun generate() {
-		for (block in knownBlocks) {
+		val dropSelf = listOf(
+			ModBlocks.ANTI_FIELD_BLOCK.get(),
+			ModBlocks.COAL_GENERATOR.get(),
+			ModBlocks.CELL_ANALYZER.get(),
+			ModBlocks.DNA_EXTRACTOR.get(),
+			ModBlocks.DNA_DECRYPTOR.get(),
+			ModBlocks.BLOOD_PURIFIER.get(),
+			ModBlocks.PLASMID_INFUSER.get(),
+			ModBlocks.PLASMID_INJECTOR.get(),
+			ModBlocks.INCUBATOR.get(),
+			ModBlocks.ADVANCED_INCUBATOR.get()
+		)
+
+		for (block in dropSelf) {
 			dropSelf(block)
 		}
 
-//		for (block in noDropBlocks) {
-//			add(block, noDrop())
-//		}
+		val noDrop = listOf(
+			ModBlocks.WEB_DEFENSE_BLOCK.get()
+		)
+
+		for (block in noDrop) {
+			add(block, noDrop())
+		}
 	}
 
-	private val noDropBlocks = setOf(
-		ModBlocks.BIOLUMINESCENCE_BLOCK.get()
-	)
-
 	override fun getKnownBlocks(): List<Block> {
-		return ModBlocks.BLOCK_REGISTRY.entries.map { it.get() } - ModBlocks.BIOLUMINESCENCE_BLOCK.get()
+		return ModBlocks.BLOCK_REGISTRY.entries.map(DeferredHolder<Block, out Block>::get)
 	}
 
 }
