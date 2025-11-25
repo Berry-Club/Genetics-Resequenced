9476887306e5da6d747d755edb68325da5df3462
aaron <aaronhowser1@gmail.com>
2025-11-25 14:45:13 -0600

    effects

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/ModPotions.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/registry/ModPotions.kt
@@ -1,37 +1,40 @@
 package dev.aaronhowser.mods.geneticsresequenced.registry
 
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
-import net.minecraft.core.registries.Registries
 import net.minecraft.world.effect.MobEffect
 import net.minecraft.world.effect.MobEffectInstance
 import net.minecraft.world.item.alchemy.Potion
-import net.neoforged.neoforge.registries.DeferredHolder
-import net.neoforged.neoforge.registries.DeferredRegister
+import net.minecraftforge.registries.DeferredRegister
+import net.minecraftforge.registries.ForgeRegistries
+import net.minecraftforge.registries.RegistryObject
 import java.util.function.Supplier
 
 object ModPotions {
 
 	val POTION_REGISTRY: DeferredRegister<Potion> =
-		DeferredRegister.create(Registries.POTION, GeneticsResequenced.ID)
+		DeferredRegister.create(ForgeRegistries.POTIONS, GeneticsResequenced.ID)
 
-	val SUBSTRATE: DeferredHolder<Potion, Potion> =
+	val SUBSTRATE: RegistryObject<Potion> =
 		register("substrate", ModEffects.SUBSTRATE)
-	val CELL_GROWTH: DeferredHolder<Potion, Potion> =
+	val CELL_GROWTH: RegistryObject<Potion> =
 		register("cell_growth", ModEffects.CELL_GROWTH)
-	val MUTATION: DeferredHolder<Potion, Potion> =
+	val MUTATION: RegistryObject<Potion> =
 		register("mutation", ModEffects.MUTATION)
-	val VIRAL_AGENTS: DeferredHolder<Potion, Potion> =
+	val VIRAL_AGENTS: RegistryObject<Potion> =
 		register("viral_agents", ModEffects.VIRAL_AGENTS)
 
-	val PANACEA: DeferredHolder<Potion, Potion> =
+	val PANACEA: RegistryObject<Potion> =
 		register("panacea", ModEffects.PANACEA)
-	val ZOMBIFY_VILLAGER: DeferredHolder<Potion, Potion> =
+	val ZOMBIFY_VILLAGER: RegistryObject<Potion> =
 		register("zombify_villager", ModEffects.ZOMBIFY_VILLAGER)
 
-	private fun register(id: String, effect: DeferredHolder<MobEffect, out MobEffect>): DeferredHolder<Potion, Potion> {
+	private fun register(
+		id: String,
+		effect: RegistryObject<out MobEffect>
+	): RegistryObject<Potion> {
 		val potionId = "${GeneticsResequenced.ID}.$id"  // Required for localization
 
-		return POTION_REGISTRY.register(id, Supplier { Potion(potionId, MobEffectInstance(effect)) })
+		return POTION_REGISTRY.register(id, Supplier { Potion(potionId, MobEffectInstance(effect.get())) })
 	}
 
 }
