614898bc9f67e77e65a027ec870c1be9794d3d27
aaron <aaronhowser1@gmail.com>
2025-11-25 16:11:45 -0600

    add packet registration for FireballPacket, TeleportPlayerPacket, NarratorPacket, and ShearedPacket

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/packet/ModPacketHandler.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/packet/ModPacketHandler.kt
@@ -2,6 +2,11 @@ package dev.aaronhowser.mods.geneticsresequenced.packet
 
 import dev.aaronhowser.mods.aaron.AaronLib
 import dev.aaronhowser.mods.aaron.packet.AaronPacketRegistrar
+import dev.aaronhowser.mods.geneticsresequenced.packet.client_to_server.FireballPacket
+import dev.aaronhowser.mods.geneticsresequenced.packet.client_to_server.TeleportPlayerPacket
+import dev.aaronhowser.mods.geneticsresequenced.packet.server_to_client.NarratorPacket
+import dev.aaronhowser.mods.geneticsresequenced.packet.server_to_client.ShearedPacket
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
 import net.minecraftforge.network.NetworkRegistry
 import net.minecraftforge.network.simple.SimpleChannel
 
@@ -18,5 +23,43 @@ object ModPacketHandler : AaronPacketRegistrar() {
 		)
 
 	override fun getChannel(): SimpleChannel = CHANNEL
+
+	@Suppress("INFERRED_INVISIBLE_RETURN_TYPE_WARNING")
+	override fun registerPackets(event: FMLCommonSetupEvent) {
+		var i = 0
+
+		CHANNEL.registerMessage(
+			++i,
+			FireballPacket::class.java,
+			{ packet, buffer -> packet.encode(buffer) },
+			{ buffer -> FireballPacket.decode(buffer) },
+			{ packet, context -> packet.receiveOnServer(context) }
+		)
+
+		CHANNEL.registerMessage(
+			++i,
+			TeleportPlayerPacket::class.java,
+			{ packet, buffer -> packet.encode(buffer) },
+			{ buffer -> TeleportPlayerPacket.decode(buffer) },
+			{ packet, context -> packet.receiveOnServer(context) }
+		)
+
+		CHANNEL.registerMessage(
+			++i,
+			NarratorPacket::class.java,
+			{ packet, buffer -> packet.encode(buffer) },
+			{ buffer -> NarratorPacket.decode(buffer) },
+			{ packet, context -> packet.receiveOnClient(context) }
+		)
+
+		CHANNEL.registerMessage(
+			++i,
+			ShearedPacket::class.java,
+			{ packet, buffer -> packet.encode(buffer) },
+			{ buffer -> ShearedPacket.decode(buffer) },
+			{ packet, context -> packet.receiveOnClient(context) }
+		)
+
+	}
 
 }
