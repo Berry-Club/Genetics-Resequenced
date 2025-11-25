f9a67b69c8058c35996aa327b9e4db3ad7a70693
aaron <aaronhowser1@gmail.com>
2025-11-25 15:17:34 -0600

    mroe

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/event/CustomEvents.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/event/CustomEvents.kt
@@ -3,8 +3,8 @@ package dev.aaronhowser.mods.geneticsresequenced.event
 import dev.aaronhowser.mods.geneticsresequenced.gene.Gene
 import net.minecraft.core.Holder
 import net.minecraft.world.entity.LivingEntity
-import net.neoforged.bus.api.Event
-import net.neoforged.bus.api.ICancellableEvent
+import net.minecraftforge.eventbus.api.Cancelable
+import net.minecraftforge.eventbus.api.Event
 
 object CustomEvents {
 
@@ -14,11 +14,12 @@ object CustomEvents {
 		abstract val geneHolder: Holder<Gene>
 		abstract val wasAdded: Boolean
 
+		@Cancelable
 		data class Pre(
 			override val entity: LivingEntity,
 			override val geneHolder: Holder<Gene>,
 			override val wasAdded: Boolean
-		) : GeneChangeEvent(), ICancellableEvent
+		) : GeneChangeEvent()
 
 		data class Post(
 			override val entity: LivingEntity,
