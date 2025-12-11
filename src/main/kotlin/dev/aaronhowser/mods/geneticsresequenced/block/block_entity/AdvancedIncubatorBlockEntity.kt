142f6d8e1389da90b428c27c2373b15d66d04988
aaron <aaronhowser1@gmail.com>
2025-12-11 00:17:57 -0600

    cleanup

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/block/block_entity/AdvancedIncubatorBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/block/block_entity/AdvancedIncubatorBlockEntity.kt
@@ -26,7 +26,6 @@ import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
 import net.minecraft.world.level.block.state.BlockState
 import net.neoforged.neoforge.items.IItemHandler
-import net.neoforged.neoforge.items.wrapper.InvWrapper
 import net.neoforged.neoforge.items.wrapper.RangedWrapper
 import java.util.function.IntSupplier
 import kotlin.math.min
