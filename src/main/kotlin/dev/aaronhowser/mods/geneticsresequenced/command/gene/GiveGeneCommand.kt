193854eafcbe9b1006f120c033084dd1d9ceb3c5
aaron <aaronhowser1@gmail.com>
2025-12-12 12:48:34 -0600

    fix command

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/command/gene/GiveGeneCommand.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/command/gene/GiveGeneCommand.kt
@@ -1,11 +1,11 @@
 package dev.aaronhowser.mods.geneticsresequenced.command.gene
 
 import com.mojang.brigadier.builder.ArgumentBuilder
-import com.mojang.brigadier.context.CommandContext
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
 import dev.aaronhowser.mods.geneticsresequenced.attachment.GenesData.Companion.addGene
 import dev.aaronhowser.mods.geneticsresequenced.attachment.GenesData.Companion.hasGene
 import dev.aaronhowser.mods.geneticsresequenced.command.ModCommands.SUGGEST_GENE_RLS
+import dev.aaronhowser.mods.geneticsresequenced.command.gene.GiveGeneCommand.addGene
 import dev.aaronhowser.mods.geneticsresequenced.datagen.lang.ModLanguageProvider
 import dev.aaronhowser.mods.geneticsresequenced.datagen.lang.ModLanguageProvider.Companion.toComponent
 import dev.aaronhowser.mods.geneticsresequenced.gene.Gene
@@ -16,6 +16,7 @@ import net.minecraft.commands.Commands
 import net.minecraft.commands.arguments.EntityArgument
 import net.minecraft.commands.arguments.ResourceLocationArgument
 import net.minecraft.core.Holder
+import net.minecraft.network.chat.Component
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.LivingEntity
@@ -35,9 +36,9 @@ object GiveGeneCommand {
 					.suggests(SUGGEST_GENE_RLS)
 					.executes { cmd ->
 						addGene(
-							cmd,
+							cmd.source,
 							ResourceLocationArgument.getId(cmd, GENE_ID_ARGUMENT),
-							entities = null
+							entities = listOf(cmd.source.playerOrException)
 						)
 					}
 					.then(
@@ -45,7 +46,7 @@ object GiveGeneCommand {
 							.argument(TARGET_ARGUMENT, EntityArgument.entities())
 							.executes { cmd ->
 								addGene(
-									cmd,
+									cmd.source,
 									ResourceLocationArgument.getId(cmd, GENE_ID_ARGUMENT),
 									EntityArgument.getEntities(cmd, TARGET_ARGUMENT)
 								)
@@ -56,74 +57,66 @@ object GiveGeneCommand {
 	}
 
 	private fun addGene(
-		context: CommandContext<CommandSourceStack>,
+		source: CommandSourceStack,
 		geneRl: ResourceLocation,
-		entities: MutableCollection<out Entity>? = null
+		entities: Collection<Entity>
 	): Int {
-		val gene = ModGenes.fromResourceLocation(context.source.registryAccess(), geneRl)
+		val gene = ModGenes.fromResourceLocation(source.registryAccess(), geneRl)
 			?: throw IllegalArgumentException("Gene with id $geneRl does not exist!")
 
-		return addGene(context, gene, entities)
+		return addGene(source, gene, entities)
 	}
 
 	private fun addGene(
-		context: CommandContext<CommandSourceStack>,
-		geneString: String,
-		entities: MutableCollection<out Entity>? = null
-	): Int {
-
-		val gene = ModGenes.fromIdPath(context.source.registryAccess(), geneString)
-			?: throw IllegalArgumentException("Gene with id $geneString does not exist!")
-
-		return addGene(context, gene, entities)
-	}
-
-	private fun addGene(
-		context: CommandContext<CommandSourceStack>,
+		source: CommandSourceStack,
 		geneToAdd: Holder<Gene>,
-		entities: MutableCollection<out Entity>? = null
+		entities: Collection<Entity>
 	): Int {
+		val targets = entities.mapNotNull { it as? LivingEntity }
 
-		val targets: List<LivingEntity> =
-			entities?.mapNotNull { it as? LivingEntity } ?: listOfNotNull(context.source.entity as? LivingEntity)
+		if (targets.isEmpty()) {
+			source.sendFailure(Component.literal("No valid living entity targets found!"))
+			return 0
+		}
 
 		if (targets.size == 1) {
-			handleSingleTarget(context, targets.first(), geneToAdd)
+			handleSingleTarget(source, targets.first(), geneToAdd)
 		} else {
-			handleMultipleTargets(context, targets, geneToAdd)
+			handleMultipleTargets(source, targets, geneToAdd)
 		}
 
 		return 1
 	}
 
 	private fun handleSingleTarget(
-		context: CommandContext<CommandSourceStack>,
+		source: CommandSourceStack,
 		target: LivingEntity,
 		geneHolder: Holder<Gene>,
 	) {
-		val geneWasAdded = addGeneToTarget(target, geneHolder)
+		val success = addGeneToTarget(target, geneHolder)
 
-		if (geneWasAdded) {
-			val component =
-				ModLanguageProvider.Commands.ADD_SINGLE_SUCCESS.toComponent(
-					geneHolder.getName(),
-					target.name
-				)
-
-			context.source.sendSuccess({ component }, false)
+		if (success) {
+			source.sendSuccess(
+				{
+					ModLanguageProvider.Commands.ADD_SINGLE_SUCCESS.toComponent(
+						geneHolder.getName(),
+						target.name
+					)
+				},
+				false
+			)
 		} else {
-			val component =
+			source.sendFailure(
 				ModLanguageProvider.Commands.ADD_SINGLE_FAIL.toComponent(
 					geneHolder.getName(),
 					target.name
 				)
-
-			context.source.sendFailure(component)
+			)
 		}
 	}
 
 	private fun handleMultipleTargets(
-		context: CommandContext<CommandSourceStack>,
+		source: CommandSourceStack,
 		targets: List<LivingEntity>,
 		geneHolder: Holder<Gene>
 	) {
@@ -131,27 +124,29 @@ object GiveGeneCommand {
 		var amountFail = 0
 
 		for (target in targets) {
-			val geneWasAdded = addGeneToTarget(target, geneHolder)
-			if (geneWasAdded) amountSuccess++ else amountFail++
+			val success = addGeneToTarget(target, geneHolder)
+			if (success) amountSuccess++ else amountFail++
 		}
 
 		if (amountSuccess != 0) {
-			val component =
-				ModLanguageProvider.Commands.ADD_MULTIPLE_SUCCESS.toComponent(
-					geneHolder.getName(),
-					amountSuccess
-				)
-
-			context.source.sendSuccess({ component }, false)
+			source.sendSuccess(
+				{
+					ModLanguageProvider.Commands.ADD_MULTIPLE_SUCCESS.toComponent(
+						geneHolder.getName(),
+						amountSuccess
+					)
+				},
+				false
+			)
 		}
 
 		if (amountFail != 0) {
-			val component =
+			source.sendFailure(
 				ModLanguageProvider.Commands.ADD_MULTIPLE_FAIL.toComponent(
 					geneHolder.getName(),
 					amountFail
 				)
-			context.source.sendFailure(component)
+			)
 		}
 	}
 
