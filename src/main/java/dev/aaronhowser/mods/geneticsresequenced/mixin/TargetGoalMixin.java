94dc2a174cac05c77ea0a9baec636d9309a0f4d4
aaron <aaronhowser1@gmail.com>
2025-12-13 05:02:33 -0600

    no shadow

--- src/main/java/dev/aaronhowser/mods/geneticsresequenced/mixin/TargetGoalMixin.java
+++ src/main/java/dev/aaronhowser/mods/geneticsresequenced/mixin/TargetGoalMixin.java
@@ -1,22 +1,14 @@
 package dev.aaronhowser.mods.geneticsresequenced.mixin;
 
 import dev.aaronhowser.mods.geneticsresequenced.gene.behavior.MobGenes;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.goal.target.TargetGoal;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import javax.annotation.Nullable;
-
 @Mixin(TargetGoal.class)
 public class TargetGoalMixin {
-
-	@Shadow
-	@Nullable
-	protected LivingEntity targetMob;
 
 	@Inject(
 			method = "canContinueToUse",
