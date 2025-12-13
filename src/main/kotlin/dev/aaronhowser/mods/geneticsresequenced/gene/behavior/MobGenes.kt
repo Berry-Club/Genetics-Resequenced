7589943def264bf9c5d445efd07a6e13381f6ed0
aaron <aaronhowser1@gmail.com>
2025-12-13 05:00:12 -0600

    MobGenes

---
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/gene/behavior/MobGenes.kt
@@ -0,0 +1,37 @@
+package dev.aaronhowser.mods.geneticsresequenced.gene.behavior
+
+import dev.aaronhowser.mods.geneticsresequenced.attachment.GenesData.Companion.hasGene
+import dev.aaronhowser.mods.geneticsresequenced.gene.Gene.Companion.isDisabled
+import dev.aaronhowser.mods.geneticsresequenced.registry.ModGenes
+import dev.aaronhowser.mods.geneticsresequenced.registry.ModGenes.getHolderOrThrow
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.entity.animal.Animal
+import net.neoforged.neoforge.event.entity.living.BabyEntitySpawnEvent
+
+object MobGenes {
+
+	private var isFertileCloning = false
+	fun handleFertile(event: BabyEntitySpawnEvent) {
+		if (isFertileCloning) return
+
+		val parentA = event.parentA as? Animal ?: return
+		val parentB = event.parentB as? Animal ?: return
+
+		val level = parentA.level() as? ServerLevel ?: return
+
+		val fertile = ModGenes.FERTILE.getHolderOrThrow(parentA.registryAccess())
+		if (fertile.isDisabled) return
+
+		var extraBabies = 0
+		if (parentA.hasGene(ModGenes.FERTILE)) extraBabies++
+		if (parentB.hasGene(ModGenes.FERTILE)) extraBabies++
+		if (extraBabies == 0) return
+
+		isFertileCloning = true
+		for (i in 0 until extraBabies) {
+			parentA.spawnChildFromBreeding(level, parentB)
+		}
+		isFertileCloning = false
+	}
+
+}
--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/gene/behavior/OtherGenes.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/gene/behavior/OtherGenes.kt
@@ -14,17 +14,14 @@ import dev.aaronhowser.mods.geneticsresequenced.registry.ModGenes
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModGenes.getHolderOrThrow
 import net.minecraft.core.BlockPos
 import net.minecraft.network.chat.Component
-import net.minecraft.server.level.ServerLevel
 import net.minecraft.sounds.SoundEvents
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.Pose
-import net.minecraft.world.entity.animal.Animal
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.phys.AABB
 import net.neoforged.neoforge.event.ServerChatEvent
-import net.neoforged.neoforge.event.entity.living.BabyEntitySpawnEvent
 import kotlin.random.Random
 
 object OtherGenes {
@@ -254,30 +251,6 @@ object OtherGenes {
 		}
 
 		return false
-	}
-
-	private var isFertileCloning = false
-	fun handleFertile(event: BabyEntitySpawnEvent) {
-		if (isFertileCloning) return
-
-		val parentA = event.parentA as? Animal ?: return
-		val parentB = event.parentB as? Animal ?: return
-
-		val level = parentA.level() as? ServerLevel ?: return
-
-		val fertile = ModGenes.FERTILE.getHolderOrThrow(parentA.registryAccess())
-		if (fertile.isDisabled) return
-
-		var extraBabies = 0
-		if (parentA.hasGene(ModGenes.FERTILE)) extraBabies++
-		if (parentB.hasGene(ModGenes.FERTILE)) extraBabies++
-		if (extraBabies == 0) return
-
-		isFertileCloning = true
-		for (i in 0 until extraBabies) {
-			parentA.spawnChildFromBreeding(level, parentB)
-		}
-		isFertileCloning = false
 	}
 
 }
