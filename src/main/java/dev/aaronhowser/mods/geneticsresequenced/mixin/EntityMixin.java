7ecb805b0436555719bfd236ceb762a4ec810c2e
aaron <aaronhowser1@gmail.com>
2025-12-13 06:05:17 -0600

    Add EntityMixin to handle slowness negation from blocks for LivingEntities

---
+++ src/main/java/dev/aaronhowser/mods/geneticsresequenced/mixin/EntityMixin.java
@@ -0,0 +1,29 @@
+package dev.aaronhowser.mods.geneticsresequenced.mixin;
+
+import dev.aaronhowser.mods.geneticsresequenced.gene.behavior.OtherGenes;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Entity.class)
+public class EntityMixin {
+
+	@Inject(
+			method = "makeStuckInBlock",
+			at = @At("HEAD"),
+			cancellable = true
+	)
+	private void geneticsresequenced$makeStuckInBlock(BlockState state, Vec3 motionMultiplier, CallbackInfo ci) {
+		if ((Object) this instanceof LivingEntity le) {
+			if (OtherGenes.shouldNegateSlownessFromBlock(le, state)) {
+				ci.cancel();
+			}
+		}
+	}
+
+}
