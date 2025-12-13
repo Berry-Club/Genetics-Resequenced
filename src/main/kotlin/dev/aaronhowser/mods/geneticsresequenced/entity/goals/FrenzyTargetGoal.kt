6fd4a484106968f042158df83bb8302d2a4054a2
aaron <aaronhowser1@gmail.com>
2025-12-13 05:35:41 -0600

    Add FrenzyMeleeAttackGoal and FrenzyTargetGoal for enhanced mob behavior

---
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/entity/goals/FrenzyTargetGoal.kt
@@ -0,0 +1,36 @@
+package dev.aaronhowser.mods.geneticsresequenced.entity.goals
+
+import dev.aaronhowser.mods.geneticsresequenced.attachment.GenesData.Companion.hasGene
+import dev.aaronhowser.mods.geneticsresequenced.gene.Gene.Companion.isDisabled
+import dev.aaronhowser.mods.geneticsresequenced.registry.ModGenes
+import dev.aaronhowser.mods.geneticsresequenced.registry.ModGenes.getHolderOrThrow
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.Mob
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal
+
+// https://github.com/Elenterius/Biomancy/blob/mc1.20.1/prod/src/main/java/com/github/elenterius/biomancy/entity/mob/ai/goal/FrenzyAttackableTargetGoal.java
+class FrenzyTargetGoal<T : LivingEntity> : NearestAttackableTargetGoal<T> {
+
+	constructor(goalOwner: Mob, targetClass: Class<T>) : super(
+		goalOwner,
+		targetClass,
+		true
+	)
+
+	override fun canUse(): Boolean {
+		val frenzyGene = ModGenes.FRENZIED.getHolderOrThrow(mob.registryAccess())
+		if (frenzyGene.isDisabled) return false
+
+		return super.canUse() && mob.hasGene(ModGenes.FRENZIED)
+	}
+
+	override fun canContinueToUse(): Boolean {
+		if (!mob.hasGene(ModGenes.FRENZIED)) {
+			stop()
+			return false
+		}
+
+		return super.canContinueToUse()
+	}
+
+}
--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/gene/behavior/MobGenes.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/gene/behavior/MobGenes.kt
@@ -1,10 +1,16 @@
 package dev.aaronhowser.mods.geneticsresequenced.gene.behavior
 
 import dev.aaronhowser.mods.geneticsresequenced.attachment.GenesData.Companion.hasGene
+import dev.aaronhowser.mods.geneticsresequenced.entity.goals.FrenzyMeleeAttackGoal
+import dev.aaronhowser.mods.geneticsresequenced.entity.goals.FrenzyTargetGoal
 import dev.aaronhowser.mods.geneticsresequenced.gene.Gene.Companion.isDisabled
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModGenes
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModGenes.getHolderOrThrow
 import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.Mob
+import net.minecraft.world.entity.PathfinderMob
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal
 import net.minecraft.world.entity.ai.goal.target.TargetGoal
 import net.minecraft.world.entity.animal.Animal
 import net.neoforged.neoforge.event.entity.living.BabyEntitySpawnEvent
@@ -43,6 +49,26 @@ object MobGenes {
 		if (placid.isDisabled) return false
 
 		return entity.hasGene(ModGenes.PLACID)
+	}
+
+	fun giveFrenzyGoals(mob: Mob) {
+		if (mob !is PathfinderMob) return
+
+		val alreadyHasFrenzyTargetGoal = mob.targetSelector.availableGoals.any { it.goal is FrenzyTargetGoal<*> }
+		if (!alreadyHasFrenzyTargetGoal) {
+			mob.targetSelector.addGoal(
+				1,
+				FrenzyTargetGoal(mob, LivingEntity::class.java)
+			)
+		}
+
+		val alreadyCanAttack = mob.goalSelector.availableGoals.any { it.goal is MeleeAttackGoal }
+		if (!alreadyCanAttack) {
+			mob.goalSelector.addGoal(
+				2,
+				FrenzyMeleeAttackGoal(mob, 1.0, false)
+			)
+		}
 	}
 
 }
