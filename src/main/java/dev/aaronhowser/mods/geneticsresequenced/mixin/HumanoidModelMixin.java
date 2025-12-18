a06424d4f7bef577fcc849039d97e1396394e25f
aaron <aaronhowser1@gmail.com>
2025-12-18 12:38:10 -0600

    Refactor HumanoidModelMixin to capture root part with additional parameters

--- src/main/java/dev/aaronhowser/mods/geneticsresequenced/mixin/HumanoidModelMixin.java
+++ src/main/java/dev/aaronhowser/mods/geneticsresequenced/mixin/HumanoidModelMixin.java
@@ -3,10 +3,15 @@ package dev.aaronhowser.mods.geneticsresequenced.mixin;
 import dev.aaronhowser.mods.geneticsresequenced.mixin_interfaces.CringeModel;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.Function;
 
 @Mixin(HumanoidModel.class)
 public abstract class HumanoidModelMixin implements CringeModel {
@@ -14,16 +19,16 @@ public abstract class HumanoidModelMixin implements CringeModel {
 	@Unique
 	private ModelPart geneticsresequenced$rootPart;
 
-	@Unique
-	public ModelPart getGeneticsresequenced$rootPart() {
+	@Override
+	public ModelPart geneticsresequenced$getRootPart() {
 		return this.geneticsresequenced$rootPart;
 	}
 
 	@Inject(
-			method = "<init>*",
-			at = @At("HEAD")
+			method = "<init>(Lnet/minecraft/client/model/geom/ModelPart;Ljava/util/function/Function;)V",
+			at = @At("RETURN")
 	)
-	private void geneticsresequenced$captureRootPart(ModelPart root) {
+	private void geneticsresequenced$captureRootPart(ModelPart root, Function<ResourceLocation, RenderType> renderType, CallbackInfo ci) {
 		this.geneticsresequenced$rootPart = root;
 	}
 
