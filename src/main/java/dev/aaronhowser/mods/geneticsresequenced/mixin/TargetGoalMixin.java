4ae9d32b03b30ad0451832ea56a74b93cf9c1764
aaron <aaronhowser1@gmail.com>
2025-12-18 12:20:28 -0600

    abstractify

--- src/main/java/dev/aaronhowser/mods/geneticsresequenced/mixin/TargetGoalMixin.java
+++ src/main/java/dev/aaronhowser/mods/geneticsresequenced/mixin/TargetGoalMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TargetGoal.class)
-public class TargetGoalMixin {
+public abstract class TargetGoalMixin {
 
 	@Inject(
 			method = "canContinueToUse",
