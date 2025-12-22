9b01c52fab0a93050eb63f27f72666279b431202
aaron <aaronhowser1@gmail.com>
2025-12-22 00:00:29 -0600

    Revert "feat: add incompatibleGenes parameter to Gene constructor"
    This reverts commit 5da56fa083882a56e92e8021301040c6f6718ac6.

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/gene/ModGeneProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/gene/ModGeneProvider.kt
@@ -6,11 +6,9 @@ import dev.aaronhowser.mods.geneticsresequenced.gene.Gene.AttributeEntry
 import dev.aaronhowser.mods.geneticsresequenced.gene.Gene.PotionDetails
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModAttributes
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModGenes
-import dev.aaronhowser.mods.geneticsresequenced.registry.ModGenes.getHolderOrThrow
 import net.minecraft.core.HolderSet
 import net.minecraft.core.RegistrySetBuilder
 import net.minecraft.core.registries.BuiltInRegistries
-import net.minecraft.core.registries.Registries
 import net.minecraft.data.worldgen.BootstrapContext
 import net.minecraft.tags.TagKey
 import net.minecraft.world.effect.MobEffects
@@ -31,18 +29,8 @@ class ModGeneProvider : RegistrySetBuilder() {
 			allowedEntities: HolderSet<EntityType<*>> = DEFAULT_ALLOWED_ENTITIES,
 			potionDetails: List<PotionDetails> = emptyList(),
 			attributeModifiers: List<AttributeEntry> = emptyList(),
-			scaresEntitiesWithTag: Optional<TagKey<EntityType<*>>> = Optional.empty(),
-			incompatibleGenes: HolderSet<Gene> = HolderSet.empty()
-		): Gene {
-			return Gene(
-				dnaPointsRequired,
-				allowedEntities,
-				potionDetails,
-				attributeModifiers,
-				scaresEntitiesWithTag,
-				incompatibleGenes
-			)
-		}
+			scaresEntitiesWithTag: Optional<TagKey<EntityType<*>>> = Optional.empty()
+		) = Gene(dnaPointsRequired, allowedEntities, potionDetails, attributeModifiers, scaresEntitiesWithTag)
 
 		val DEFAULT_ALLOWED_ENTITIES = AnyHolderSet(BuiltInRegistries.ENTITY_TYPE.asLookup())
 		val NO_ENTITIES: HolderSet<EntityType<*>> = HolderSet.empty()
@@ -954,14 +942,11 @@ class ModGeneProvider : RegistrySetBuilder() {
 				)
 			)
 
-			val a = context.lookup(ModGenes.GENE_REGISTRY_KEY)
-
 			context.register(
 				ModGenes.PLACID,
 				makeGene(
 					dnaPointsRequired = 30,
-					allowedEntities = NON_PLAYERS,
-					incompatibleGenes = HolderSet.direct(ModGenes.FRENZIED.getHolderOrThrow(a))
+					allowedEntities = NON_PLAYERS
 				)
 			)
 
