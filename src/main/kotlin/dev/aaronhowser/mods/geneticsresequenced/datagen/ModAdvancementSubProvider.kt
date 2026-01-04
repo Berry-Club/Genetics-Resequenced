d954646b76f9d03c3d5a886a4adf76bb78414e05
aaron <aaronhowser1@gmail.com>
2026-01-04 04:47:43 -0600

    chore: update aaron_lib_version to 1.5.0 and adjust version range refactor: enhance ModAdvancementSubProvider for improved readability and maintainability

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/ModAdvancementSubProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/ModAdvancementSubProvider.kt
@@ -1,5 +1,7 @@
 package dev.aaronhowser.mods.geneticsresequenced.datagen
 
+import dev.aaronhowser.mods.aaron.AaronExtensions.withComponent
+import dev.aaronhowser.mods.aaron.datagen.AaronAdvancementSubProvider
 import dev.aaronhowser.mods.geneticsresequenced.advancement.HelixGenePredicate
 import dev.aaronhowser.mods.geneticsresequenced.datagen.lang.ModAdvancementLang
 import dev.aaronhowser.mods.geneticsresequenced.datagen.lang.ModLanguageProvider.Companion.toComponent
@@ -9,22 +11,23 @@ import dev.aaronhowser.mods.geneticsresequenced.registry.ModDataComponents
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModItemSubPredicates
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModItems
 import dev.aaronhowser.mods.geneticsresequenced.util.OtherUtil
-import net.minecraft.advancements.*
-import net.minecraft.advancements.critereon.ImpossibleTrigger
+import net.minecraft.advancements.Advancement
+import net.minecraft.advancements.AdvancementHolder
+import net.minecraft.advancements.AdvancementRequirements
+import net.minecraft.advancements.AdvancementType
 import net.minecraft.advancements.critereon.InventoryChangeTrigger
 import net.minecraft.advancements.critereon.ItemPredicate
 import net.minecraft.core.HolderLookup
 import net.minecraft.network.chat.Component
 import net.minecraft.world.item.Items
-import net.neoforged.neoforge.common.data.AdvancementProvider
 import net.neoforged.neoforge.common.data.ExistingFileHelper
 import java.util.*
 import java.util.concurrent.CompletableFuture
 import java.util.function.Consumer
 
 class ModAdvancementSubProvider(
-	val lookupProvider: CompletableFuture<HolderLookup.Provider>
-) : AdvancementProvider.AdvancementGenerator {
+	lookupProvider: CompletableFuture<HolderLookup.Provider>
+) : AaronAdvancementSubProvider(lookupProvider) {
 
 	override fun generate(
 		registries: HolderLookup.Provider,
@@ -32,110 +35,93 @@ class ModAdvancementSubProvider(
 		existingFileHelper: ExistingFileHelper
 	) {
 
-		val root =
-			Advancement.Builder.advancement()
-				.display(
-					ModItems.SCRAPER.get(),
-					Component.literal("Genetics: Resequenced"),
-					ModAdvancementLang.SCRAPER_DESC.toComponent(),
-					OtherUtil.modResource("textures/block/machine_bottom.png"),
-					AdvancementType.TASK,
-					true,
-					true,
-					false
-				)
-				.addCriterion(
-					"scraper",
-					InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.SCRAPER.get())
-				)
-				.save(saver, ROOT, existingFileHelper)
+		val root = advancement()
+			.display(
+				ModItems.SCRAPER.get(),
+				Component.literal("Genetics: Resequenced"),
+				ModAdvancementLang.SCRAPER_DESC.toComponent(),
+				OtherUtil.modResource("textures/block/machine_bottom.png"),
+				AdvancementType.TASK,
+				true,
+				true,
+				false
+			)
+			.addCriterion(
+				"scraper",
+				InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.SCRAPER.get())
+			)
+			.save(saver, ROOT, existingFileHelper)
 
-		val cellAnalyzer =
-			Advancement.Builder.advancement()
-				.parent(root)
-				.display(
-					ModBlocks.CELL_ANALYZER.get(),
-					ModAdvancementLang.ANALYZER_TITLE.toComponent(),
-					ModAdvancementLang.ANALYZER_DESC.toComponent(),
-					null,
-					AdvancementType.TASK,
-					true, true, false
-				)
-				.addCriterion(
-					"cell_analyzer",
-					InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.CELL_ANALYZER.get())
-				)
-				.addCriterion(
-					"cell",
-					InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.CELL.get())
-				)
-				.requirements(AdvancementRequirements.Strategy.OR)
-				.save(saver, CELL_ANALYZER, existingFileHelper)
+		val cellAnalyzer = advancement()
+			.parent(root)
+			.display(
+				ModBlocks.CELL_ANALYZER.get(),
+				ModAdvancementLang.ANALYZER_TITLE.toComponent(),
+				ModAdvancementLang.ANALYZER_DESC.toComponent()
+			)
+			.addCriterion(
+				"cell_analyzer",
+				InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.CELL_ANALYZER.get())
+			)
+			.addCriterion(
+				"cell",
+				InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.CELL.get())
+			)
+			.requirements(AdvancementRequirements.Strategy.OR)
+			.save(saver, CELL_ANALYZER, existingFileHelper)
 
-		val dnaExtractor =
-			Advancement.Builder.advancement()
-				.parent(cellAnalyzer)
-				.display(
-					ModBlocks.DNA_EXTRACTOR.get(),
-					ModAdvancementLang.EXTRACTOR_TITLE.toComponent(),
-					ModAdvancementLang.EXTRACTOR_DESC.toComponent(),
-					null,
-					AdvancementType.TASK,
-					true, true, false
-				)
-				.addCriterion(
-					"dna_extractor",
-					InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.DNA_EXTRACTOR.get())
-				)
-				.addCriterion(
-					"dna_helix",
-					InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.DNA_HELIX.get())
-				)
-				.requirements(AdvancementRequirements.Strategy.OR)
-				.save(saver, DNA_EXTRACTOR, existingFileHelper)
+		val dnaExtractor = advancement()
+			.parent(cellAnalyzer)
+			.display(
+				ModBlocks.DNA_EXTRACTOR.get(),
+				ModAdvancementLang.EXTRACTOR_TITLE.toComponent(),
+				ModAdvancementLang.EXTRACTOR_DESC.toComponent(),
+			)
+			.addCriterion(
+				"dna_extractor",
+				InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.DNA_EXTRACTOR.get())
+			)
+			.addCriterion(
+				"dna_helix",
+				InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.DNA_HELIX.get())
+			)
+			.requirements(AdvancementRequirements.Strategy.OR)
+			.save(saver, DNA_EXTRACTOR, existingFileHelper)
 
-		val dnaDecryptor =
-			Advancement.Builder.advancement()
-				.parent(dnaExtractor)
-				.display(
-					ModBlocks.DNA_DECRYPTOR.get(),
-					ModAdvancementLang.DECRYPTOR_TITLE.toComponent(),
-					ModAdvancementLang.DECRYPTOR_DESC.toComponent(),
-					null,
-					AdvancementType.TASK,
-					true, true, false
-				)
-				.addCriterion(
-					"dna_decryptor",
-					InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.DNA_DECRYPTOR.get())
-				)
-				.save(saver, DNA_DECRYPTOR, existingFileHelper)
+		val dnaDecryptor = advancement()
+			.parent(dnaExtractor)
+			.display(
+				ModBlocks.DNA_DECRYPTOR.get(),
+				ModAdvancementLang.DECRYPTOR_TITLE.toComponent(),
+				ModAdvancementLang.DECRYPTOR_DESC.toComponent(),
+			)
+			.addCriterion(
+				"dna_decryptor",
+				InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.DNA_DECRYPTOR.get())
+			)
+			.save(saver, DNA_DECRYPTOR, existingFileHelper)
 
-		val decryptDna =
-			Advancement.Builder.advancement()
-				.parent(dnaDecryptor)
-				.display(
-					ModItems.DNA_HELIX.get(),
-					ModAdvancementLang.DECRYPT_TITLE.toComponent(),
-					ModAdvancementLang.DECRYPT_DESC.toComponent(),
-					null,
-					AdvancementType.TASK,
-					true, true, false
+		val decryptDna = advancement()
+			.parent(dnaDecryptor)
+			.display(
+				ModItems.DNA_HELIX.get(),
+				ModAdvancementLang.DECRYPT_TITLE.toComponent(),
+				ModAdvancementLang.DECRYPT_DESC.toComponent(),
+			)
+			.addCriterion(
+				"decrypted_dna",
+				InventoryChangeTrigger.TriggerInstance.hasItems(
+					ItemPredicate.Builder
+						.item()
+						.of(ModItems.DNA_HELIX.get())
+						.withSubPredicate(
+							ModItemSubPredicates.HELIX_GENE.get(),
+							HelixGenePredicate.any()
+						)
+						.build()
 				)
-				.addCriterion(
-					"decrypted_dna",
-					InventoryChangeTrigger.TriggerInstance.hasItems(
-						ItemPredicate.Builder
-							.item()
-							.of(ModItems.DNA_HELIX.get())
-							.withSubPredicate(
-								ModItemSubPredicates.HELIX_GENE.get(),
-								HelixGenePredicate.any()
-							)
-							.build()
-					)
-				)
-				.save(saver, DECRYPT_DNA, existingFileHelper)
+			)
+			.save(saver, DECRYPT_DNA, existingFileHelper)
 
 		Advancement.Builder.advancement()
 			.parent(decryptDna)
@@ -162,176 +148,130 @@ class ModAdvancementSubProvider(
 			)
 			.save(saver, BLACK_DEATH, existingFileHelper)
 
-		val plasmidInfuser =
-			Advancement.Builder.advancement()
-				.parent(dnaExtractor)
-				.display(
-					ModBlocks.PLASMID_INFUSER.get(),
-					ModAdvancementLang.INFUSER_TITLE.toComponent(),
-					ModAdvancementLang.INFUSER_DESC.toComponent(),
-					null,
-					AdvancementType.TASK,
-					true, true, false
-				)
-				.addCriterion(
-					"plasmid_infuser",
-					InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.PLASMID_INFUSER.get())
-				)
-				.save(saver, PLASMID_INFUSER, existingFileHelper)
+		val plasmidInfuser = advancement()
+			.parent(dnaExtractor)
+			.display(
+				ModBlocks.PLASMID_INFUSER.get(),
+				ModAdvancementLang.INFUSER_TITLE.toComponent(),
+				ModAdvancementLang.INFUSER_DESC.toComponent(),
+			)
+			.addCriterion(
+				"plasmid_infuser",
+				InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.PLASMID_INFUSER.get())
+			)
+			.save(saver, PLASMID_INFUSER, existingFileHelper)
 
-		val plasmidInjector =
-			Advancement.Builder.advancement()
-				.parent(plasmidInfuser)
-				.display(
-					ModBlocks.PLASMID_INJECTOR.get(),
-					ModAdvancementLang.INJECTOR_TITLE.toComponent(),
-					ModAdvancementLang.INJECTOR_DESC.toComponent(),
-					null,
-					AdvancementType.TASK,
-					true, true, false
-				)
-				.addCriterion(
-					"plasmid_injector",
-					InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.PLASMID_INJECTOR.get())
-				)
-				.save(saver, PLASMID_INJECTOR, existingFileHelper)
+		val plasmidInjector = advancement()
+			.parent(plasmidInfuser)
+			.display(
+				ModBlocks.PLASMID_INJECTOR.get(),
+				ModAdvancementLang.INJECTOR_TITLE.toComponent(),
+				ModAdvancementLang.INJECTOR_DESC.toComponent(),
+			)
+			.addCriterion(
+				"plasmid_injector",
+				InventoryChangeTrigger.TriggerInstance.hasItems(ModBlocks.PLASMID_INJECTOR.get())
+			)
+			.save(saver, PLASMID_INJECTOR, existingFileHelper)
 
-		val getGene =
-			Advancement.Builder.advancement()
-				.parent(plasmidInjector)
-				.display(
-					ModItems.SYRINGE.toStack().apply {
-						set(
-							ModDataComponents.SPECIFIC_ENTITY,
-							SpecificEntityItemComponent(
-								UUID.fromString("b0aa4edd-29e0-421f-b65c-be90055071b0"), //Arbitrary UUID
-								Component.literal("A Fake Mob")
-							)
-						)
-					},
-					ModAdvancementLang.GET_GENE_TITLE.toComponent(),
-					ModAdvancementLang.GET_GENE_DESC.toComponent(),
-					null,
-					AdvancementType.GOAL,
-					true, true, false
-				)
-				.addCriterion(
-					"impossible",
-					CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance())
-				)
-				.save(saver, GET_GENE, existingFileHelper)
+		val getGene = advancement()
+			.parent(plasmidInjector)
+			.display(
+				ModItems.SYRINGE.withComponent(
+					ModDataComponents.SPECIFIC_ENTITY.get(),
+					SpecificEntityItemComponent(
+						UUID.fromString("b0aa4edd-29e0-421f-b65c-be90055071b0"), //Arbitrary UUID
+						Component.literal("A Fake Mob")
+					)
+				),
+				ModAdvancementLang.GET_GENE_TITLE.toComponent(),
+				ModAdvancementLang.GET_GENE_DESC.toComponent(),
+				null,
+				AdvancementType.GOAL,
+				true, true, false
+			)
+			.addImpossibleCriterion()
+			.save(saver, GET_GENE, existingFileHelper)
 
-		Advancement.Builder.advancement()
+		advancement()
 			.parent(getGene)
 			.display(
 				Items.ELYTRA,
 				ModAdvancementLang.FLIGHT_TITLE.toComponent(),
 				ModAdvancementLang.FLIGHT_DESC.toComponent(),
-				null,
 				AdvancementType.CHALLENGE,
-				true, true, false
 			)
-			.addCriterion(
-				"impossible",
-				CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance())
-			)
+			.addImpossibleCriterion()
 			.save(saver, GET_FLIGHT, existingFileHelper)
 
-		Advancement.Builder.advancement()
+		advancement()
 			.parent(getGene)
 			.display(
 				Items.JACK_O_LANTERN,
 				ModAdvancementLang.SCARE_TITLE.toComponent(),
 				ModAdvancementLang.SCARE_DESC.toComponent(),
-				null,
 				AdvancementType.CHALLENGE,
-				true, true, false
 			)
-			.addCriterion(
-				"impossible",
-				CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance())
-			)
+			.addImpossibleCriterion()
 			.save(saver, GET_ALL_SCARE_GENES, existingFileHelper)
 
-		Advancement.Builder.advancement()
+		advancement()
 			.parent(getGene)
 			.display(
 				Items.SKELETON_SKULL,
 				ModAdvancementLang.CRINGE_TITLE.toComponent(),
 				ModAdvancementLang.CRINGE_DESC.toComponent(),
-				null,
 				AdvancementType.GOAL,
-				true, true, false
 			)
-			.addCriterion(
-				"impossible",
-				CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance())
-			)
+			.addImpossibleCriterion()
 			.save(saver, GET_CRINGE, existingFileHelper)
 
-		Advancement.Builder.advancement()
+		advancement()
 			.parent(getGene)
 			.display(
 				Items.MILK_BUCKET,
 				ModAdvancementLang.GET_MILKED_TITLE.toComponent(),
 				ModAdvancementLang.GET_MILKED_DESC.toComponent(),
-				null,
 				AdvancementType.CHALLENGE,
-				true, true, false
 			)
-			.addCriterion(
-				"impossible",
-				CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance())
-			)
+			.addImpossibleCriterion()
 			.save(saver, GET_MILKED, existingFileHelper)
 
-		Advancement.Builder.advancement()
+		advancement()
 			.parent(getGene)
 			.display(
 				Items.SLIME_BALL,
 				ModAdvancementLang.SLIMY_TITLE.toComponent(),
 				ModAdvancementLang.SLIMY_DESC.toComponent(),
-				null,
 				AdvancementType.CHALLENGE,
-				true, true, false
 			)
-			.addCriterion(
-				"impossible",
-				CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance())
-			)
+			.addImpossibleCriterion()
 			.save(saver, TRIGGER_SLIMY_DEATH, existingFileHelper)
 
-		val syringe =
-			Advancement.Builder.advancement()
-				.parent(root)
-				.display(
-					ModItems.SYRINGE.get(),
-					ModAdvancementLang.SYRINGE_TITLE.toComponent(),
-					ModAdvancementLang.SYRINGE_DESC.toComponent(),
-					null,
-					AdvancementType.TASK,
-					true, true, false
-				)
-				.addCriterion(
-					"syringe",
-					InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.SYRINGE.get())
-				)
-				.addCriterion(
-					"metal_syringe",
-					InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.METAL_SYRINGE.get())
-				)
-				.requirements(AdvancementRequirements.Strategy.OR)
-				.save(saver, SYRINGE, existingFileHelper)
+		val syringe = advancement()
+			.parent(root)
+			.display(
+				ModItems.SYRINGE.get(),
+				ModAdvancementLang.SYRINGE_TITLE.toComponent(),
+				ModAdvancementLang.SYRINGE_DESC.toComponent(),
+			)
+			.addCriterion(
+				"syringe",
+				InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.SYRINGE.get())
+			)
+			.addCriterion(
+				"metal_syringe",
+				InventoryChangeTrigger.TriggerInstance.hasItems(ModItems.METAL_SYRINGE.get())
+			)
+			.requirements(AdvancementRequirements.Strategy.OR)
+			.save(saver, SYRINGE, existingFileHelper)
 
-		Advancement.Builder.advancement()
+		advancement()
 			.parent(syringe)
 			.display(
 				ModBlocks.BLOOD_PURIFIER.get(),
 				ModAdvancementLang.PURIFIER_TITLE.toComponent(),
 				ModAdvancementLang.PURIFIER_DESC.toComponent(),
-				null,
-				AdvancementType.TASK,
-				true, true, false
 			)
 			.addCriterion(
 				"blood_purifier",
