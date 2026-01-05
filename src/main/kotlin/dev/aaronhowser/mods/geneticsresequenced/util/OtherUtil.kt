54118a0f29d15da193d92881f90d7a7230116d1b
aaron <aaronhowser1@gmail.com>
2026-01-05 13:20:31 -0600

    progress

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/util/OtherUtil.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/util/OtherUtil.kt
@@ -3,7 +3,6 @@ package dev.aaronhowser.mods.geneticsresequenced.util
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
 import net.minecraft.core.Holder
 import net.minecraft.core.Registry
-import net.minecraft.core.component.DataComponents
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.core.registries.Registries
 import net.minecraft.network.chat.Component
@@ -16,17 +15,14 @@ import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.ai.attributes.Attributes
 import net.minecraft.world.entity.projectile.ProjectileUtil
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.Items
-import net.minecraft.world.item.alchemy.Potion
-import net.minecraft.world.item.alchemy.PotionContents
 import net.minecraft.world.item.enchantment.Enchantment
 import net.minecraft.world.level.ItemLike
-import kotlin.jvm.optionals.getOrNull
+import net.minecraftforge.common.ForgeMod
 
 object OtherUtil {
 
 	fun modResource(path: String): ResourceLocation =
-		ResourceLocation.fromNamespaceAndPath(GeneticsResequenced.MOD_ID, path)
+		ResourceLocation(GeneticsResequenced.MOD_ID, path)
 
 	val ItemLike.itemStack: ItemStack
 		get() = this.asItem().defaultInstance
@@ -42,7 +38,7 @@ object OtherUtil {
 	}
 
 	fun getLookedAtEntity(livingEntity: LivingEntity): Entity? {
-		val reach = livingEntity.getAttributeValue(Attributes.ENTITY_INTERACTION_RANGE)
+		val reach = livingEntity.getAttributeValue(ForgeMod.ENTITY_REACH.get())
 
 		val entityHitResult = ProjectileUtil.getEntityHitResult(
 			livingEntity,
@@ -54,13 +50,6 @@ object OtherUtil {
 		)
 
 		return entityHitResult?.entity
-	}
-
-	fun getPotionContents(itemStack: ItemStack): PotionContents? = itemStack.get(DataComponents.POTION_CONTENTS)
-	fun getPotion(itemStack: ItemStack): Holder<Potion>? = getPotionContents(itemStack)?.potion?.getOrNull()
-
-	fun getPotionStack(potion: Holder<Potion>): ItemStack {
-		return PotionContents.createItemStack(Items.POTION, potion)
 	}
 
 	fun getEnchantmentRegistry(entity: Entity): Registry<Enchantment> {
