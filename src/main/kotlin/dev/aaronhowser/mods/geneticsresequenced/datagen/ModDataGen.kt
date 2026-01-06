b09f4005fea0eb72adf8ec7bbc0e6cde210b8c0b
aaron <aaronhowser1@gmail.com>
2026-01-06 13:30:55 -0600

    ModGeneRequirementsProvider

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/ModDataGen.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/ModDataGen.kt
@@ -89,7 +89,7 @@ object ModDataGen {
 
 		generator.addProvider(
 			event.includeServer(),
-			ModGeneRequirementsProvider(output, lookupProvider, existingFileHelper)
+			ModGeneRequirementsProvider(output, existingFileHelper)
 		)
 
 		generator.addProvider(
--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/gene/ModGeneRequirementsProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/gene/ModGeneRequirementsProvider.kt
@@ -1,46 +1,47 @@
 package dev.aaronhowser.mods.geneticsresequenced.datagen.gene
 
+import com.mojang.serialization.JsonOps
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
 import dev.aaronhowser.mods.geneticsresequenced.data.GeneRequirements
 import dev.aaronhowser.mods.geneticsresequenced.gene.Gene
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModGenes
-import net.minecraft.core.HolderLookup
 import net.minecraft.data.PackOutput
 import net.minecraft.resources.ResourceKey
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.packs.PackType
 import net.minecraftforge.common.data.ExistingFileHelper
 import net.minecraftforge.common.data.JsonCodecProvider
-import java.util.concurrent.CompletableFuture
+import java.util.function.BiConsumer
 
 class ModGeneRequirementsProvider(
 	output: PackOutput,
-	lookupProvider: CompletableFuture<HolderLookup.Provider>,
 	existingFileHelper: ExistingFileHelper
 ) : JsonCodecProvider<GeneRequirements.GeneRequirementsData>(
 	output,
-	PackOutput.Target.DATA_PACK,
-	GeneRequirements.DIRECTORY,
-	PackType.SERVER_DATA,
-	GeneRequirements.GeneRequirementsData.CODEC,
-	lookupProvider,
+	existingFileHelper,
 	GeneticsResequenced.MOD_ID,
-	existingFileHelper
+	JsonOps.INSTANCE,
+	PackType.SERVER_DATA,
+	"gene/entity",
+	GeneRequirements.GeneRequirementsData.CODEC,
+	mapOf()
 ) {
 
-	private fun addRequirements(
-		geneRk: ResourceKey<Gene>,
-		vararg requirements: ResourceKey<Gene>
-	) {
-		this.unconditional(
-			geneRk.location(),
-			GeneRequirements.GeneRequirementsData(
-				geneRk,
-				requirements.toList()
-			)
-		)
-	}
+	override fun gather(consumer: BiConsumer<ResourceLocation, GeneRequirements.GeneRequirementsData>) {
 
-	override fun gather() {
+		fun addRequirements(
+			geneRk: ResourceKey<Gene>,
+			vararg requirements: ResourceKey<Gene>
+		) {
+			consumer.accept(
+				geneRk.location(),
+				GeneRequirements.GeneRequirementsData(
+					geneRk,
+					requirements.toList()
+				)
+			)
+		}
+
 		addRequirements(ModGenes.CLAWS_TWO, ModGenes.CLAWS)
 		addRequirements(ModGenes.EFFICIENCY_FOUR, ModGenes.EFFICIENCY)
 		addRequirements(ModGenes.FLIGHT, ModGenes.TELEPORT, ModGenes.STEP_ASSIST, ModGenes.NO_FALL_DAMAGE)
@@ -54,8 +55,6 @@ class ModGeneRequirementsProvider(
 		addRequirements(ModGenes.SPEED_TWO, ModGenes.SPEED)
 		addRequirements(ModGenes.STRENGTH_TWO, ModGenes.STRENGTH)
 		addRequirements(ModGenes.LAVA_PROOF, ModGenes.FIRE_PROOF)
-		addRequirements(ModGenes.WEB_WALKER, ModGenes.WEAVING)
-		addRequirements(ModGenes.WEB_DEFENSE, ModGenes.WEAVING)
 		addRequirements(ModGenes.BOUNTIFUL_TWO, ModGenes.BOUNTIFUL)
 	}
 }
