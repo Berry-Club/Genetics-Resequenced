53be597f5b677dca2854cbfff19ff50b376fadfe
aaron <aaronhowser1@gmail.com>
2026-01-07 07:14:54 -0600

    simplify

--- src/main/java/dev/aaronhowser/mods/geneticsresequenced/mixin/ClientHooksMixin.java
+++ src/main/java/dev/aaronhowser/mods/geneticsresequenced/mixin/ClientHooksMixin.java
@@ -1,9 +1,7 @@
 package dev.aaronhowser.mods.geneticsresequenced.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import dev.aaronhowser.mods.geneticsresequenced.util.ClientUtil;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.neoforged.neoforge.client.ClientHooks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -15,11 +13,7 @@ public abstract class ClientHooksMixin {
 			at = @At("RETURN")
 	)
 	private static boolean geneticsresequenced$hideGenePotions(boolean original, MobEffectInstance effectInstance) {
-		if (original) {
-			return !ClientUtil.shouldHidePotionInInventory(effectInstance);
-		} else {
-			return false;
-		}
+		return original && !ClientUtil.shouldHidePotionInInventory(effectInstance);
 	}
 
 }
