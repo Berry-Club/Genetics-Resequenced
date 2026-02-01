0718a29ccf94542e702096ede2404cbf528ab819
aaron <aaronhowser1@gmail.com>
2026-02-01 00:14:08 -0600

    refactor: optimize gene requirements retrieval logic in GeneRequirements

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/data/GeneRequirements.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/data/GeneRequirements.kt
@@ -4,13 +4,14 @@ import com.mojang.serialization.Codec
 import com.mojang.serialization.codecs.RecordCodecBuilder
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
 import dev.aaronhowser.mods.geneticsresequenced.gene.Gene
+import dev.aaronhowser.mods.geneticsresequenced.gene.Gene.Companion.isGene
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModGenes
-import dev.aaronhowser.mods.geneticsresequenced.registry.ModGenes.getHolderOrThrow
 import net.minecraft.core.Holder
 import net.minecraft.core.HolderLookup
 import net.minecraft.core.Registry
 import net.minecraft.resources.ResourceKey
 import net.minecraft.resources.ResourceLocation
+import kotlin.jvm.optionals.getOrNull
 
 class GeneRequirements(
 	val gene: ResourceKey<Gene>,
@@ -44,19 +45,18 @@ class GeneRequirements(
 			registries: HolderLookup.Provider
 		): Set<Holder<Gene>> {
 			val registry = registries.lookupOrThrow(REGISTRY_KEY)
-			val geneRk = gene.key ?: return emptySet()
-			val geneRequirementsRk = ResourceKey.create(REGISTRY_KEY, geneRk.location())
 
-			val requirementsData = registry
-				.get(geneRequirementsRk)
-				.orElse(null)
-				?: return emptySet()
+			val resultRks = mutableSetOf<ResourceKey<Gene>>()
 
-			return requirementsData
-				.value()
-				.requirements
-				.map { it.getHolderOrThrow(registries) }
-				.toSet()
+			for (grHolder in registry.listElements()) {
+				val gr = grHolder.value()
+				if (gene.isGene(gr.gene)) {
+					resultRks.addAll(gr.requirements)
+				}
+			}
+
+			val geneRegistry = registries.lookupOrThrow(ModGenes.GENE_REGISTRY_KEY)
+			return resultRks.mapNotNull { geneRegistry.get(it).getOrNull() }.toSet()
 		}
 	}
 
