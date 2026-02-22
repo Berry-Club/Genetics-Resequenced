66386f1a94c2a228737ff41da561d65a3fe0810c
aaron <aaronhowser1@gmail.com>
2026-02-22 00:08:29 -0600

    add: create ModifyEntityGenesEvent to manage gene weights for entities

---
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/event/custom/ModifyEntityGenesEvent.kt
@@ -0,0 +1,29 @@
+package dev.aaronhowser.mods.geneticsresequenced.event.custom
+
+import dev.aaronhowser.mods.geneticsresequenced.gene.Gene
+import net.minecraft.resources.ResourceKey
+import net.minecraft.world.entity.EntityType
+import net.minecraftforge.eventbus.api.Event
+
+data class ModifyEntityGenesEvent(
+	val entityType: ResourceKey<EntityType<*>>,
+	val geneWeights: MutableMap<ResourceKey<Gene>, Int>
+) : Event() {
+
+	fun getTotalWeight(): Int = geneWeights.values.sum()
+	fun getEntityTypeString(): String = entityType.location().toString()
+
+	fun getWeight(gene: ResourceKey<Gene>): Int = geneWeights.getOrDefault(gene, 0)
+
+	fun setWeight(gene: ResourceKey<Gene>, weight: Int) {
+		if (weight <= 0) {
+			geneWeights.remove(gene)
+			return
+		}
+
+		geneWeights[gene] = weight
+	}
+
+	fun remove(gene: ResourceKey<Gene>) = setWeight(gene, 0)
+
+}
