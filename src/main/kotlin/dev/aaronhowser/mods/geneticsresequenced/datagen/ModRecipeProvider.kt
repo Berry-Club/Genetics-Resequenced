cc4e9be60d98f1930485b064812c3eee2cb7e9ca
aaron <aaronhowser1@gmail.com>
2026-04-05 03:01:25 -0500

    refactor

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/ModRecipeProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/datagen/ModRecipeProvider.kt
@@ -28,12 +28,12 @@ class ModRecipeProvider(
 ) : RecipeProvider(output, lookupProvider) {
 
 	override fun buildRecipes(pRecipeOutput: RecipeOutput) {
-		shapedRecipes(pRecipeOutput)
-		specialRecipes(pRecipeOutput)
-		incubator(pRecipeOutput)
+		buildShapedRecipes(pRecipeOutput)
+		buildSpecialRecipes(pRecipeOutput)
+		buildIncubatorRecipes(pRecipeOutput)
 	}
 
-	private fun specialRecipes(pRecipeOutput: RecipeOutput) {
+	private fun buildSpecialRecipes(pRecipeOutput: RecipeOutput) {
 		val setAntiPlasmid = SpecialRecipeBuilder.special(::SetAntiPlasmidRecipe)
 		val unsetAntiPlasmid = SpecialRecipeBuilder.special(::UnsetAntiPlasmidRecipe)
 
@@ -41,236 +41,248 @@ class ModRecipeProvider(
 		unsetAntiPlasmid.save(pRecipeOutput, OtherUtil.modResource("unset_anti_plasmid"))
 	}
 
-	private fun shapedRecipes(pRecipeOutput: RecipeOutput) {
-		val shapedRecipes = listOf(
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.ADVANCED_INCUBATOR.get())
-				.pattern("OOO")
-				.pattern("OIO")
-				.pattern("EEE")
-				.define('O', Tags.Items.OBSIDIANS)
-				.define('I', ModBlocks.INCUBATOR.get())
-				.define('E', Tags.Items.END_STONES)
-				.unlockedBy("has_incubator", has(ModBlocks.INCUBATOR.get())),
+	private fun buildShapedRecipes(pRecipeOutput: RecipeOutput) {
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.ADVANCED_INCUBATOR.get())
+			.pattern("OOO")
+			.pattern("OIO")
+			.pattern("EEE")
+			.define('O', Tags.Items.OBSIDIANS)
+			.define('I', ModBlocks.INCUBATOR.get())
+			.define('E', Tags.Items.END_STONES)
+			.unlockedBy("has_incubator", has(ModBlocks.INCUBATOR.get()))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.ANTI_FIELD_BLOCK.get())
-				.pattern("   ")
-				.pattern(" O ")
-				.pattern(" L ")
-				.define('O', ModItems.ANTI_FIELD_ORB.get())
-				.define('L', Items.REDSTONE_LAMP)
-				.unlockedBy("has_anti_field_orb", has(ModItems.ANTI_FIELD_ORB.get())),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.ANTI_FIELD_BLOCK.get())
+			.pattern("   ")
+			.pattern(" O ")
+			.pattern(" L ")
+			.define('O', ModItems.ANTI_FIELD_ORB.get())
+			.define('L', Items.REDSTONE_LAMP)
+			.unlockedBy("has_anti_field_orb", has(ModItems.ANTI_FIELD_ORB.get()))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.ANTI_FIELD_ORB.get())
-				.pattern("EGE")
-				.pattern("GFG")
-				.pattern("EGE")
-				.define('E', Tags.Items.ENDER_PEARLS)
-				.define('G', Tags.Items.GLASS_BLOCKS_COLORLESS)
-				.define('F', Items.FERMENTED_SPIDER_EYE)
-				.unlockedBy("has_plasmid", has(ModItems.PLASMID.get())),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.ANTI_FIELD_ORB.get())
+			.pattern("EGE")
+			.pattern("GFG")
+			.pattern("EGE")
+			.define('E', Tags.Items.ENDER_PEARLS)
+			.define('G', Tags.Items.GLASS_BLOCKS_COLORLESS)
+			.define('F', Items.FERMENTED_SPIDER_EYE)
+			.unlockedBy("has_plasmid", has(ModItems.PLASMID.get()))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.ANTI_PLASMID.get())
-				.pattern("DDD")
-				.pattern("DFD")
-				.pattern("DDD")
-				.define('D', ModItems.DNA_HELIX.get())
-				.define('F', Items.FERMENTED_SPIDER_EYE)
-				.unlockedBy("has_plasmid", has(ModItems.PLASMID.get())),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.ANTI_PLASMID.get())
+			.pattern("DDD")
+			.pattern("DFD")
+			.pattern("DDD")
+			.define('D', ModItems.DNA_HELIX.get())
+			.define('F', Items.FERMENTED_SPIDER_EYE)
+			.unlockedBy("has_plasmid", has(ModItems.PLASMID.get()))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.BLOOD_PURIFIER.get())
-				.pattern("IWI")
-				.pattern("SBS")
-				.pattern("IWI")
-				.define('I', Tags.Items.INGOTS_IRON)
-				.define('W', Items.WHITE_WOOL)
-				.define('S', ModItems.SYRINGE.get())
-				.define('B', Items.BUCKET)
-				.unlockedBy("has_syringe", has(ModItems.SYRINGE.get())),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.BLOOD_PURIFIER.get())
+			.pattern("IWI")
+			.pattern("SBS")
+			.pattern("IWI")
+			.define('I', Tags.Items.INGOTS_IRON)
+			.define('W', Items.WHITE_WOOL)
+			.define('S', ModItems.SYRINGE.get())
+			.define('B', Items.BUCKET)
+			.unlockedBy("has_syringe", has(ModItems.SYRINGE.get()))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.CELL_ANALYZER.get())
-				.pattern("III")
-				.pattern("ISI")
-				.pattern("IRI")
-				.define('I', Tags.Items.INGOTS_IRON)
-				.define('S', ModItems.SYRINGE.get())
-				.define('R', Tags.Items.DUSTS_REDSTONE)
-				.unlockedBy("has_cell", has(ModItems.CELL.get())),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.CELL_ANALYZER.get())
+			.pattern("III")
+			.pattern("ISI")
+			.pattern("IRI")
+			.define('I', Tags.Items.INGOTS_IRON)
+			.define('S', ModItems.SYRINGE.get())
+			.define('R', Tags.Items.DUSTS_REDSTONE)
+			.unlockedBy("has_cell", has(ModItems.CELL.get()))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.COAL_GENERATOR.get())
-				.pattern("III")
-				.pattern("GFG")
-				.pattern("GRG")
-				.define('I', Tags.Items.INGOTS_IRON)
-				.define('G', Tags.Items.GLASS_BLOCKS_COLORLESS)
-				.define('F', Items.FURNACE)
-				.define('R', Tags.Items.INGOTS_IRON)
-				.unlockedBy("has_cell_analyzer", has(ModBlocks.CELL_ANALYZER.get())),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.COAL_GENERATOR.get())
+			.pattern("III")
+			.pattern("GFG")
+			.pattern("GRG")
+			.define('I', Tags.Items.INGOTS_IRON)
+			.define('G', Tags.Items.GLASS_BLOCKS_COLORLESS)
+			.define('F', Items.FURNACE)
+			.define('R', Tags.Items.INGOTS_IRON)
+			.unlockedBy("has_cell_analyzer", has(ModBlocks.CELL_ANALYZER.get()))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.DNA_DECRYPTOR.get())
-				.pattern("ILI")
-				.pattern("DGD")
-				.pattern("ILI")
-				.define('I', Tags.Items.INGOTS_IRON)
-				.define('L', Tags.Items.GLASS_BLOCKS_COLORLESS)
-				.define('D', ModItems.DNA_HELIX.get())
-				.define('G', Tags.Items.INGOTS_GOLD)
-				.unlockedBy("has_dna_extractor", has(ModBlocks.DNA_EXTRACTOR.get())),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.DNA_DECRYPTOR.get())
+			.pattern("ILI")
+			.pattern("DGD")
+			.pattern("ILI")
+			.define('I', Tags.Items.INGOTS_IRON)
+			.define('L', Tags.Items.GLASS_BLOCKS_COLORLESS)
+			.define('D', ModItems.DNA_HELIX.get())
+			.define('G', Tags.Items.INGOTS_GOLD)
+			.unlockedBy("has_dna_extractor", has(ModBlocks.DNA_EXTRACTOR.get()))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.DNA_EXTRACTOR.get())
-				.pattern("III")
-				.pattern("PCP")
-				.pattern("III")
-				.define('I', Tags.Items.INGOTS_IRON)
-				.define('P', Items.STICKY_PISTON)
-				.define('C', ModItems.CELL.get())
-				.unlockedBy("has_cell_analyzer", has(ModBlocks.CELL_ANALYZER.get())),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.DNA_EXTRACTOR.get())
+			.pattern("III")
+			.pattern("PCP")
+			.pattern("III")
+			.define('I', Tags.Items.INGOTS_IRON)
+			.define('P', Items.STICKY_PISTON)
+			.define('C', ModItems.CELL.get())
+			.unlockedBy("has_cell_analyzer", has(ModBlocks.CELL_ANALYZER.get()))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.DRAGON_HEALTH_CRYSTAL.get())
-				.pattern("DED")
-				.pattern("EDE")
-				.pattern("DED")
-				.define('D', Tags.Items.GEMS_DIAMOND)
-				.define('E', Items.END_CRYSTAL)
-				.unlockedBy("has_plasmid", has(ModItems.PLASMID.get())),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.DRAGON_HEALTH_CRYSTAL.get())
+			.pattern("DED")
+			.pattern("EDE")
+			.pattern("DED")
+			.define('D', Tags.Items.GEMS_DIAMOND)
+			.define('E', Items.END_CRYSTAL)
+			.unlockedBy("has_plasmid", has(ModItems.PLASMID.get()))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.INCUBATOR.get())
-				.pattern("IGI")
-				.pattern("IBI")
-				.pattern("SRS")
-				.define('I', Tags.Items.INGOTS_IRON)
-				.define('G', Tags.Items.GLASS_BLOCKS_COLORLESS)
-				.define('B', Items.BLAZE_ROD)
-				.define('S', Tags.Items.STONES)
-				.define('R', Tags.Items.INGOTS_IRON)
-				.unlockedBy("has_brewing_stand", has(Items.BREWING_STAND)),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.INCUBATOR.get())
+			.pattern("IGI")
+			.pattern("IBI")
+			.pattern("SRS")
+			.define('I', Tags.Items.INGOTS_IRON)
+			.define('G', Tags.Items.GLASS_BLOCKS_COLORLESS)
+			.define('B', Items.BLAZE_ROD)
+			.define('S', Tags.Items.STONES)
+			.define('R', Tags.Items.INGOTS_IRON)
+			.unlockedBy("has_brewing_stand", has(Items.BREWING_STAND))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.OVERCLOCKER.get())
-				.pattern("CLC")
-				.pattern("LOL")
-				.pattern("CLC")
-				.define('C', ModItems.CELL.get())
-				.define('L', Tags.Items.GEMS_LAPIS)
-				.define('O', Items.CLOCK)
-				.unlockedBy("has_cell_analyzer", has(ModBlocks.CELL_ANALYZER.get())),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.OVERCLOCKER.get())
+			.pattern("CLC")
+			.pattern("LOL")
+			.pattern("CLC")
+			.define('C', ModItems.CELL.get())
+			.define('L', Tags.Items.GEMS_LAPIS)
+			.define('O', Items.CLOCK)
+			.unlockedBy("has_cell_analyzer", has(ModBlocks.CELL_ANALYZER.get()))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.PLASMID.get())
-				.pattern("DDD")
-				.pattern("D D")
-				.pattern("DDD")
-				.define('D', ModItems.DNA_HELIX.get())
-				.unlockedBy("has_plasmid_injector", has(ModBlocks.PLASMID_INJECTOR.get())),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.PLASMID.get())
+			.pattern("DDD")
+			.pattern("D D")
+			.pattern("DDD")
+			.define('D', ModItems.DNA_HELIX.get())
+			.unlockedBy("has_plasmid_injector", has(ModBlocks.PLASMID_INJECTOR.get()))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.PLASMID_INFUSER.get())
-				.pattern("III")
-				.pattern("LPL")
-				.pattern("IDI")
-				.define('I', Tags.Items.INGOTS_IRON)
-				.define('L', ModItems.PLASMID.get())
-				.define('P', Items.PISTON)
-				.define('D', Tags.Items.GEMS_DIAMOND)
-				.unlockedBy("has_dna_decryptor", has(ModBlocks.DNA_DECRYPTOR.get())),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.PLASMID_INFUSER.get())
+			.pattern("III")
+			.pattern("LPL")
+			.pattern("IDI")
+			.define('I', Tags.Items.INGOTS_IRON)
+			.define('L', ModItems.PLASMID.get())
+			.define('P', Items.PISTON)
+			.define('D', Tags.Items.GEMS_DIAMOND)
+			.unlockedBy("has_dna_decryptor", has(ModBlocks.DNA_DECRYPTOR.get()))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.PLASMID_INJECTOR.get())
-				.pattern("ISI")
-				.pattern("SBS")
-				.pattern("ISI")
-				.define('I', Tags.Items.INGOTS_IRON)
-				.define('S', ModItems.SYRINGE.get())
-				.define('B', Items.BUCKET)
-				.unlockedBy("has_plasmid_infuser", has(ModBlocks.PLASMID_INFUSER.get())),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.PLASMID_INJECTOR.get())
+			.pattern("ISI")
+			.pattern("SBS")
+			.pattern("ISI")
+			.define('I', Tags.Items.INGOTS_IRON)
+			.define('S', ModItems.SYRINGE.get())
+			.define('B', Items.BUCKET)
+			.unlockedBy("has_plasmid_infuser", has(ModBlocks.PLASMID_INFUSER.get()))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.SCRAPER.get())
-				.pattern(" IS")
-				.pattern(" SI")
-				.pattern("S  ")
-				.define('I', Tags.Items.INGOTS_IRON)
-				.define('S', Items.STICK)
-				.unlockedBy("has_iron_ingot", has(Tags.Items.INGOTS_IRON)),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.SCRAPER.get())
+			.pattern(" IS")
+			.pattern(" SI")
+			.pattern("S  ")
+			.define('I', Tags.Items.INGOTS_IRON)
+			.define('S', Items.STICK)
+			.unlockedBy("has_iron_ingot", has(Tags.Items.INGOTS_IRON))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.SYRINGE.get())
-				.pattern(" P ")
-				.pattern("GBG")
-				.pattern("GAG")
-				.define('P', Items.PISTON)
-				.define('G', Tags.Items.GLASS_BLOCKS_COLORLESS)
-				.define('B', Items.GLASS_BOTTLE)
-				.define('A', Items.ARROW)
-				.unlockedBy("has_scraper", has(ModItems.SCRAPER.get())),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.SYRINGE.get())
+			.pattern(" P ")
+			.pattern("GBG")
+			.pattern("GAG")
+			.define('P', Items.PISTON)
+			.define('G', Tags.Items.GLASS_BLOCKS_COLORLESS)
+			.define('B', Items.GLASS_BOTTLE)
+			.define('A', Items.ARROW)
+			.unlockedBy("has_scraper", has(ModItems.SCRAPER.get()))
+			.save(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.METAL_SYRINGE.get())
-				.pattern("IOI")
-				.pattern("ISI")
-				.pattern("IDI")
-				.define('I', Tags.Items.INGOTS_IRON)
-				.define('D', Tags.Items.GEMS_DIAMOND)
-				.define('S', ModItems.SYRINGE.get())
-				.define('O', Tags.Items.OBSIDIANS)
-				.unlockedBy("has_syringe", has(ModItems.SYRINGE.get())),
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.METAL_SYRINGE.get())
+			.pattern("IOI")
+			.pattern("ISI")
+			.pattern("IDI")
+			.define('I', Tags.Items.INGOTS_IRON)
+			.define('D', Tags.Items.GEMS_DIAMOND)
+			.define('S', ModItems.SYRINGE.get())
+			.define('O', Tags.Items.OBSIDIANS)
+			.unlockedBy("has_syringe", has(ModItems.SYRINGE.get()))
+			.save(pRecipeOutput)
 
-			modonomicon(),
+		modonomicon(pRecipeOutput)
 
-			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.GENE_CHECKER.get())
-				.pattern("III")
-				.pattern("IGI")
-				.pattern("ISI")
-				.define('I', Tags.Items.INGOTS_IRON)
-				.define('G', Tags.Items.GLASS_BLOCKS)
-				.define('S', ModItemTagsProvider.SYRINGES)
-				.unlockedBy("has_scraper", has(ModItems.SCRAPER.get())),
-		)
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.GENE_CHECKER.get())
+			.pattern("III")
+			.pattern("IGI")
+			.pattern("ISI")
+			.define('I', Tags.Items.INGOTS_IRON)
+			.define('G', Tags.Items.GLASS_BLOCKS)
+			.define('S', ModItemTagsProvider.SYRINGES)
+			.unlockedBy("has_scraper", has(ModItems.SCRAPER.get()))
+			.save(pRecipeOutput)
 
-		for (shapedRecipe in shapedRecipes) {
-			shapedRecipe.save(pRecipeOutput)
-		}
 	}
 
-	private fun modonomicon(): ShapedRecipeBuilder {
+	private fun modonomicon(pRecipeOutput: RecipeOutput) {
 		val bookStack = com.klikli_dev.modonomicon.registry.ItemRegistry.MODONOMICON.get().itemStack
 		val bookIdComponent = com.klikli_dev.modonomicon.registry.DataComponentRegistry.BOOK_ID.get()
 
 		bookStack.set(bookIdComponent, OtherUtil.modResource("guide"))
 
-		val recipe = ShapedRecipeBuilder.shaped(RecipeCategory.MISC, bookStack)
+		ShapedRecipeBuilder.shaped(RecipeCategory.MISC, bookStack)
 			.pattern("OB")
 			.define('O', ModItems.ORGANIC_MATTER)
 			.define('B', Items.BOOK)
 			.unlockedBy("has_scraper", has(ModItems.SCRAPER.get()))
-
-		return recipe
+			.save(pRecipeOutput)
 	}
 
-	private fun incubator(pRecipeOutput: RecipeOutput) {
-		val basicIncubatorRecipes = listOf(
-			BasicIncubatorRecipeBuilder(
-				DataComponentIngredient.of(
-					false,
-					DnaHelixItem.getHelixStack(ModGenes.BASIC, lookupProvider.get())
-				),
-				DataComponentIngredient.of(false, OtherUtil.getPotionStack(ModPotions.SUBSTRATE)),
-				OtherUtil.getPotionStack(ModPotions.CELL_GROWTH),
-				"cell_growth"
+	private fun buildIncubatorRecipes(pRecipeOutput: RecipeOutput) {
+		BasicIncubatorRecipeBuilder(
+			DataComponentIngredient.of(
+				false,
+				DnaHelixItem.getHelixStack(ModGenes.BASIC, lookupProvider.get())
 			),
+			DataComponentIngredient.of(false, OtherUtil.getPotionStack(ModPotions.SUBSTRATE)),
+			OtherUtil.getPotionStack(ModPotions.CELL_GROWTH),
+			"cell_growth"
+		).save(pRecipeOutput)
 
-			BasicIncubatorRecipeBuilder(
-				DataComponentIngredient.of(
-					false,
-					DnaHelixItem.getHelixStack(ModGenes.REGENERATION, lookupProvider.get())
-				),
-				DataComponentIngredient.of(false, OtherUtil.getPotionStack(ModPotions.VIRAL_AGENTS)),
-				OtherUtil.getPotionStack(ModPotions.PANACEA),
-				"panacea"
+		BasicIncubatorRecipeBuilder(
+			DataComponentIngredient.of(
+				false,
+				DnaHelixItem.getHelixStack(ModGenes.REGENERATION, lookupProvider.get())
 			),
+			DataComponentIngredient.of(false, OtherUtil.getPotionStack(ModPotions.VIRAL_AGENTS)),
+			OtherUtil.getPotionStack(ModPotions.PANACEA),
+			"panacea"
+		).save(pRecipeOutput)
 
-			BasicIncubatorRecipeBuilder(
-				DataComponentIngredient.of(
-					false,
-					DnaHelixItem.getHelixStack(ModGenes.EMERALD_HEART, lookupProvider.get())
-				),
-				DataComponentIngredient.of(false, OtherUtil.getPotionStack(ModPotions.VIRAL_AGENTS)),
-				OtherUtil.getPotionStack(ModPotions.ZOMBIFY_VILLAGER),
-				"zombify_villager"
-			)
-		)
+
+		BasicIncubatorRecipeBuilder(
+			DataComponentIngredient.of(
+				false,
+				DnaHelixItem.getHelixStack(ModGenes.EMERALD_HEART, lookupProvider.get())
+			),
+			DataComponentIngredient.of(false, OtherUtil.getPotionStack(ModPotions.VIRAL_AGENTS)),
+			OtherUtil.getPotionStack(ModPotions.ZOMBIFY_VILLAGER),
+			"zombify_villager"
+		).save(pRecipeOutput)
 
 		val gmoRecipes = listOf(
 			GmoRecipeBuilder(EntityType.BLAZE, Items.GLOWSTONE_DUST, ModGenes.BIOLUMINESCENCE, 0.85f)
@@ -412,10 +424,6 @@ class ModRecipeProvider(
 			VirusRecipeBuilder(ModGenes.RESISTANCE, ModGenes.GRAY_DEATH),
 			VirusRecipeBuilder(ModGenes.DRAGON_BREATH, ModGenes.WHITE_DEATH)
 		)
-
-		for (recipe in basicIncubatorRecipes) {
-			recipe.save(pRecipeOutput)
-		}
 
 		for (recipe in gmoRecipes) {
 			recipe.save(pRecipeOutput)
