cd2a10d581e200201ba421981951a7b4afecc68a
aaron <aaronhowser1@gmail.com>
2026-05-06 19:48:01 -0500

    Events

---
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/event/custom/GeneCooldownEvent.kt
@@ -0,0 +1,24 @@
+package dev.aaronhowser.mods.geneticsresequenced.event.custom
+
+import dev.aaronhowser.mods.geneticsresequenced.gene.Gene
+import net.minecraft.core.Holder
+import net.minecraft.world.entity.LivingEntity
+import net.neoforged.bus.api.Event
+
+sealed class GeneCooldownEvent : Event() {
+
+	abstract val entity: LivingEntity
+	abstract val geneHolder: Holder<Gene>
+
+	data class Add(
+		override val entity: LivingEntity,
+		override val geneHolder: Holder<Gene>,
+		val cooldownTicks: Int
+	) : GeneCooldownEvent()
+
+	data class Remove(
+		override val entity: LivingEntity,
+		override val geneHolder: Holder<Gene>
+	) : GeneCooldownEvent()
+
+}
