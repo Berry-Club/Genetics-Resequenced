9f912fc53a0c84ddd10a343f150bed1d56ebce7a
aaron <aaronhowser1@gmail.com>
2026-05-11 23:46:10 -0500

    feat: update command registration structure and increment aaron_lib_version

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/command/ModCommands.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/command/ModCommands.kt
@@ -4,6 +4,7 @@ import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.context.CommandContext
 import com.mojang.brigadier.suggestion.SuggestionProvider
 import com.mojang.brigadier.suggestion.SuggestionsBuilder
+import dev.aaronhowser.mods.aaron.command.AaronCommandHelper
 import dev.aaronhowser.mods.geneticsresequenced.GeneticsResequenced
 import dev.aaronhowser.mods.geneticsresequenced.command.gene.*
 import dev.aaronhowser.mods.geneticsresequenced.registry.ModGenes
@@ -11,20 +12,25 @@ import net.minecraft.commands.CommandSourceStack
 import net.minecraft.commands.Commands
 import net.minecraft.commands.SharedSuggestionProvider
 
-object ModCommands {
+object ModCommands : AaronCommandHelper {
 
 	fun register(dispatcher: CommandDispatcher<CommandSourceStack>) {
-		val primary = dispatcher.register(
-			Commands.literal(GeneticsResequenced.MOD_ID)
-				.then(ClearBioGlowCommand.register())
-				.then(ListGenesCommand.register())
-				.then(ListAllGenesCommand.register())
-				.then(GiveGeneCommand.register())
-				.then(GiveTemporaryGeneCommand.register())
-				.then(GiveAllGenesCommand.register())
-				.then(RemoveGeneCommand.register())
-				.then(RemoveAllGenesCommand.register())
-		)
+
+		val command = literal(GeneticsResequenced.MOD_ID) {
+			then(ClearBioGlowCommand.register())
+
+			thenLiteral("gene") {
+				then(ListGenesCommand.register())
+				then(ListAllGenesCommand.register())
+				then(GiveGeneCommand.register())
+				then(GiveTemporaryGeneCommand.register())
+				then(GiveAllGenesCommand.register())
+				then(RemoveGeneCommand.register())
+				then(RemoveAllGenesCommand.register())
+			}
+		}
+
+		val primary = dispatcher.register(command)
 
 		dispatcher.register(Commands.literal("genetics").redirect(primary))
 		dispatcher.register(Commands.literal("gr").redirect(primary))
