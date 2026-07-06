5c4d8c8656ce1eed7e6451ce3a4b1b9591a73c5e
aaron <aaronhowser1@gmail.com>
2026-07-06 12:29:48 -0500

    just do LE

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/compatibility/kubejs/GeneticsJS.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/compatibility/kubejs/GeneticsJS.kt
@@ -1,5 +1,6 @@
 package dev.aaronhowser.mods.geneticsresequenced.compatibility.kubejs
 
+import dev.aaronhowser.mods.geneticsresequenced.attachment.GeneCooldowns
 import dev.aaronhowser.mods.geneticsresequenced.attachment.GenesData.Companion.addGene
 import dev.aaronhowser.mods.geneticsresequenced.attachment.GenesData.Companion.hasGene
 import dev.aaronhowser.mods.geneticsresequenced.attachment.GenesData.Companion.removeGene
@@ -15,7 +16,6 @@ import dev.latvian.mods.kubejs.plugin.KubeJSPlugin
 import dev.latvian.mods.kubejs.registry.ServerRegistryRegistry
 import dev.latvian.mods.kubejs.script.BindingRegistry
 import net.minecraft.resources.ResourceKey
-import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.LivingEntity
 
 @Suppress("unused")
@@ -38,33 +38,40 @@ class GeneticsJS : KubeJSPlugin {
 		val GENE_CHANGED: EventHandler = EVENT_GROUP.server("geneChanged") { GeneChangeKubeEvent::class.java }.hasResult()
 
 		@JvmStatic
-		fun hasGene(entity: Entity, geneRk: ResourceKey<Gene>): Boolean {
+		fun hasGene(entity: LivingEntity, geneRk: ResourceKey<Gene>): Boolean {
 			return entity.hasGene(geneRk)
 		}
 
 		@JvmStatic
-		fun addGene(entity: Entity, geneRk: ResourceKey<Gene>): Boolean {
-			if (entity !is LivingEntity) return false
+		fun addGene(entity: LivingEntity, geneRk: ResourceKey<Gene>): Boolean {
 			return entity.addGene(geneRk)
 		}
 
 		@JvmStatic
-		fun addTemporaryGene(entity: Entity, geneRk: ResourceKey<Gene>, tickDuration: Int): Boolean {
-			if (entity !is LivingEntity) return false
+		fun addTemporaryGene(entity: LivingEntity, geneRk: ResourceKey<Gene>, tickDuration: Int): Boolean {
 			return entity.addTemporaryGene(geneRk, tickDuration)
 		}
 
 		@JvmStatic
-		fun removeGene(entity: Entity, geneRk: ResourceKey<Gene>): Boolean {
-			if (entity !is LivingEntity) return false
+		fun removeGene(entity: LivingEntity, geneRk: ResourceKey<Gene>): Boolean {
 			return entity.removeGene(geneRk)
 		}
 
 		@JvmStatic
-		fun removeTemporaryGene(entity: Entity, geneRk: ResourceKey<Gene>): Boolean {
-			if (entity !is LivingEntity) return false
+		fun removeTemporaryGene(entity: LivingEntity, geneRk: ResourceKey<Gene>): Boolean {
 			return entity.removeTemporaryGene(geneRk)
 		}
+
+		@JvmStatic
+		fun addCooldown(entity: LivingEntity, geneRk: ResourceKey<Gene>, tickDuration: Int, notify: Boolean): Boolean {
+			return GeneCooldowns.addCooldown(entity, geneRk, tickDuration, notify)
+		}
+
+		@JvmStatic
+		fun removeCooldown(entity: LivingEntity, geneRk: ResourceKey<Gene>): Boolean {
+			return GeneCooldowns.removeCooldown(entity, geneRk)
+		}
+
 	}
 
 }
