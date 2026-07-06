d3815ae46df0add95cb79dae682efe7498d2ed6e
aaron <aaronhowser1@gmail.com>
2026-07-06 18:11:47 -0500

    Revert "tried to improve squish etc"
    This reverts commit d480d1dd804891d11d3f4df8038699686fb10356.

--- src/main/kotlin/dev/aaronhowser/mods/genetics_resequenced/client/renderer/entity/SupportSlimeRenderer.kt
+++ src/main/kotlin/dev/aaronhowser/mods/genetics_resequenced/client/renderer/entity/SupportSlimeRenderer.kt
@@ -64,9 +64,13 @@ class SupportSlimeRenderer(
 		val skinRenderType = state.ownerSkinRenderType ?: return
 
 		poseStack.withPose {
-			poseStack.translate(0f, 0.5f, 0f)
-			scale(state, poseStack)
+			val scale = state.size.toFloat()
+
+			poseStack.translate(-scale / 2.0, 0.0, -scale / 2.0)
+			poseStack.scale(scale, scale, scale)
+			poseStack.translate(0.5, 0.0, 0.5)
 			poseStack.mulPose(Axis.YP.rotationDegrees(state.skullYaw))
+			poseStack.scale(-1.0f, -1.0f, 1.0f)
 
 			SkullBlockRenderer.submitSkull(
 				0f,
@@ -79,16 +83,6 @@ class SupportSlimeRenderer(
 				null
 			)
 		}
-	}
-
-	override fun scale(state: State, poseStack: PoseStack) {
-		val s = 0.999f
-		poseStack.scale(s, s, s)
-		poseStack.translate(0f, 0.001f, 0f)
-		val size = state.size
-		val ss = state.squish / (size * 0.5f + 1f)
-		val w = 1f / (ss + 1f)
-		poseStack.scale(w * size, 1f / w * size, w * size)
 	}
 
 	override fun getTextureLocation(state: State): Identifier {
