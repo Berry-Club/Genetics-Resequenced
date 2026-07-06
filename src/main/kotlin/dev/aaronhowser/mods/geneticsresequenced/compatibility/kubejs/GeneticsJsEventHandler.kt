72b848ddb701d1faf961b63f9702809c842295ad
aaron <aaronhowser1@gmail.com>
2026-07-06 12:46:25 -0500

    rename

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/compatibility/kubejs/GeneticsJsEventHandler.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/compatibility/kubejs/GeneticsJsEventHandler.kt
@@ -11,7 +11,7 @@ import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
 
 @EventBusSubscriber(modid = GeneticsResequenced.MOD_ID)
-object GeneticsJsServerHandler {
+object GeneticsJsEventHandler {
 
 	@SubscribeEvent
 	fun beforeGeneChange(event: GeneChangeEvent.Pre) {
