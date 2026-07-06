8b41b4b9bf0a0ced79bc570492c12ae5223d6545
aaron <aaronhowser1@gmail.com>
2026-07-06 14:27:34 -0500

    info

--- src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/compatibility/kubejs/GeneticsJS.kt
+++ src/main/kotlin/dev/aaronhowser/mods/geneticsresequenced/compatibility/kubejs/GeneticsJS.kt
@@ -25,6 +25,7 @@ import dev.latvian.mods.kubejs.event.TargetedEventHandler
 import dev.latvian.mods.kubejs.plugin.KubeJSPlugin
 import dev.latvian.mods.kubejs.registry.ServerRegistryRegistry
 import dev.latvian.mods.kubejs.script.BindingRegistry
+import dev.latvian.mods.kubejs.typings.Info
 import net.minecraft.core.registries.Registries
 import net.minecraft.resources.ResourceKey
 import net.minecraft.world.entity.EntityType
@@ -89,81 +90,96 @@ class GeneticsJS : KubeJSPlugin {
 				.supportsTarget(GENE_TARGET)
 
 		@JvmStatic
+		@Info("Check whether the entity has the Gene as either a permanent or temporary Gene")
 		fun hasGene(entity: LivingEntity, geneRk: ResourceKey<Gene>): Boolean {
 			return entity.hasGene(geneRk)
 		}
 
 		@JvmStatic
+		@Info("Check whether the entity has the Gene as a permanent Gene")
 		fun hasPermanentGene(entity: LivingEntity, geneRk: ResourceKey<Gene>): Boolean {
 			val holder = ModGenes.fromResourceKey(entity.registryAccess(), geneRk) ?: return false
 			return entity.hasPermanentGene(holder)
 		}
 
 		@JvmStatic
+		@Info("Check whether the entity has the Gene as a temporary Gene")
 		fun hasTemporaryGene(entity: LivingEntity, geneRk: ResourceKey<Gene>): Boolean {
 			return entity.hasTemporaryGene(geneRk)
 		}
 
 		@JvmStatic
+		@Info("Get the entity's permanent Gene ids")
 		fun getPermanentGenes(entity: LivingEntity): List<ResourceKey<Gene>> {
 			return entity.permanentGeneHolders.mapNotNull { it.unwrapKey().orElse(null) }
 		}
 
 		@JvmStatic
+		@Info("Get the entity's temporary Gene ids")
 		fun getTemporaryGenes(entity: LivingEntity): List<ResourceKey<Gene>> {
 			return entity.temporaryGeneHolders.mapNotNull { it.unwrapKey().orElse(null) }
 		}
 
 		@JvmStatic
+		@Info("Get the entity's permanent and temporary Gene ids")
 		fun getActiveGenes(entity: LivingEntity): List<ResourceKey<Gene>> {
 			return entity.getActiveGenes().mapNotNull { it.unwrapKey().orElse(null) }
 		}
 
 		@JvmStatic
+		@Info("Add a permanent Gene to the entity")
 		fun addGene(entity: LivingEntity, geneRk: ResourceKey<Gene>): Boolean {
 			return entity.addGene(geneRk)
 		}
 
 		@JvmStatic
+		@Info("Add or refresh a temporary Gene on the entity for the given number of ticks")
 		fun addTemporaryGene(entity: LivingEntity, geneRk: ResourceKey<Gene>, tickDuration: Int): Boolean {
 			return entity.addTemporaryGene(geneRk, tickDuration)
 		}
 
 		@JvmStatic
+		@Info("Remove a permanent Gene from the entity")
 		fun removeGene(entity: LivingEntity, geneRk: ResourceKey<Gene>): Boolean {
 			return entity.removeGene(geneRk)
 		}
 
 		@JvmStatic
+		@Info("Remove a temporary Gene from the entity")
 		fun removeTemporaryGene(entity: LivingEntity, geneRk: ResourceKey<Gene>): Boolean {
 			return entity.removeTemporaryGene(geneRk)
 		}
 
 		@JvmStatic
+		@Info("Check whether the Gene is on cooldown for the entity")
 		fun isOnCooldown(entity: LivingEntity, geneRk: ResourceKey<Gene>): Boolean {
 			return GeneCooldowns.isOnCooldown(entity, geneRk)
 		}
 
 		@JvmStatic
+		@Info("Get the Gene's remaining cooldown ticks for the entity, or -1 if it is not on cooldown")
 		fun getCooldownTicks(entity: LivingEntity, geneRk: ResourceKey<Gene>): Int {
 			val holder = geneRk.getHolderOrThrow(entity.registryAccess())
 			val entry = entity.geneCooldowns.cooldowns.firstOrNull { it.geneHolder.isGene(holder) }
-			return entry?.remainingTicks() ?: 0
+			return entry?.remainingTicks() ?: -1
 		}
 
 		@JvmStatic
+		@Info("Get the temporary Gene's remaining ticks for the entity, or -1 if it is not present")
 		fun getTemporaryGeneTicks(entity: LivingEntity, geneRk: ResourceKey<Gene>): Int {
 			val holder = ModGenes.fromResourceKey(entity.registryAccess(), geneRk) ?: return 0
 			val temporaryGene = entity.temporaryGenes.firstOrNull { it.geneHolder.isGene(holder) }
-			return temporaryGene?.ticksRemaining ?: 0
+			return temporaryGene?.ticksRemaining ?: -1
 		}
 
 		@JvmStatic
+		@Info("Put the Gene on cooldown for the entity for the given number of ticks")
 		fun addCooldown(entity: LivingEntity, geneRk: ResourceKey<Gene>, tickDuration: Int, notify: Boolean): Boolean {
 			return GeneCooldowns.addCooldown(entity, geneRk, tickDuration, notify)
 		}
 
 		@JvmStatic
+		@Info("Remove the Gene's cooldown from the entity")
 		fun removeCooldown(entity: LivingEntity, geneRk: ResourceKey<Gene>): Boolean {
 			return GeneCooldowns.removeCooldown(entity, geneRk)
 		}
