e6c8c4d561996caa571d39b8cdf268f42654857e
aaron <aaronhowser1@gmail.com>
2026-09-24 06:44:48 -0500

    fix?

--- src/main/kotlin/dev/aaronhowser/mods/genetics_resequenced/GeneticsResequenced.kt
+++ src/main/kotlin/dev/aaronhowser/mods/genetics_resequenced/GeneticsResequenced.kt
@@ -4,19 +4,21 @@ import dev.aaronhowser.mods.genetics_resequenced.config.ClientConfig
 import dev.aaronhowser.mods.genetics_resequenced.config.ServerConfig
 import dev.aaronhowser.mods.genetics_resequenced.registry.ModRegistries
 import net.minecraft.resources.ResourceLocation
+import net.minecraftforge.fml.ModLoadingContext
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.config.ModConfig
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import thedarkcolour.kotlinforforge.forge.MOD_BUS
 
 @Mod(GeneticsResequenced.MOD_ID)
-class GeneticsResequenced(
-	modLoadingContext: FMLJavaModLoadingContext
-) {
+class GeneticsResequenced {
 
 	init {
+		// KotlinForForge 4 requires a no-argument constructor, so the context cannot be injected.
+		@Suppress("DEPRECATION")
+		val modLoadingContext = ModLoadingContext.get()
+
 		ModRegistries.register(MOD_BUS)
 
 //		runWhenOn(Dist.CLIENT) {
